f8194f6662d1bddf42a6c982be695de77ee42184
gvl <gustav.larsson@tt.se>
2023-10-16 19:01:53 +0200

    merge

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import NavigationProvider from '@/contexts/navigation'
+import { NavigationProvider } from '@/contexts/navigation'
 
 import { ThemeProvider } from '@/contexts/theme-provider'
 
