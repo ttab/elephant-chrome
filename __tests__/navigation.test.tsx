62aeee4ec37f0989c7a01301464e6abde8b9b9b6
gvl <gustav.larsson@tt.se>
2023-10-27 08:26:14 +0200

    fix: remove props from test as well

--- __tests__/navigation.test.tsx
+++ __tests__/navigation.test.tsx
@@ -1,30 +1,11 @@
 import { NavigationProvider } from '@/contexts'
-import type { NavigationState } from '@/types'
 import { render, screen } from '../setupTests'
 import App from '@/App'
-
-function init(): NavigationState {
-  return {
-    registry: {
-      get: () => {
-        return {
-          metadata: {
-            path: '/',
-            name: 'Test'
-          },
-          component: () => <p>test</p>
-        }
-      },
-      set: () => {}
-    },
-    content: []
-  }
-}
 
 describe('Use NavigationProvider', () => {
   it('should render view from registry', () => {
     render(
-      <NavigationProvider init={init}>
+      <NavigationProvider>
         <App />
       </NavigationProvider>
     )
