c762b42cbdeaadfd22e22a089c020cb836a624f4
gvl <gustav.larsson@tt.se>
2023-11-01 12:51:40 +0100

    fix: remove imports

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,6 +1,4 @@
 import { AppMenu, ThemeSwitcher } from '@/components'
-import { Button } from '@ttab/elephant-ui'
-import { Maximize2 } from '@ttab/elephant-ui/icons'
 
 export const AppHeader = (): JSX.Element => {
   return (
