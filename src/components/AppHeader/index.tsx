36925dd20ab6a863091722539acd05786e4bd6cb
gvl <gustav.larsson@tt.se>
2023-11-01 10:40:32 +0100

    fix: remove maximize

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -5,9 +5,6 @@ import { Maximize2 } from '@ttab/elephant-ui/icons'
 export const AppHeader = (): JSX.Element => {
   return (
     <header className='justify-end flex'>
-      <Button variant='ghost' className='w-9 px-0'>
-        <Maximize2 className='h-[1.2rem] w-[1.2rem]'/>
-      </Button>
       <ThemeSwitcher />
       <AppMenu />
     </header>
