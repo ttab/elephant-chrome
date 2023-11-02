d1d6a9246d0e5746d441267242e78bd178c85efe
gvl <gustav.larsson@tt.se>
2023-11-02 13:28:13 +0100

    fix: wrong merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useNavigation, useSession } from '@/hooks'
 import { AppHeader } from '@/components'
-import { ApiProvider } from '@/contexts/ApiProvider'
 import { Login } from './views/auth/Login'
 
 export const App = (): JSX.Element => {
@@ -8,22 +7,20 @@ export const App = (): JSX.Element => {
   const { state } = useNavigation()
 
   return (
-    <ApiProvider protocol={apiProtocol} host={apiHost} port={apiPort} session={session}>
-      <div className='relative flex h-screen flex-col bg-white dark:bg-black'>
-
-        {!session.jwt
-          ? <Login />
-          : (
-              <>
-                <div className='absolute top-0 right-0 w-28 h-10 p-2 z-10 justify-end flex'>
-                  <AppHeader />
-                </div>
-                <div className='flex flex-1 gap-4 p-2 h-max'>
-                  {state.content}
-                </div>
-              </>
-            )}
-      </div>
-    </ApiProvider>
+    <div className='relative flex h-screen flex-col bg-white dark:bg-black'>
+      {!jwt
+        ? <Login />
+        : (
+            <>
+              <div className='absolute top-0 right-0 w-28 h-10 p-2 z-10 justify-end flex'>
+                <AppHeader />
+              </div>
+              <div className='flex flex-1 gap-4 p-2 h-max'>
+                {state.content}
+              </div>
+            </>
+          )
+      }
+    </div>
   )
 }
