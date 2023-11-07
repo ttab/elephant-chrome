cf05399ae38bde6d57458a8a968bbf0427cb2259
Danne Lundqvist <danne@dotvoid.com>
2023-11-07 15:13:02 +0100

    Add awareness and slate yjs cursors

--- src/views/Editor/index.tsx
+++ src/views/Editor/index.tsx
@@ -1,7 +1,7 @@
 import { useApi } from '@/hooks/useApi'
 import { useEffect, useMemo, useState } from 'react'
 import { ViewHeader } from '@/components'
-import { withYjs, withYHistory, YjsEditor } from '@slate-yjs/core'
+import { withYjs, withYHistory, YjsEditor, withCursors } from '@slate-yjs/core'
 import * as Y from 'yjs'
 import { createEditor } from 'slate'
 
@@ -53,27 +53,32 @@ export const Editor = (props: ViewProps): JSX.Element => {
           console.warn('Lost connection', viewId)
           setConnectionStatus(WebSocketStatus.Disconnected)
         }
+      },
+      onAwarenessUpdate: ({ states }) => {
+        console.log(states)
       }
-      // TODO: Implement awareness
-      // onAwarenessUpdate: ({ states }) => {
-      //   console.log(states)
-      // }
     })
   }, [documentId, hocuspocusWebsocket, jwt?.accessToken, viewId])
 
   // Create YjsEditor for Textbit to use
   const editor = useMemo(() => {
-    if (!provider) {
+    if (!provider?.awareness) {
       return
     }
 
     return withYHistory(
-      withYjs(
-        createEditor(),
-        provider.document.get('content', Y.XmlText) as Y.XmlText // sharedType as Y.XmlText
+      withCursors(
+        withYjs(
+          createEditor(),
+          provider.document.get('content', Y.XmlText) as Y.XmlText
+        ),
+        provider?.awareness,
+        {
+          data: cursorData(jwt?.sub_name || '')
+        }
       )
     )
-  }, [provider])
+  }, [provider, jwt?.sub_name])
 
   // Connect Yjs to editor once
   useEffect(() => {
@@ -85,7 +90,7 @@ export const Editor = (props: ViewProps): JSX.Element => {
 
   const views = [
     // @ts-expect-error FIXME: yjsEditor needs more refinement
-    <TextbitEditor yjsEditor={editor} />,
+    < TextbitEditor yjsEditor={editor} />,
     <strong className="animate-pulse">Not connected</strong>
   ]
 
@@ -99,6 +104,45 @@ export const Editor = (props: ViewProps): JSX.Element => {
       </main>
     </>
   )
+}
+
+function cursorData(name: string): Record<string, unknown> {
+  const colors = [
+    'aquamarine',
+    'beige',
+    'blueviolet',
+    'brown',
+    'cadetblue',
+    'burlywood',
+    'chocoalate',
+    'coral',
+    'crimson',
+    'hotpink',
+
+    'lightcoral',
+    'lightpink',
+    'lightgreen',
+    'lightgray',
+    'lightcyan',
+    'lightblue',
+    'lightsalmon',
+    'lightseagreen',
+    'lightsteelblue',
+    'lemonchiffon',
+    'palegreen',
+    'tomato'
+  ]
+
+  const [first, last] = name.split(' ')
+  const f1 = first.substring(0, 1)
+  const l1 = last.substring(0, 1)
+
+  return {
+    color: colors[Math.floor(Math.random() * colors.length)],
+    initials: `${f1}${l1}`,
+    name,
+    avatar: ''
+  }
 }
 
 Editor.displayName = 'Editor'
