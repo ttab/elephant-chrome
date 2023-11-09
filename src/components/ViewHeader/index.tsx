97beeaa8b564b3d2bf95b5ee4751a559fe905747
gvl <gustav.larsson@tt.se>
2023-11-09 09:44:40 +0100

    feat: close menu on link click

--- src/components/ViewHeader/index.tsx
+++ src/components/ViewHeader/index.tsx
@@ -5,7 +5,7 @@ interface ViewHeaderProps {
 export const ViewHeader = ({ title }: ViewHeaderProps): JSX.Element => {
   return (
     <header className='relative top-0 group-last:w-[calc(100%-7rem)]'>
-      <h1 className='font-bold text-2xl mb-8'>{title}</h1>
+      <h1 className='font-bold text-2xl mb-8 break-all'>{title}</h1>
     </header>
   )
 }
