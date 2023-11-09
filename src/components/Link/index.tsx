1ad9d32da3c095321b1d276810de57efd95a9273
Gustav Larsson <gularsson@users.noreply.github.com>
2023-11-09 15:10:24 +0100

    feat: close menu on link click (#13)

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,3 +1,4 @@
+import { type ForwardedRef, forwardRef } from 'react'
 import { type View, type ViewProps } from '@/types'
 import { useNavigation } from '@/hooks'
 import { v4 as uuid } from 'uuid'
@@ -8,20 +9,28 @@ interface LinkProps {
   children: React.ReactNode
   to: View
   props?: ViewProps
+  onClick?: (event: unknown) => void
 }
 
-export const Link = ({ children, to, props }: LinkProps): JSX.Element => {
+export const Link = forwardRef((props: LinkProps, ref: ForwardedRef<HTMLAnchorElement>) => {
   const { state, dispatch } = useNavigation()
   const id = uuid()
-  const linkItem = state.registry.get(to)
+  const linkItem = state.registry.get(props.to)
 
   return (
     <a
+      {...props}
       className='p-1 hover:font-bold'
       href={`${linkItem.metadata.path}?id=${id}`}
-      onClick={(event) => handleLink({ event, dispatch, linkItem, props, id })}
-    >
-      {children}
+      onClick={(event) => {
+        // Execute forwarded onClick handler
+        props.onClick && props.onClick(event)
+
+        // Our onClick handler
+        handleLink({ event, dispatch, linkItem, props: props.props, id })
+      }}
+      ref={ref}>
+        {props.children}
     </a>
   )
-}
+})
