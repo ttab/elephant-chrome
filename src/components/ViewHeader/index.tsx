e4f59175dc5cfd4bd288c7a6521a5fd7f025511e
gvl <gustav.larsson@tt.se>
2023-11-24 11:19:20 +0100

    fix: hide ViewFocus if one view, fix id

--- src/components/ViewHeader/index.tsx
+++ src/components/ViewHeader/index.tsx
@@ -1,15 +1,19 @@
 import { type ViewProps } from '@/types'
 import { ViewFocus } from './ViewFocus'
+import { useNavigation } from '@/hooks'
 
 interface ViewHeaderProps extends ViewProps {
   title: string
 }
 
 export const ViewHeader = ({ title, id }: ViewHeaderProps): JSX.Element => {
+  const { state } = useNavigation()
   return (
     <header className='relative top-0 group-last:w-[calc(100%-8rem)] flex justify-between'>
       <h1 className='font-bold text-2xl mb-8 break-all'>{title}</h1>
-      <ViewFocus id={id} />
+      {state.content.length > 1 &&
+        <ViewFocus id={id} />
+      }
     </header>
   )
 }
