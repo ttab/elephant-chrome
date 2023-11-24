665cdfe2a4cf4d709f6101e47d2378341f00dd24
Gustav Larsson <gularsson@users.noreply.github.com>
2023-11-24 15:17:43 +0100

    chore: add test link, global mocks (#21)

---
+++ __tests__/link.test.tsx
@@ -0,0 +1,27 @@
+import { SessionProvider } from '@/contexts'
+import { NavigationProvider } from '@/navigation/components'
+import userEvent from '@testing-library/user-event'
+
+import { render, screen } from '../setupTests'
+import { Link } from '@/components'
+
+describe('Link', () => {
+  it('should render Link component', async () => {
+    render(
+      <SessionProvider endpoint={new URL('/user', 'http://localhost')}>
+        <NavigationProvider>
+          <Link to='Editor' props={{ documentId: 'abc123' }}>
+            Planning Overview
+          </Link>
+        </NavigationProvider>
+      </SessionProvider>
+    )
+    expect(screen.getByText('Planning Overview')).toBeInTheDocument()
+
+    await userEvent.click(screen.getByText('Planning Overview'))
+
+    expect(history.state.props.documentId).toBe('abc123')
+    expect(history.state.itemName).toBe('Editor')
+    expect(history.state.contentState[1].path).toBe('/editor?documentId=abc123')
+  })
+})
