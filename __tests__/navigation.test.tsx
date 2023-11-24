1eb06ce1f87bd7d06efe8372d9b5d51f00f1c5c8
gvl <gustav.larsson@tt.se>
2023-11-24 13:33:04 +0100

    chore: add test link, global mocks

--- __tests__/navigation.test.tsx
+++ __tests__/navigation.test.tsx
@@ -4,31 +4,6 @@ import { NavigationProvider } from '@/navigation/components'
 import { App } from '@/App'
 import { render, screen } from '../setupTests'
 
-
-const JWT = { sub: 'abc', sub_name: 'ABC', units: ['a', 'b', 'c'], scope: 'AbC', access_token: 'xxx' }
-
-jest.mock('../src/hooks/useSession.tsx', () => ({
-  useSession: jest.fn().mockImplementation(() => { return { jwt: JWT, setJwt: () => { } } })
-}))
-
-function mockUrl(url: string): unknown {
-  switch (url.split('/')[1]) {
-    case 'user':
-      return JSON.stringify(JWT)
-
-    // Fixme: Add and anonymize planning data for mocks
-    case 'core_planning_item/_search':
-      return ''
-  }
-}
-global.fetch = jest.fn().mockImplementation(async (url: string) => {
-  return await Promise.resolve({
-    status: 200,
-    ok: true,
-    text: async () => await Promise.resolve(mockUrl(url))
-  })
-})
-
 describe('Use NavigationProvider', () => {
   it('should render view from registry', async () => {
     render(
--- setupTests.ts
+++ setupTests.ts
@@ -1,2 +1,26 @@
 import '@testing-library/jest-dom'
 export * from '@testing-library/react'
+
+const JWT = { sub: 'abc', sub_name: 'ABC', units: ['a', 'b', 'c'], scope: 'AbC', access_token: 'xxx' }
+
+function mockUrl(url: string): unknown {
+  switch (url.split('/')[1]) {
+    case 'user':
+      return JSON.stringify(JWT)
+
+    // Fixme: Add and anonymize planning data for mocks
+    case 'core_planning_item/_search':
+      return ''
+  }
+}
+global.fetch = jest.fn().mockImplementation(async (url: string) => {
+  return await Promise.resolve({
+    status: 200,
+    ok: true,
+    text: async () => await Promise.resolve(mockUrl(url))
+  })
+})
+
+jest.mock('.@/hooks/useSession.tsx', () => ({
+  useSession: jest.fn().mockImplementation(() => { return { jwt: JWT, setJwt: () => { } } })
+}))
