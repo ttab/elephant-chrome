2d93c17fc4af937c60d441598d60713856225b8a
Gustav Larsson <gularsson@users.noreply.github.com>
2023-11-28 17:34:20 +0100

    test: add CommandMenu (#23)

---
+++ __tests__/commandMenu.test.tsx
@@ -0,0 +1,27 @@
+import { SessionProvider } from '@/contexts'
+import { NavigationProvider } from '@/navigation/components'
+import { render, screen } from '../setupTests'
+import { CommandMenu } from '@/components/CommandMenu'
+import userEvent from '@testing-library/user-event'
+
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn()
+}))
+
+describe('CommandMenu', () => {
+  it('should render CommandMenu component', async () => {
+    render(
+      <SessionProvider endpoint={new URL('/user', 'http://localhost')}>
+        <NavigationProvider>
+          <CommandMenu />
+        </NavigationProvider>
+      </SessionProvider>
+    )
+    await userEvent.keyboard('{Control>}k')
+    expect(screen.getByText('Planning overview')).toBeInTheDocument()
+    await userEvent.click(screen.getByRole('option'))
+    expect(history.state.itemName).toBe('PlanningOverview')
+  })
+})
