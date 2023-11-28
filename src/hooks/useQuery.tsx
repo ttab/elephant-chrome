3bf5d72ea6a2d2bf19448b1040db8641f0c6cb58
Danne Lundqvist <danne@dotvoid.com>
2023-11-28 15:24:48 +0100

    Set query param state from params imeediately to avoid one extra rerender when loading views from urls

--- src/hooks/useQuery.tsx
+++ src/hooks/useQuery.tsx
@@ -1,22 +1,20 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 
 export const useQuery = (): Record<string, string | undefined> => {
-  const [queryParams, setQueryParams] = useState<Record<string, string | undefined>>({})
+  const parseQueryString = useCallback((): Record<string, string | undefined> => {
+    const searchParams = new URLSearchParams(window.location.search)
+    const params: Record<string, string | undefined> = {}
 
-  useEffect(() => {
-    const parseQueryString = (): Record<string, string | undefined> => {
-      const searchParams = new URLSearchParams(window.location.search)
-      const params: Record<string, string | undefined> = {}
-
-      for (const [key, value] of searchParams.entries()) {
-        params[key] = value
-      }
-
-      return params
+    for (const [key, value] of searchParams.entries()) {
+      params[key] = value
     }
 
-    setQueryParams(parseQueryString())
+    return params
+  }, [])
 
+  const [queryParams, setQueryParams] = useState<Record<string, string | undefined>>(parseQueryString)
+
+  useEffect(() => {
     const handlePopstate = (): void => {
       setQueryParams(parseQueryString())
     }
@@ -25,7 +23,7 @@ export const useQuery = (): Record<string, string | undefined> => {
     return () => {
       window.removeEventListener('popstate', handlePopstate)
     }
-  }, [])
+  }, [parseQueryString])
 
   return queryParams
 }
