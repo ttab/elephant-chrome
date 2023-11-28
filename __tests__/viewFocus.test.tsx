0a821c4d22dda512a58db341ec32b09758bfb15e
Gustav Larsson <gularsson@users.noreply.github.com>
2023-11-28 17:35:19 +0100

    test: add viewfocus (#22)

---
+++ __tests__/viewFocus.test.tsx
@@ -0,0 +1,42 @@
+import { type Dispatch } from 'react'
+import { SessionProvider } from '@/contexts'
+import { NavigationProvider } from '@/navigation/components'
+import userEvent from '@testing-library/user-event'
+
+import { useNavigation } from '@/navigation/hooks'
+
+import { render, screen } from '../setupTests'
+import { ViewFocus } from '@/components/ViewHeader/ViewFocus'
+import { type NavigationActionType } from '@/types'
+import { init } from '@/lib/init'
+
+
+jest.mock('@/navigation/hooks/useNavigation', () => ({
+  useNavigation: jest.fn()
+}))
+const mockState = init()
+
+const mockDispatch = jest.fn() as Dispatch<NavigationActionType>
+
+(useNavigation as jest.Mock).mockReturnValue({
+  state: mockState,
+  dispatch: mockDispatch
+})
+
+describe('ViewFocus', () => {
+  it('should render ViewFocus component', async () => {
+    render(
+      <SessionProvider endpoint={new URL('/user', 'http://localhost')}>
+        <NavigationProvider>
+          <ViewFocus id={'abc123'}/>
+        </NavigationProvider>
+      </SessionProvider>
+    )
+
+    // Open with button, close with escape
+    await userEvent.click(screen.getByRole('button'))
+    mockState.focus = 'abc123'
+    await userEvent.keyboard('{Escape}')
+    expect(mockDispatch).toHaveBeenCalledWith({ id: 'abc123', type: 'focus' })
+  })
+})
