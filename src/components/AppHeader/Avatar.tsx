412b30791d3daff74b65aea247351daf1ac8edba
Gustav Larsson <gularsson@users.noreply.github.com>
2023-12-05 08:57:19 +0100

    fix: color, size, remove theme toggle (#29)

--- src/components/AppHeader/Avatar.tsx
+++ src/components/AppHeader/Avatar.tsx
@@ -14,8 +14,12 @@ export const Avatar = (): JSX.Element => {
     <Popover>
 
       <PopoverTrigger className='mr-2'>
-        <AvatarMain>
-          <AvatarFallback>{jwt?.sub.replace('user://tt/', '') || '?'}</AvatarFallback>
+        <AvatarMain className='h-8 w-8 mt-1'>
+          <AvatarFallback
+            className='bg-[#973C9F] text-background'
+          >
+            {jwt?.sub.replace('user://tt/', '') || '?'}
+          </AvatarFallback>
         </AvatarMain>
       </PopoverTrigger>
 
