502365b3c868f03cd2c97e7ca133836b1f9354f6
gvl <gustav.larsson@tt.se>
2023-12-06 09:16:53 +0100

    fix: priority placing, private planning, slugline, remove header

--- src/components/PlanningTable/Columns.tsx
+++ src/components/PlanningTable/Columns.tsx
@@ -10,36 +10,68 @@ import {
 
 import { assignmentTypes, sectors, priorities } from './data/data'
 import { type Planning } from './data/schema'
-import { ColumnHeader } from './ColumnHeader'
 import { RowActions } from './RowActions'
 import { cn } from '@ttab/elephant-ui/utils'
 
 export const columns: Array<ColumnDef<Planning>> = [
   {
+    id: 'priority',
+    accessorFn: (data) => data._source['document.meta.core_planning_item.data.priority'][0],
+    cell: ({ row }) => {
+      const priority = priorities.find(
+        (priority) => {
+          return priority.value === row.getValue('priority')
+        }
+      )
+      if (!priority) {
+        return null
+      }
+
+      return (
+        <div className='flex w-1'>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              {priority.icon && (
+                <priority.icon className='h-6 w-6 text-muted-foreground' color={priority.color} />
+              )}
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{priority.label}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        </div>
+      )
+    },
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id))
+    }
+  },
+  {
     id: 'title',
     accessorFn: (data) => data._source['document.title'][0],
-    header: ({ column }) => (
-      <ColumnHeader column={column} title="Title" />
-    ),
     cell: ({ row }) => {
       const status = row.original._source['document.meta.core_planning_item.data.public'][0] === 'true'
       const slugline = row.original._source['document.meta.core_assignment.meta.tt_slugline.value']
 
       return (
-        <div className="flex space-x-2 w-fit">
-          {status && (
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger>
-                  <div className="flex items-center bg-[#5895FF] h-2 w-2 rounded-full mx-4" />
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Public</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          )}
-          <span className="w-[200px] md:w-[300px] lg:w-[500px] truncate font-medium">
+        <div className='flex space-x-2 w-fit'>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <div className={cn(
+                  'flex items-center h-2 w-2 rounded-full mx-4',
+                  status
+                    ? 'bg-[#5895FF]'
+                    : 'border')} />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Public</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <span className='max-w-[200px] md:max-w-[300px] lg:max-w-[700px] truncate font-medium'>
             {row.getValue('title')}
           </span>
           {slugline?.length && (
@@ -52,62 +84,23 @@ export const columns: Array<ColumnDef<Planning>> = [
   {
     id: 'section',
     accessorFn: (data) => data._source['document.rel.sector.title'][0],
-    header: ({ column }) => (
-      <ColumnHeader column={column} title="Section" />
-    ),
     cell: ({ row }) => {
       const sector = sectors.find((label) => label.value === row.original._source['document.rel.sector.title'][0])
 
-      return sector && <Badge variant="outline">
+      return sector && <Badge variant='outline'>
         <div className={cn('h-2 w-2 rounded-full mr-2', sector?.color) } />
         <span className='text-slate-500 font-medium font-sans'>{sector.label}</span>
       </Badge>
     }
   },
   {
-    id: 'priority',
-    accessorFn: (data) => data._source['document.meta.core_planning_item.data.priority'][0],
-    header: ({ column }) => (
-      <ColumnHeader column={column} title="Priority" />
-    ),
-    cell: ({ row }) => {
-      const priority = priorities.find(
-        (priority) => {
-          return priority.value === row.getValue('priority')
-        }
-      )
-      if (!priority) {
-        return null
-      }
-
-      return (
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger>
-              {priority.icon && (
-                <priority.icon className="mr-2 h-6 w-6 text-muted-foreground" color={priority.color} />
-              )}
-            </TooltipTrigger>
-            <TooltipContent>
-              <p>{priority.label}</p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      )
-    },
-    filterFn: (row, id, value) => {
-      return value.includes(row.getValue(id))
-    }
-  },
-  {
     id: 'assignees',
     accessorFn: (data) => data._source['document.meta.core_assignment.rel.assignee.name'],
-    header: ({ column }) => (<ColumnHeader column={column} title="Assignees" />),
     cell: ({ row }) => {
       const assignees = row.getValue<string[]>('assignees') || []
       return (
-        <div className={cn('flex -space-x-2 w-fit text-sm font-semibold leading-6',
-          assignees.length > 3 && 'border-2 rounded-full')}>
+        <div className={cn('flex -space-x-2 w-fit text-sm font-semibold leading-6 h-8 items-center',
+          assignees.length > 3 && 'border rounded-full hidden lg:flex')}>
           {(assignees || []).slice(0, 3).map((assignee: string, index: number) => {
             const [first, last] = assignee.trim().split(' ')
             const initials = `${first[0]}${last[0]}`
@@ -115,7 +108,7 @@ export const columns: Array<ColumnDef<Planning>> = [
               <TooltipProvider key={index}>
                 <Tooltip>
                   <TooltipTrigger>
-                    <div className='w-8 h-8 rounded-full flex items-center stroke-slate-100 justify-center bg-[#973C9F] text-background dark:text-foreground border-2'>
+                    <div className='hidden lg:flex w-8 h-8 rounded-full items-center justify-center bg-[#973C9F] text-background dark:text-foreground border relative z-1'>
                       {initials}
                     </div>
                   </TooltipTrigger>
@@ -137,7 +130,7 @@ export const columns: Array<ColumnDef<Planning>> = [
                   const initials = `${first[0]}${last[0]}`
                   return (
                     <div key={index} className='flex p-1'>
-                      <div className='w-8 h-8 rounded-full flex items-center stroke-slate-100 justify-center bg-[#973C9F] text-background dark:text-foreground border-2 mx-4'>
+                      <div className='w-8 h-8 rounded-full flex items-center justify-center bg-[#973C9F] text-background dark:text-foreground border-2 mr-4'>
                         {initials}
                       </div>
                       <p>{assignee}</p>
@@ -154,9 +147,6 @@ export const columns: Array<ColumnDef<Planning>> = [
   {
     id: 'assignmentType',
     accessorFn: (data) => data._source['document.meta.core_assignment.meta.core_assignment_type.value'],
-    header: ({ column }) => (
-      <ColumnHeader column={column} title="Type" />
-    ),
     cell: ({ row }) => {
       const data = assignmentTypes.filter(
         (assignmentType) => row.getValue<string[]>('assignmentType').includes(assignmentType.value)
@@ -166,13 +156,13 @@ export const columns: Array<ColumnDef<Planning>> = [
       }
 
       return (
-        <div className="flex items-center">
+        <div className='flex items-center'>
           {data.map((item, index) => {
             return item.icon && (
               <TooltipProvider key={index}>
                 <Tooltip>
                   <TooltipTrigger>
-                    <item.icon className="mr-2 h-5 w-5 text-muted-foreground" color='#818FB4' />
+                    <item.icon className='mr-2 h-5 w-5 text-muted-foreground' color='#818FB4' />
                   </TooltipTrigger>
                   <TooltipContent>
                     <p>{item.label}</p>
