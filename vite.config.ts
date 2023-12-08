fdb1251305b93e1de7c2ac9ba307fceb1a3eb5d2
Gustav Larsson <gularsson@users.noreply.github.com>
2023-12-08 12:17:04 +0100

    fix: fonts sizes and weights (#33)
    * fix: fonts sizes and weights
    
    * fingers crossed
    
    * remove post-url
    
    * chore: remove unused dep

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './node_modules/@ttab/elephant-ui/dist/styles/**/*',
+          dest: './'
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
