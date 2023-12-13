26f8a730aaafda3e2d21c220c65be6c7c292d92e
Danne Lundqvist <danne@dotvoid.com>
2023-12-13 17:23:49 +0100

    List navigation using up/down arrows boilerplate

--- src/components/PlanningTable/index.tsx
+++ src/components/PlanningTable/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   type ColumnDef,
   type ColumnFiltersState,
@@ -18,14 +18,14 @@ import { Table, TableBody, TableCell, TableRow } from '@ttab/elephant-ui'
 import { Pagination } from './Pagination'
 import { Toolbar } from './Toolbar'
 
-
 interface PlanningTableProps<TData, TValue> {
   columns: Array<ColumnDef<TData, TValue>>
   data: TData[]
+  onRowSelected?: (row?: TData) => void
 }
 
 
-export const PlanningTable = <TData, TValue>({ columns, data }: PlanningTableProps<TData, TValue>): JSX.Element => {
+export const PlanningTable = <TData, TValue>({ columns, data, onRowSelected }: PlanningTableProps<TData, TValue>): JSX.Element => {
   const [rowSelection, setRowSelection] = useState({})
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
@@ -41,6 +41,7 @@ export const PlanningTable = <TData, TValue>({ columns, data }: PlanningTablePro
       columnFilters
     },
     enableRowSelection: true,
+    enableMultiRowSelection: false,
     onRowSelectionChange: setRowSelection,
     onSortingChange: setSorting,
     onColumnFiltersChange: setColumnFilters,
@@ -52,6 +53,55 @@ export const PlanningTable = <TData, TValue>({ columns, data }: PlanningTablePro
     getFacetedRowModel: getFacetedRowModel(),
     getFacetedUniqueValues: getFacetedUniqueValues()
   })
+
+  // Handle navigation using arrow keys
+  useEffect(() => {
+    if (!onRowSelected) {
+      return
+    }
+
+    const keyDownHandler = (evt: KeyboardEvent): void => {
+      if (!table || !['ArrowDown', 'ArrowUp', 'Escape'].includes(evt.key)) {
+        return
+      }
+
+      evt.preventDefault()
+      const rows = table.getRowModel().rows
+      if (!rows?.length) {
+        return
+      }
+
+      const selectedRows = table.getSelectedRowModel()
+      const selectedRow = selectedRows?.rows[0]
+
+      if (evt.key === 'Escape') {
+        if (selectedRow) {
+          selectedRow.toggleSelected(false)
+        }
+      } else if (!selectedRow) {
+        const idx = evt.key === 'ArrowDown' ? 0 : rows.length - 1
+        rows[idx].toggleSelected(true)
+      } else {
+        const nextIdx = selectedRow.index + ((evt.key === 'ArrowDown') ? 1 : -1)
+        const idx = nextIdx < 0 ? rows.length - 1 : nextIdx >= rows.length ? 0 : nextIdx
+        rows[idx].toggleSelected(true)
+      }
+    }
+
+    document.addEventListener('keydown', keyDownHandler)
+
+    return () => document.removeEventListener('keydown', keyDownHandler)
+  }, [table, onRowSelected])
+
+
+  // When row selection changes, report back to callback
+  useEffect(() => {
+    if (onRowSelected) {
+      const selectedRows = table.getSelectedRowModel()
+      onRowSelected(selectedRows?.rows[0]?.original)
+    }
+  }, [table, rowSelection, onRowSelected])
+
 
   const TableBodyElement = (): React.ReactNode => {
     if (table.getRowModel().rows?.length === 0) {
@@ -71,6 +121,14 @@ export const PlanningTable = <TData, TValue>({ columns, data }: PlanningTablePro
       <TableRow
         key={row.id}
         data-state={row.getIsSelected() && 'selected'}
+        onClick={(event) => {
+          if (!onRowSelected) {
+            return
+          }
+
+          event.preventDefault()
+          row.toggleSelected(!row.getIsSelected())
+        }}
       >
         {row.getVisibleCells().map((cell) => (
           <TableCell key={cell.id}>
