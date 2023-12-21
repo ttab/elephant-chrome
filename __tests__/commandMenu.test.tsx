08acc106aa1b1cb417a366a51f2df89fb6167127
Danne Lundqvist <danne@dotvoid.com>
2023-12-21 15:44:22 +0100

    Amended tests with correct mocks for navigation

--- __tests__/commandMenu.test.tsx
+++ __tests__/commandMenu.test.tsx
@@ -3,12 +3,28 @@ import { NavigationProvider } from '@/navigation'
 import { render, screen } from '../setupTests'
 import { CommandMenu } from '@/components/CommandMenu'
 import userEvent from '@testing-library/user-event'
+import { initializeNavigationState } from '@/navigation/lib/initializeNavigationState'
+import { type NavigationActionType } from '@/types'
+import { type Dispatch } from 'react'
+import { useNavigation } from '@/hooks'
 
 global.ResizeObserver = jest.fn().mockImplementation(() => ({
   observe: jest.fn(),
   unobserve: jest.fn(),
   disconnect: jest.fn()
 }))
+
+jest.mock('@/navigation/hooks/useNavigation', () => ({
+  useNavigation: jest.fn()
+}))
+const mockState = initializeNavigationState()
+
+const mockDispatch = jest.fn() as Dispatch<NavigationActionType>
+
+(useNavigation as jest.Mock).mockReturnValue({
+  state: mockState,
+  dispatch: mockDispatch
+})
 
 describe('CommandMenu', () => {
   it('should render CommandMenu component', async () => {
--- __tests__/link.test.tsx
+++ __tests__/link.test.tsx
@@ -4,6 +4,22 @@ import userEvent from '@testing-library/user-event'
 
 import { render, screen } from '../setupTests'
 import { Link } from '@/components'
+import { initializeNavigationState } from '@/navigation/lib/initializeNavigationState'
+import { type NavigationActionType } from '@/types'
+import { type Dispatch } from 'react'
+import { useNavigation } from '@/hooks'
+
+jest.mock('@/navigation/hooks/useNavigation', () => ({
+  useNavigation: jest.fn()
+}))
+const mockState = initializeNavigationState()
+
+const mockDispatch = jest.fn() as Dispatch<NavigationActionType>
+
+(useNavigation as jest.Mock).mockReturnValue({
+  state: mockState,
+  dispatch: mockDispatch
+})
 
 describe('Link', () => {
   it('should render Link component', async () => {
