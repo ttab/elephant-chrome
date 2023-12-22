e6b2d8501a61a6716416c5e33d628efc005e48a7
Gustav Larsson <gularsson@users.noreply.github.com>
2023-12-22 16:44:07 +0100

    fix: use sheet close button (#61)
    * fix: use sheet close button
    
    * fix: bump elephant-ui @ v0.1.1

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -3,7 +3,7 @@ import { Avatar } from './Avatar'
 
 export const AppHeader = (): JSX.Element => {
   return (
-    <header className='justify-end flex border-b pb-3 h-max'>
+    <header className='justify-end flex border-b pb-[11px] h-max'>
       <Avatar />
       <Menu />
     </header>
