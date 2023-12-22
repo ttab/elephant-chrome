56fbfa09211d49c30bd760e81ca7f381cea0b542
Danne Lundqvist <danne@dotvoid.com>
2023-12-22 14:07:41 +0100

    Feature/ele 281 enable keyboard navigation in planning list view using (#58)
    * Add keyboard nav to date selector <- and ->. Ignore arrow keys when an editable element is the target
    
    * Ignore arrow commands when editable element has focus

---
+++ src/lib/isEditableTarget.ts
@@ -0,0 +1,11 @@
+export function isEditableTarget(event: KeyboardEvent): boolean {
+  const target = event?.target as HTMLElement
+
+
+  return !target ||
+    target.isContentEditable ||
+    target.nodeName === 'INPUT' ||
+    target.nodeName === 'TEXTAREA' ||
+    target.nodeName === 'SELECT' ||
+    target.nodeName === 'OPTION'
+}
