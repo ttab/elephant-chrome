f95febbd9842d5898bb9862f6813b903c333a3aa
Gustav Larsson <gularsson@users.noreply.github.com>
2023-12-22 14:35:48 +0100

    fix: redis connection url type (#59)

--- src-srv/utils/hocuspocus.ts
+++ src-srv/utils/hocuspocus.ts
@@ -15,12 +15,21 @@ interface CreateServerOptions {
 
 export async function createServer(options: CreateServerOptions): Promise<Hocuspocus> {
   const { repository, cache } = options
+  const REDIS_URL = process.env.REDIS_URL
+
+  if (!REDIS_URL) {
+    throw new Error('Missing REDIS_URL')
+  }
 
   const server = Server.configure({
     port: process.env.PORT ? parseInt(process.env.PORT) : 5183,
     extensions: [
       new Logger(),
-      new Redis(process.env.REDIS_URL),
+      new Redis({
+        options: {
+          host: REDIS_URL
+        }
+      }),
       new Database({
         fetch: async (data) => {
           // Try fetching from cache
