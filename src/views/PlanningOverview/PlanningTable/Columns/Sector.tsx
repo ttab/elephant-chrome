5f91c218de415e326ace821289038e07cbbcea48
gvl <gustav.larsson@tt.se>
2024-01-18 10:32:29 +0100

    fix: move settings to @/defaults

--- src/views/PlanningOverview/PlanningTable/Columns/Sector.tsx
+++ src/views/PlanningOverview/PlanningTable/Columns/Sector.tsx
@@ -2,40 +2,12 @@ import { type ColumnDef } from '@tanstack/react-table'
 import { type Planning } from '../data/schema'
 import { SectorBadge } from '@/components/DataItem/SectorBadge'
 import { Shapes } from '@ttab/elephant-ui/icons'
-import { type ColumnValueOption } from '@/types'
-
-export const columnValueOptions: ColumnValueOption[] = [
-  {
-    value: 'Utrikes',
-    label: 'Utrikes',
-    color: 'bg-[#BD6E11]'
-  },
-  {
-    value: 'Inrikes',
-    label: 'Inrikes',
-    color: 'bg-[#DA90E1]'
-  },
-  {
-    value: 'Sport',
-    label: 'Sport',
-    color: 'bg-[#6CA8DF]'
-  },
-  {
-    value: 'Kultur och nöje',
-    label: 'Kultur/Nöje',
-    color: 'bg-[#12E1D4]'
-  },
-  {
-    value: 'Ekonomi',
-    label: 'Ekonomi',
-    color: 'bg-[#FFB9B9]'
-  }
-]
+import { Sectors } from '@/defaults'
 
 export const sector: ColumnDef<Planning> = {
   id: 'section',
   meta: {
-    options: columnValueOptions,
+    options: Sectors,
     filter: 'facet',
     name: 'Sector',
     columnIcon: Shapes,
