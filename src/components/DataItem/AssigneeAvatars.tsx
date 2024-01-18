084ab8721d1e34b191625c78feb58346dcf2a43f
Gustav Larsson <gularsson@users.noreply.github.com>
2024-01-18 10:28:44 +0100

    fix: condensed AssigneeAvatar hover, tooltip (#72)

--- src/components/DataItem/AssigneeAvatars.tsx
+++ src/components/DataItem/AssigneeAvatars.tsx
@@ -1,32 +1,8 @@
 import { getInitials } from '@/lib/getInitials'
 import {
-  TooltipProvider, Tooltip, TooltipTrigger, TooltipContent,
-  Popover, PopoverTrigger, PopoverContent
+  TooltipProvider, Tooltip, TooltipTrigger, TooltipContent
 } from '@ttab/elephant-ui'
 import { cn } from '@ttab/elephant-ui/utils'
-
-const AssigneePopover = ({ assignees }: { assignees: string[] }): JSX.Element => {
-  return assignees.length > 3
-    ? (
-      <Popover>
-        <PopoverTrigger>
-          <span className='font-normal px-3 pt-1'>{assignees.length > 3 && `+${assignees.length - 3}`}</span>
-        </PopoverTrigger>
-        <PopoverContent>
-          {assignees.map((assignee: string, index: number) => {
-            return (
-              <div key={index} className='flex p-1'>
-                <div className='w-8 h-8 rounded-full flex items-center justify-center border-2 mr-4'>
-                  {getInitials(assignee)}
-                </div>
-                <p>{assignee}</p>
-              </div>
-            )
-          })}
-        </PopoverContent>
-      </Popover>)
-    : <></>
-}
 
 export const AssigneeAvatars = ({ assignees }: { assignees: string[] }): JSX.Element => {
   return (
@@ -48,7 +24,26 @@ export const AssigneeAvatars = ({ assignees }: { assignees: string[] }): JSX.Ele
           </TooltipProvider>
         )
       })}
-      <AssigneePopover assignees={assignees} />
+      {assignees.length > 3 && (
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <span className='font-normal px-3 pt-1'>{assignees.length > 3 && `+${assignees.length - 3}`}</span>
+            </TooltipTrigger>
+            <TooltipContent>
+              {assignees.map((assignee, index) => {
+                return (
+                  <div key={index} className='flex p-1 text-xs font-semibold leading-6 items-center'>
+                    <div className='w-8 h-8 rounded-full flex items-center justify-center border-2 mr-4'>
+                      {getInitials(assignee)}
+                    </div>
+                    <p>{assignee}</p>
+                  </div>)
+              })}
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>)
+      }
     </div>
   )
 }
