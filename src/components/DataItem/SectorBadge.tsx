4cb4cecf088bbdaf00283beac86cc53646f369bd
gvl <gustav.larsson@tt.se>
2024-01-18 10:36:14 +0100

    fix: update settings SectorBadge

--- src/components/DataItem/SectorBadge.tsx
+++ src/components/DataItem/SectorBadge.tsx
@@ -1,9 +1,10 @@
+import { Sectors } from '@/defaults'
 import { Badge } from '@ttab/elephant-ui'
 import { cn } from '@ttab/elephant-ui/utils'
-import { columnValueOptions } from '@/views/PlanningOverview/PlanningTable/Columns/Sector'
+
 
 export const SectorBadge = ({ color, value }: { color?: string, value: string }): JSX.Element => {
-  const sector = columnValueOptions.find((label) => label.value === value)
+  const sector = Sectors.find((label) => label.value === value)
   const chosenColor = color || sector?.color
 
   return <Badge variant='outline' className='rounded-md'>
