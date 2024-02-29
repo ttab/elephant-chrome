0ea382de8aafef16b0d7b0363c880f1089c94948
Gustav Larsson <gularsson@users.noreply.github.com>
2024-02-29 12:27:29 +0100

    fix: use transact to bundle operations (#206)

--- src/views/Planning/components/PlanCategory.tsx
+++ src/views/Planning/components/PlanCategory.tsx
@@ -2,22 +2,24 @@ import { ComboBox } from '@/components/ui'
 import { Categories } from '@/defaults/categories'
 import { useYObserver } from '@/hooks'
 
-export const PlanCategory = (): JSX.Element => {
-  const { get, set } = useYObserver('planning', 'links.core/category[0]')
+export const PlanCategory = (): JSX.Element | undefined => {
+  const { get, set, loading } = useYObserver('planning', 'links.core/category[0]')
 
   const selectedOption = Categories.find(c => c.value === get('title'))
 
-  return <ComboBox
-    size='xs'
-    className='w-fit text-muted-foreground text-xs font-sans font-normal whitespace-nowrap text-ellipsis px-2'
-    options={Categories}
-    selectedOption={selectedOption}
-    placeholder={selectedOption?.label || 'Add Category'}
-    onSelect={(option) => {
-      if (option.value === selectedOption?.value) {
-        console.log('Should delete value')
-      }
-      set(option.payload)
-    }}
+  return !loading
+    ? <ComboBox
+        size='xs'
+        className='w-fit text-muted-foreground text-xs font-sans font-normal whitespace-nowrap text-ellipsis px-2'
+        options={Categories}
+        selectedOption={selectedOption}
+        placeholder={selectedOption?.label || 'Add Category'}
+        onSelect={(option) => {
+          if (option.value === selectedOption?.value) {
+            console.log('Should delete value')
+          }
+          set(option.payload)
+        }}
     />
+    : undefined
 }
