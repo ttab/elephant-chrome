fb5d9cad943a39b9af7df445870bcdd361c9c5ed
Gustav Larsson <gularsson@users.noreply.github.com>
2024-03-15 09:10:06 +0100

    fix: use combobox for assigmenttype (#251)
    * fix: use combobox for assigmenttype
    
    * fix: nullcheck

--- src/views/Planning/components/PlanSector.tsx
+++ src/views/Planning/components/PlanSector.tsx
@@ -1,11 +1,14 @@
 import { ComboBox } from '@/components/ui'
 import { Sectors } from '@/defaults'
 import { useYObserver } from '@/hooks'
+import { cn } from '@ttab/elephant-ui/utils'
 
 export const PlanSector = (): JSX.Element | undefined => {
   const { get, set, loading } = useYObserver('planning', 'links.tt/sector[0]')
 
   const selectedOption = Sectors.find(c => c.value === get('title'))
+
+  const placeholder = selectedOption?.label || 'Add Sector'
 
   return !loading
     ? <ComboBox
@@ -13,10 +16,13 @@ export const PlanSector = (): JSX.Element | undefined => {
         className='w-fit text-muted-foreground font-sans font-normal whitespace-nowrap text-ellipsis px-2 h-7'
         options={Sectors}
         selectedOption={selectedOption}
-        placeholder={selectedOption?.label || 'Add Sector'}
+        placeholder={placeholder}
         onSelect={(option) => {
           set(option.payload)
         }}
-      />
+      >
+      {selectedOption?.color && <div className={cn('h-2 w-2 rounded-full mr-2', selectedOption?.color)} /> }
+      {selectedOption ? <>{selectedOption.label}</> : <>{placeholder}</>}
+    </ComboBox>
     : undefined
 }
