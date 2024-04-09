50ffb54b25c3e5e750edba94f489f532e542dbb0
gvl <gustav.larsson@tt.se>
2024-04-09 18:53:28 +0200

    fix(transformation): transform all root keys

--- src-srv/utils/transformations/yjs/yPlanning.ts
+++ src-srv/utils/transformations/yjs/yPlanning.ts
@@ -5,15 +5,12 @@ import { groupBy } from '../lib/groupBy.js'
 
 export function newsDocToYPlanning(document: Document, planningYMap: Y.Map<unknown>): Y.Map<unknown> {
   try {
-    const root = {
-      title: document.title
-    }
-    const meta = toYMap(groupBy(document.meta, 'type'), new Y.Map())
-    const links = toYMap(groupBy(document.links, 'type'), new Y.Map())
+    const { meta, links, ...rest } = document
 
-    planningYMap.set('meta', meta)
-    planningYMap.set('links', links)
-    planningYMap.set('root', toYMap(root, new Y.Map()))
+    planningYMap.set('meta', toYMap(groupBy(document.meta, 'type'), new Y.Map()))
+    planningYMap.set('links', toYMap(groupBy(document.links, 'type'), new Y.Map()))
+    planningYMap.set('root', toYMap(rest, new Y.Map()))
+
     return planningYMap
   } catch (err) {
     if (err instanceof Error) {
