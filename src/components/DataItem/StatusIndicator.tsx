5f28b986e6f4a9f61bebeca5b56b1698c7811baa
Danne Lundqvist <danne@dotvoid.com>
2024-04-18 11:37:56 +0200

    Cleaned up grid view cards somewhat

--- src/components/DataItem/StatusIndicator.tsx
+++ src/components/DataItem/StatusIndicator.tsx
@@ -1,14 +1,18 @@
 import { Tooltip } from '@ttab/elephant-ui'
 import { Building, Globe } from '@ttab/elephant-ui/icons'
+import { cn } from '@ttab/elephant-ui/utils'
 
-export const StatusIndicator = ({ internal }: { internal: boolean }): JSX.Element => {
+export const StatusIndicator = ({ internal, className }: {
+  internal: boolean
+  className?: string
+}): JSX.Element => {
   return (
     <Tooltip content={internal ? 'Internal' : 'Public'}>
-      <div className='items-center'>
+      <div className={cn('items-center', className)}>
         {internal
           ? <Building size={18} strokeWidth={1.75} className='text-muted-foreground' />
           : <Globe size={18} strokeWidth={1.75} className='text-muted-foreground' />
-            }
+        }
       </div>
 
     </Tooltip>
