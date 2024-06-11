6241bfceddcf8facd7d3cd6e4d987ffc45789f73
Danne Lundqvist <danne@dotvoid.com>
2024-06-11 14:36:00 +0200

    The whole signout menu option should be a link. Moved logged in user info to bottom to not be in the way

--- src/components/App/Menu.tsx
+++ src/components/App/Menu.tsx
@@ -20,40 +20,29 @@ export const Menu = (): JSX.Element => {
         <MenuIcon strokeWidth={2.25} size={18} />
       </SheetTrigger>
 
-      <SheetContent side="left" className="w-100vw h-100vh p-0" defaultClose={false}>
+      <SheetContent side="left" className="w-100vw h-100vh p-0 flex flex-col justify-between" defaultClose={false}>
+        <div>
+          <SheetHeader>
+            <SheetTitle className="flex flex-row gap-4 justify-between justify-items-center items-center h-14 px-4">
+              <SheetClose className="rounded-md hover:bg-gray-100 w-9 h-9 flex items-center justify-center">
+                <X strokeWidth={2.25} />
+              </SheetClose>
 
-        <SheetHeader>
-          <SheetTitle className="flex flex-row gap-4 justify-between justify-items-center items-center h-14 px-4 border-b">
-            <SheetClose className="rounded-md hover:bg-gray-100 w-9 h-9 flex items-center justify-center">
-              <X strokeWidth={2.25} />
-            </SheetClose>
+              <SheetClose asChild>
+                <Link to='PlanningOverview' className="leading-9 px-3 rounded-md">
+                  Elefanten
+                  <span className="text-2xl pl-2">🐘</span>
+                </Link>
+              </SheetClose>
 
+              <ThemeSwitcher />
+            </SheetTitle>
+          </SheetHeader>
+
+
+          <div className="px-3 py-4 border-t">
             <SheetClose asChild>
-              <Link to='PlanningOverview' className="leading-9 px-3 rounded-md">
-                Elefanten
-                <span className="text-2xl pl-2">🐘</span>
-              </Link>
-            </SheetClose>
-
-            <ThemeSwitcher />
-          </SheetTitle>
-        </SheetHeader>
-
-        <div className="">
-          <div className="flex flex-col items-center justify-center rounded-md bg-gray-100 dark:bg-gray-800 mb-10">
-            <div className="p-2 py-4 leading-loose text-center">
-              <div className="font-bold">{data?.user.name || '(Namn saknas)'}</div>
-              <div className="text-xs opacity-60">{''}</div>
-            </div>
-            <div className="border-4 border-background rounded-full -mb-7">
-              <Avatar user={data?.user} size="xl" variant="color" />
-            </div>
-          </div>
-
-          <div className="px-3 py-4 border-y">
-
-            <SheetClose asChild>
-              <Link to='PlanningOverview' className="flex gap-3 items-center px-3 py-2 rounded-md  hover:bg-gray-100">
+              <Link to='PlanningOverview' className="flex gap-3 items-center px-3 py-2 rounded-md hover:bg-gray-100">
                 <div className="flex items-center justify-center opacity-80 pr-2">
                   <CalendarDaysIcon strokeWidth={1.75} size={18} />
                 </div>
@@ -61,19 +50,29 @@ export const Menu = (): JSX.Element => {
               </Link>
             </SheetClose>
 
-
             <SheetClose asChild>
-              <div className="flex gap-3 items-center px-3 py-2 rounded-md  hover:bg-gray-100">
-                <div className="flex items-center justify-center opacity-80 pr-2">
-                  <LogOut strokeWidth={1.75} size={18} />
+              <a href={`${import.meta.env.BASE_URL}/api/auth/signout`}>
+                <div className="flex gap-3 items-center px-3 py-2 rounded-md hover:bg-gray-100">
+                  <div className="flex items-center justify-center opacity-80 pr-2">
+                    <LogOut strokeWidth={1.75} size={18} />
+                  </div>
+                  <div>Logga ut</div>
                 </div>
-                <div>
-                  <a href={`${import.meta.env.BASE_URL}/api/auth/signout`}>Logga ut</a>
-                </div>
-              </div>
+              </a>
             </SheetClose>
-
           </div>
+        </div>
+
+        <div className="justify-self-end flex flex-col items-center justify-center rounded-md bg-gray-100 dark:bg-gray-800 mt-10">
+          <div className="border-4 border-background rounded-full -mt-7">
+            <Avatar user={data?.user} size="xl" variant="color" />
+          </div>
+
+          <div className="p-2 py-4 pb-10 leading-loose text-center">
+            <div className="font-bold">{data?.user.name || '(Namn saknas)'}</div>
+            <div className="text-xs opacity-60">{''}</div>
+          </div>
+
         </div>
       </SheetContent>
     </Sheet>
