c4480a120d937fe439100b35e9189bda8e38c15d
gvl <gustav.larsson@tt.se>
2024-06-27 14:56:34 +0200

    test: add useYValue test

---
+++ __tests__/utils/newHocuspocusProvider.ts
@@ -0,0 +1,26 @@
+import {
+  HocuspocusProvider,
+  type HocuspocusProviderConfiguration, type HocuspocusProviderWebsocketConfiguration
+} from '@hocuspocus/provider'
+import { type Hocuspocus } from '@hocuspocus/server'
+import { newHocuspocusProviderWebsocket } from './newHocuspocusProviderWebsocket.js'
+
+export const newHocuspocusProvider = (
+  server: Hocuspocus,
+  document: any,
+  options: Partial<HocuspocusProviderConfiguration> = {},
+  websocketOptions: Partial<HocuspocusProviderWebsocketConfiguration> = {}
+): HocuspocusProvider => {
+  return new HocuspocusProvider({
+    websocketProvider: newHocuspocusProviderWebsocket(server, websocketOptions),
+    document,
+    // Just use a generic document name for all tests.
+    name: 'hocuspocus-test',
+    // There is no need to share data with other browser tabs in the testing environment.
+    broadcast: false,
+    // We don’t need console logging in tests. If we actually do, we can overwrite it anyway.
+    quiet: true,
+    // Add or overwrite settings, depending on the test case.
+    ...options
+  })
+}
