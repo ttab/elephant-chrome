46a135ef0f49d2fc511809b27d95e8fafa5c33d7
Danne Lundqvist <danne@dotvoid.com>
2024-08-13 14:04:48 +0200

    Removed unused files, replaced with generic get()

--- src/lib/index/authors.ts
+++
@@ -1,9 +0,0 @@
-import { type IndexedAuthor } from './schemas'
-import { type SearchIndexResponse } from './searchIndex'
-import { get, type SearchParams } from './get'
-
-export const Authors = {
-  get: async (endpoint: URL, accessToken: string, params?: SearchParams): Promise<SearchIndexResponse<IndexedAuthor>> => {
-    return await get<IndexedAuthor>(endpoint, accessToken, 'core_author', params)
-  }
-}
--- src/lib/index/stories.ts
+++
@@ -1,9 +0,0 @@
-import { type IndexedStory } from './schemas'
-import { type SearchIndexResponse } from './searchIndex'
-import { get, type SearchParams } from './get'
-
-export const Stories = {
-  get: async (endpoint: URL, accessToken: string, params?: SearchParams): Promise<SearchIndexResponse<IndexedStory>> => {
-    return await get<IndexedStory>(endpoint, accessToken, 'core_story', params)
-  }
-}
