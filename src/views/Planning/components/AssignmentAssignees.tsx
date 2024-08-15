48b336b2aa7d55c0961e251858ce019f4d4c3a19
gvl <gustav.larsson@tt.se>
2024-08-15 10:47:16 +0200

    fix: updated useYValue usage

--- src/views/Planning/components/AssignmentAssignees.tsx
+++ src/views/Planning/components/AssignmentAssignees.tsx
@@ -2,46 +2,30 @@ import { Awareness } from '@/components'
 import { ComboBox } from '@/components/ui'
 import { useAuthors } from '@/hooks/useAuthors'
 import { useYValue } from '@/hooks/useYValue'
-import { isYArray } from '@/lib/isType'
 import { Block } from '@/protos/service'
 import { UserPlus } from '@ttab/elephant-ui/icons'
-import { useCallback, useEffect, useRef, useState } from 'react'
-import type * as Y from 'yjs'
+import { useRef } from 'react'
 
 import { AssigneeAvatars } from '@/components/DataItem/AssigneeAvatars'
-
-interface IAssignee {
-  value: string
-  label: string
-}
 
 export const Assignees = ({ path }: {
   path: string
 }): JSX.Element | undefined => {
-  const authors = useAuthors({ sort: 'title' })
-  const [assignees] = useYValue<Y.Array<unknown>>(path, {
-    createOnEmpty: {
-      path,
-      data: []
+  const allAuthors = useAuthors().map((_) => {
+    return {
+      value: _.id,
+      label: _.title
     }
   })
-  const [selectedOptions, setSelectedOptions] = useState<IAssignee[]>([])
 
-  const updateSelectedOptions = useCallback((assignees: Y.Array<unknown>) => {
-    setSelectedOptions((assignees?.toJSON() || [])?.map(a => {
-      return {
-        value: a.uuid,
-        label: a.name
-      }
-    })
-    )
-  }, [])
+  const [assignees, setAssignees] = useYValue<Block[]>(path)
 
-  useEffect(() => {
-    if (isYArray(assignees)) {
-      updateSelectedOptions(assignees)
+  const selectedOptions = ((assignees || [])?.map(a => {
+    return {
+      value: a.uuid,
+      label: a.name
     }
-  }, [updateSelectedOptions, assignees])
+  }))
 
   const setFocused = useRef<(value: boolean) => void>(null)
   const placeholder = 'Lägg till uppdragstagare'
@@ -52,12 +36,7 @@ export const Assignees = ({ path }: {
         <ComboBox
           size='xs'
           className='w-fit text-muted-foreground font-sans font-normal text-ellipsis px-2 h-7'
-          options={authors.map(_ => {
-            return {
-              value: _.id,
-              label: _.title
-            }
-          })}
+          options={allAuthors}
           selectedOption={selectedOptions}
           placeholder={placeholder}
           closeOnSelect={false}
@@ -67,24 +46,20 @@ export const Assignees = ({ path }: {
             }
           }}
           onSelect={(option) => {
-            if (!isYArray(assignees)) {
-              return
-            }
-
             const selectedAuthor = selectedOptions.findIndex(sa => sa.value === option.value)
+
             if (selectedAuthor > -1) {
-              assignees.delete(selectedAuthor)
+              setAssignees((assignees || []).filter(a => a.uuid !== option.value))
             } else {
-              assignees.push([Block.create({
+              setAssignees([...(assignees || []), Block.create({
                 type: 'core/author',
                 uuid: option.value,
                 name: option.label,
                 rel: 'assignee',
                 role: 'primary'
-              })])
+              })
+              ])
             }
-
-            updateSelectedOptions(assignees)
           }}
         >
           <UserPlus size={20} strokeWidth={1.75} />
@@ -97,7 +72,6 @@ export const Assignees = ({ path }: {
           size='xs'
         />
       </div>
-
     </div>
   )
 }
