cff8f41188e5bb2defa4b69c1b7cd2978249ad0c
gvl <gustav.larsson@tt.se>
2024-08-19 13:53:35 +0200

    fix: add faceted unique values to assignee filter

--- src/views/PlanningOverview/PlanningListColumns.tsx
+++ src/views/PlanningOverview/PlanningListColumns.tsx
@@ -149,8 +149,7 @@ export function planningTableColumns({ sections = [], authors = [] }: {
       meta: {
         options: authors.map((_) => ({ value: _.title, label: _.title })),
         Filter: ({ column, setSearch }) => (
-          // TODO: Add facetFn for arrays
-          <FacetedFilter column={column} setSearch={setSearch} />
+          <FacetedFilter column={column} setSearch={setSearch} facetFn={() => getNestedFacetedUniqueValues(column)} />
         ),
         name: 'Uppdragstagare',
         columnIcon: Users,
