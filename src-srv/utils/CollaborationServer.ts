c13cb6d0918af525b9aaef8670a69aa4ae179eff
gvl <gustav.larsson@tt.se>
2024-09-12 22:28:17 +0200

    fix: event stateless accessToken

--- src-srv/utils/CollaborationServer.ts
+++ src-srv/utils/CollaborationServer.ts
@@ -106,7 +106,7 @@ export class CollaborationServer {
           store: async (payload) => { await this.#storeDocument(payload) }
         }),
         new Snapshot({
-          debounce: 120000,
+          debounce: 5000,
           snapshot: async (payload: onStoreDocumentPayload) => {
             return async () => {
               await this.#snapshotDocument(payload)
