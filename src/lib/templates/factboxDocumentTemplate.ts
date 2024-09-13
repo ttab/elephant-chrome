227bf5ec31294da3845693ae771b9959c515bd91
Victor Lindh <victorlindh@hotmail.com>
2024-09-13 14:08:55 +0200

    add new factbox document template
    export factbox document template

---
+++ src/lib/templates/factboxDocumentTemplate.ts
@@ -0,0 +1,24 @@
+import { Block, Document } from '@/protos/service'
+
+/**
+* Create a template for a factbox document
+* @returns Document
+*/
+
+export function factboxDocumentTemplate(id: string): Document {
+  return Document.create({
+    uuid: id,
+    type: 'core/factbox',
+    uri: `core://factbox/${id}`,
+    language: 'sv-se',
+    title: 'Rubrik',
+    content: [
+      Block.create({
+        type: 'core/text',
+        data: {
+          text: ''
+        }
+      })
+    ]
+  })
+}
