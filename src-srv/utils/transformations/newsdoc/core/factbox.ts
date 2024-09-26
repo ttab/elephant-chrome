8f7cb0eca4461279c6e835798f9eea6ceedaab38
Gustav Larsson <gularsson@users.noreply.github.com>
2024-09-26 14:21:28 +0200

    fix: parse and validate factboxes from repo (#732)
    * fix: parse and validate factboxes from repo
    
    * fix: imports and data

--- src-srv/utils/transformations/newsdoc/core/factbox.ts
+++ src-srv/utils/transformations/newsdoc/core/factbox.ts
@@ -1,53 +1,54 @@
-import { decode } from 'html-entities'
 import { Block } from '@ttab/elephant-api/newsdoc'
 import { type TBElement } from '@ttab/textbit'
+import { newsDocToSlate, slateToNewsDoc } from '../index.js'
 
 export const transformFactbox = (element: Block): TBElement => {
-  const { id, data } = element
+  const { id, data, title, content } = element
   return {
     id: id || crypto.randomUUID(),
     class: 'block',
     type: 'core/factbox',
     properties: {
-      modified: data.modified,
-      title: data.title,
-      text: data.text
+      byline: data.byline
     },
     children: [
       {
+        id: crypto.randomUUID(),
+        class: 'text',
         type: 'core/factbox/title',
-        children: [{ text: decode(data.title) ?? '' }]
+        children: [
+          {
+            text: title
+          }
+        ]
       },
-      {
-        type: 'core/factbox/text',
-        children: [{ text: decode(data.text) ?? '' }]
-      }
+      ...newsDocToSlate(content)
     ]
   }
 }
 
-export function revertFactbox(element: TBElement): Block {
+interface FactboxChild {
+  text?: string
+}
+
+export async function revertFactbox(element: TBElement): Promise<Block> {
   function toString(value: string | number | boolean | undefined): string {
     return (value ?? '').toString()
   }
 
+  const factboxTitle = element.children.find((child) => child.type === 'core/factbox/title')
+  const title = (factboxTitle?.children as FactboxChild[] | undefined)?.[0]?.text ?? ''
+
+  const factboxChildren = element.children.filter((child) => child.type !== 'core/factbox/title')
+
+
   const { id, properties } = element
+  const byline = { byline: toString(properties?.byline) }
   return Block.create({
     id,
     type: 'core/factbox',
-    data: {
-      modified: toString(properties?.modified),
-      title: toString(properties?.title),
-      text: toString(properties?.text)
-    },
-    links: [
-      {
-        data: {
-          modified: toString(properties?.modified),
-          title: toString(properties?.title),
-          text: toString(properties?.text)
-        }
-      }
-    ]
+    title: toString(title),
+    data: properties?.byline ? byline : {},
+    content: await slateToNewsDoc(factboxChildren as TBElement[])
   })
 }
