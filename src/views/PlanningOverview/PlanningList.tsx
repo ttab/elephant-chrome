f031baa555416219cd7c5790d05a9a14e3046579
gvl <gustav.larsson@tt.se>
2024-09-26 12:43:24 +0200

    fix: memoize PlanningList

--- src/views/PlanningOverview/PlanningList.tsx
+++ src/views/PlanningOverview/PlanningList.tsx
@@ -1,7 +1,8 @@
-import { useMemo } from 'react'
+import { type Dispatch, useCallback, useMemo } from 'react'
 import useSWR from 'swr'
 import { useIndexUrl, useTable, useSections, useRepositoryEvents } from '@/hooks'
 import { useSession } from 'next-auth/react'
+import type { Session } from 'next-auth'
 import {
   type SearchIndexResponse,
   type Planning,
@@ -12,13 +13,70 @@ import { Table } from '@/components/Table'
 import { planningTableColumns } from '@/views/PlanningOverview/PlanningListColumns'
 import { convertToISOStringInUTC, getDateTimeBoundaries } from '@/lib/datetime'
 
+const getCurrentDocumentStatus = (obj: Planning): string => {
+  const item: Record<string, null | string[]> = obj._source
+  const defaultStatus = 'draft'
+
+  const createdValues = Object.keys(item)
+    .filter(key => key.startsWith('heads.') && key.includes('.created'))
+    .map(key => {
+      const newkey = key.replace('heads.', '').replace('.created', '')
+      const dateCreated = item[key] ? item[key][0] : null
+      return { status: newkey, created: dateCreated }
+    })
+
+  createdValues.sort((a, b) => {
+    if (a.created === null) return 1
+    if (b.created === null) return -1
+    return a.created > b.created ? -1 : 1
+  })
+
+  return createdValues[0]?.status || defaultStatus
+}
+
+const fetchPlannings = async (
+  setData: Dispatch<SearchIndexResponse<Planning>>,
+  indexUrl: URL,
+  session: Session | null,
+  date: Date
+): Promise<SearchIndexResponse<Planning> | undefined> => {
+  if (!session) return undefined
+
+  const { startTime, endTime } = getDateTimeBoundaries(date)
+  const result = await Plannings.search(indexUrl, session.accessToken, {
+    size: 100,
+    where: {
+      start: convertToISOStringInUTC(startTime),
+      end: convertToISOStringInUTC(endTime)
+    }
+  })
+
+  if (result.ok) {
+    const planningsWithStatus = {
+      ...result,
+      hits: result?.hits?.map((planningItem: Planning) => {
+        const status = getCurrentDocumentStatus(planningItem)
+        planningItem._source = {
+          ...planningItem._source,
+          'document.meta.status': [status]
+        }
+        return planningItem
+      })
+    }
+    setData(planningsWithStatus)
+    return planningsWithStatus
+  }
+}
+
 export const PlanningList = ({ date }: { date: Date }): JSX.Element => {
   const { setData } = useTable<Planning>()
-  const { data: session, status } = useSession()
+  const { data: session } = useSession()
 
   const sections = useSections()
   const indexUrl = useIndexUrl()
   const { startTime, endTime } = getDateTimeBoundaries(date)
+
+  const columns = useMemo(() => planningTableColumns({ sections }), [sections])
 
   // Create url to base SWR caching on
   const searchUrl = useMemo(() => {
@@ -34,79 +92,45 @@ export const PlanningList = ({ date }: { date: Date }): JSX.Element => {
     return searchUrl
   }, [startTime, endTime, indexUrl])
 
-  const { data, mutate } = useSWR(searchUrl?.href, async (): Promise<SearchIndexResponse<Planning> | undefined> => {
-    if (status !== 'authenticated' || !indexUrl) {
-      return
-    }
-
-    const { startTime, endTime } = getDateTimeBoundaries(date)
-    const result = await Plannings.search(indexUrl, session.accessToken, {
-      size: 100,
-      where: {
-        start: convertToISOStringInUTC(startTime),
-        end: convertToISOStringInUTC(endTime)
-      }
+  const { data, mutate } = useSWR(
+    searchUrl?.href, async () =>
+      await fetchPlannings(setData, indexUrl, session, date), {
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false
     })
-    if (result.ok) {
-      const getCurrentDocumentStatus = (obj: Planning): string => {
-        const item: Record<string, null | string[]> = obj._source
-        const defaultStatus = 'draft'
-        const createdValues = []
-        for (const key in item) {
-          if (Array.isArray(item[key])) {
-            if (Object.prototype.hasOwnProperty.call(item, key) && key.startsWith('heads.')) {
-              let newkey = key.split('heads.')[1]
-              if (newkey.includes('.created')) {
-                newkey = newkey.replace('.created', '')
-                const [dateCreated] = item[key]
-                createdValues.push({ status: newkey, created: dateCreated })
-              }
-            }
-          }
-        }
-        createdValues.sort((a, b) => a?.created > b?.created ? -1 : 1)
-        return createdValues[0]?.status || defaultStatus
-      }
-      const planningsWithStatus = {
-        ...result,
-        hits: result?.hits?.map((planningItem: Planning) => {
-          const status = getCurrentDocumentStatus(planningItem)
-          planningItem._source = Object.assign({}, planningItem._source, {
-            'document.meta.status': [status]
-          })
-          return planningItem
-        })
-      }
-      setData(planningsWithStatus)
-      return planningsWithStatus
-    }
-  })
 
   // FIXME: This should be supported by the useRepositoryEvents hook
   // but isn't working right now. The message goes only to the leader tab
-  useRepositoryEvents('core/planning-item', () => {
-    void (async () => {
-      try {
-        await mutate()
-      } catch (error) {
-        console.error('Error when mutating Planning list', error)
-      }
-    })()
-  })
+  useRepositoryEvents(
+    'core/planning-item',
+    useCallback(() => {
+      void (async () => {
+        try {
+          await mutate()
+        } catch (error) {
+          console.error('Error when mutating Planning list', error)
+        }
+      })()
+    }, [mutate])
+  )
+
+  const onRowSelected = useCallback((row?: Planning) => {
+    if (row) {
+      console.info(`Selected planning item ${row._id}`)
+    } else {
+      console.info('Deselected row')
+    }
+    return row
+  }, [])
+
 
   return (
     <>
-      {data?.ok === true &&
+      {data?.ok &&
         <Table
           type='Planning'
-          columns={planningTableColumns({ sections })}
-          onRowSelected={(row): void => {
-            if (row) {
-              console.info(`Selected planning item ${row._id}`)
-            } else {
-              console.info('Deselected row')
-            }
-          }}
+          columns={columns}
+          onRowSelected={onRowSelected}
         />
       }
     </>
