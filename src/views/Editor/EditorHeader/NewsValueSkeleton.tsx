1a7abddfd7a0ba4c80e37c724a63a86758430d49
gvl <gustav.larsson@tt.se>
2024-10-11 14:46:13 +0200

    fix: remove NewsValueTimeDropDown

--- src/views/Editor/EditorHeader/NewsValueSkeleton.tsx
+++
@@ -1,9 +0,0 @@
-import { Skeleton } from '@ttab/elephant-ui'
-export const NewsValueSkeleton = (): JSX.Element => {
-  return (
-    <div>
-      <Skeleton className='h-5 w-5 space-y-6' />
-    </div>
-  )
-}
-
