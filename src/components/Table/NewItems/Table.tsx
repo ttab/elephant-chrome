ad1b1f6c786f91ed45c044867e1cbbd4eb9285c4
Gustav Larsson <gularsson@users.noreply.github.com>
2024-10-16 08:48:08 +0200

    fix: use import.meta (#799)

--- src/components/Table/NewItems/Table.tsx
+++ src/components/Table/NewItems/Table.tsx
@@ -14,7 +14,7 @@ import { type View } from '@/types/index'
 import { useRepositoryEvents } from '@/hooks/useRepositoryEvents'
 import { useCallback } from 'react'
 
-const BASE_URL = process.env.BASE_URL || ''
+const BASE_URL = import.meta.env.BASE_URL || ''
 
 export const Table = ({ type, header }: {
   type: View
