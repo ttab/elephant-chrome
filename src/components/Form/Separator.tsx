225aff98416a1439b8143962636d29479e61b5f4
gvl <gustav.larsson@tt.se>
2024-10-24 15:44:24 +0200

    fix: remove unused Form.Separator

--- src/components/Form/Separator.tsx
+++
@@ -1,7 +0,0 @@
-import { Separator as _Separator } from '@ttab/elephant-ui'
-
-export const Separator = (): JSX.Element => (
-  <div className='pb-2'>
-    <_Separator className='absolute left-0 right-0 w-full' />
-  </div>
-)
