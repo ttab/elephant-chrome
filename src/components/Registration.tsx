2c87ccc1be6a36ca1de4a965d0a2c57d7a0c5b2d
gvl <gustav.larsson@tt.se>
2024-10-28 12:00:26 +0100

    fix: margin Registration Textbox

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -5,7 +5,7 @@ export const Registration = (): JSX.Element => {
   return (
     <TextBox
       path='meta.core/event[0].data.registration'
-      icon={<Key size={20} strokeWidth={1.75} className='text-muted-foreground' />}
+      icon={<Key size={18} strokeWidth={1.75} className='text-muted-foreground mr-4' />}
       placeholder='Ackreditering'
       className='text-sm'
     />
