2520b73861f77d13f71eea2920572dd9389d64df
gvl <gustav.larsson@tt.se>
2024-10-30 20:38:33 +0100

    fix: exclude auth paths when serving locally

--- src-srv/utils/assertAuthenticatedUser.ts
+++ src-srv/utils/assertAuthenticatedUser.ts
@@ -1,6 +1,21 @@
 import { getSession } from '@auth/express'
 import { type NextFunction, type Response, type Request } from 'express'
 import { authConfig } from './authConfig.js'
+import fs from 'fs'
+import path from 'path'
+
+const BASE_URL = process.env.BASE_URL || ''
+const projectRoot = process.cwd()
+
+// Exclude paths when serving a unbundled project locally
+const localPaths = [
+  ...fs.readdirSync(projectRoot).filter((file) => {
+    return fs.statSync(path.join(projectRoot, file)).isDirectory()
+  }).map((dir) => `${BASE_URL}/${dir}`),
+
+  `${BASE_URL}/@react-refresh`,
+  `${BASE_URL}/@vite`
+]
 
 export async function assertAuthenticatedUser(
   req: Request,
@@ -21,12 +36,22 @@ export async function assertAuthenticatedUser(
   }
 }
 
-
 export function isUnprotectedRoute(req: Request): boolean {
-  const unprotectedRoutes = [
-    '/auth/',
-    '/init'
-  ]
-
+  const unprotectedRoutes = getUnprotectedRoutes()
   return unprotectedRoutes.some((route) => req.path.startsWith(route))
 }
+
+function getUnprotectedRoutes(): string[] {
+  const unprotectedRoutes = [
+    `${BASE_URL}/auth/`,
+    `${BASE_URL}/init`,
+    `${BASE_URL}/assets`
+  ]
+
+  if (process.env.NODE_ENV === 'development') {
+    return unprotectedRoutes.concat(localPaths)
+  }
+
+  return unprotectedRoutes
+}
+
