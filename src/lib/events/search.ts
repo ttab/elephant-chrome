b6f6876f16b2a11f8ebd4fc71fe2f02010878540
Victor Lindh <victorlindh@hotmail.com>
2024-11-11 15:32:02 +0100

    modify searches in plannings, events and assignments in search view
    export withStatus

--- src/lib/events/search.ts
+++ src/lib/events/search.ts
@@ -2,11 +2,14 @@ import { type SearchIndexResponse, type Event } from '../index'
 import { searchIndex } from '../index'
 
 interface SearchEventsParams {
+  page?: number
   skip?: number
   size?: number
+  when?: 'anytime' | 'fixed'
   where?: {
     start?: string | Date
     end?: string | Date
+    text?: string
   }
   sort?: {
     start?: 'asc' | 'desc'
@@ -15,6 +18,9 @@ interface SearchEventsParams {
 }
 
 export const search = async (endpoint: URL, accessToken: string, params?: SearchEventsParams): Promise<SearchIndexResponse<Event>> => {
+  if (params && (!params?.when || params?.when !== 'anytime')) {
+    params.when = 'fixed'
+  }
   const start = params?.where?.start ? new Date(params.where.start) : new Date()
   const end = params?.where?.end ? new Date(params.where.end) : new Date()
   const sort: Array<Record<string, 'asc' | 'desc'>> = []
@@ -29,26 +35,58 @@ export const search = async (endpoint: URL, accessToken: string, params?: Search
 
   sort.push({ 'document.meta.core_newsvalue.value': 'desc' })
 
+  if (params?.when === 'anytime') {
+    sort.push({ 'document.meta.core_event.data.start': 'desc' })
+  }
+
+  const timeRange = params?.when === 'anytime'
+    ? undefined
+    : {
+        range: {
+          'document.meta.core_event.data.start': {
+            gte: start.toISOString(),
+            lte: end.toISOString()
+          }
+        }
+      }
+
+  const textCriteria = !params?.where?.text
+    ? undefined
+    : {
+        bool: {
+          should: [
+            {
+              prefix: {
+                'document.title': {
+                  value: params.where.text,
+                  boost: 2.0,
+                  case_insensitive: true
+                }
+              }
+            },
+            {
+              prefix: {
+                'document.rel.section.title': {
+                  value: params.where.text,
+                  case_insensitive: true
+                }
+              }
+            }
+          ]
+        }
+      }
+
   const query = {
     query: {
       bool: {
-        must: [
-          {
-            range: {
-              'document.meta.core_event.data.start': {
-                gte: start.toISOString(),
-                lte: end.toISOString()
+        must: [],
+        must_not: params?.when !== 'anytime'
+          ? [{
+              term: {
+                'document.meta.core_event.data.end': 'now+1d/d'
               }
-            }
-          }
-        ],
-        must_not: [
-          {
-            term: {
-              'document.meta.core_event.data.end': 'now+1d/d'
-            }
-          }
-        ]
+            }]
+          : []
       }
     },
     _source: true,
@@ -58,6 +96,15 @@ export const search = async (endpoint: URL, accessToken: string, params?: Search
     ],
     sort
   }
+  if (textCriteria) {
+    // @ts-expect-error We don't have types for opensearch queries
+    query.query.bool.must.push(textCriteria)
+  }
+
+  if (timeRange) {
+    // @ts-expect-error We don't have types for opensearch queries
+    query.query.bool.must.push(timeRange)
+  }
 
   return await searchIndex(
     query,
@@ -66,7 +113,7 @@ export const search = async (endpoint: URL, accessToken: string, params?: Search
       endpoint,
       accessToken
     },
-    params?.skip,
+    params?.skip || params?.page,
     params?.size
   )
 }
