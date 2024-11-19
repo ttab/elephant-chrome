9f2c3974e577576d27f8df6187d7ddce015d6d5e
Danne Lundqvist <danne@dotvoid.com>
2024-11-19 15:03:08 +0100

    Don't hide app hamburger menu

--- src/components/View/ViewHeader/Root.tsx
+++ src/components/View/ViewHeader/Root.tsx
@@ -2,13 +2,13 @@ import { cn } from '@ttab/elephant-ui/utils'
 import { type PropsWithChildren } from 'react'
 
 export const Root = ({ children, className }: PropsWithChildren &
-  {
-    className?: string
-  }): JSX.Element => {
+{
+  className?: string
+}): JSX.Element => {
   return (
     <header className={
       cn(
-        'sticky top-0 flex items-center justify-items-start group-first/view-container:ml-[2.5rem] h-14 gap-4 pl-6 pr-3 border-b bg-background z-50',
+        'sticky top-0 flex items-center justify-items-start group-first/view-container:ml-[3.5rem] h-14 gap-4 pr-3 border-b bg-background z-50',
         className
       )
     }
