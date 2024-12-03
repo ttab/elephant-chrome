076214b5f003691c0172e3d965992714ea0ed15b
Danne Lundqvist <danne@dotvoid.com>
2024-12-03 10:37:16 +0100

    Correct color on selected row

--- src/components/Table/Row.tsx
+++ src/components/Table/Row.tsx
@@ -11,7 +11,17 @@ export const Row = ({ row, handleOpen, openDocuments }: {
   return (
     <TableRow
       tabIndex={0}
-      className='flex items-center cursor-default scroll-mt-10 focus:outline-table-selected ml-[2px] mr-[3px] my-[1px] data-[state=selected]:rounded-sm'
+      className={`flex
+        items-center
+        cursor-default
+        scroll-mt-10
+        ml-[2px]
+        mr-[3px]
+        my-[1px]
+        focus:outline-table-selected
+        data-[state=selected]:rounded-sm
+        data-[state=selected]:bg-table-selected
+      `}
       // @ts-expect-error unknown type
       data-state={openDocuments.includes(row.original._id as string) && 'selected'}
       onClick={(event: MouseEvent<HTMLTableRowElement>) => handleOpen(event, row)}
