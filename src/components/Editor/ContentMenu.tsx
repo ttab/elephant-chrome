e809e0d41a612f45f9345029c0cd897da4d675ca
Gustav Larsson <gularsson@users.noreply.github.com>
2024-12-09 16:17:14 +0100

    fix(editor): focused fixes, menus and footer (#958)
    - Added new Footer component to View
    - Updated ContentMenu and ContextMenu with z-index

--- src/components/Editor/ContentMenu.tsx
+++ src/components/Editor/ContentMenu.tsx
@@ -12,7 +12,7 @@ export const ContentMenu = (): JSX.Element => {
   return (
     <Menu.Root className='group'>
       <Menu.Trigger className="flex justify-center place-items-center center font-bold border w-8 h-8 ml-3 rounded-full cursor-default group-data-[state='open']:border-gray-200 hover:border-gray-400 dark:text-slate-200 dark:bg-slate-950 dark:border-slate-600 dark:group-data-[state='open']:border-slate-700 dark:hover:border-slate-500">⋮</Menu.Trigger>
-      <Menu.Content className='flex flex-col mt-[1.3rem] -ml-[1.3rem] border rounded-lg divide-y shadow-xl bg-white border-gray-100 dark:text-white dark:bg-slate-900 dark:border-slate-800 dark:divide-slate-800 dark:shadow-none'>
+      <Menu.Content className='flex flex-col mt-[1.3rem] -ml-[1.3rem] border rounded-lg divide-y shadow-xl bg-white border-gray-100 dark:text-white dark:bg-slate-900 dark:border-slate-800 dark:divide-slate-800 dark:shadow-none z-50'>
         {textActions.length > 0
         && (
           <ContentMenuGroup>
