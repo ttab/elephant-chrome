3fa0d19f8a75026df5c1328228710d64deec406e
Danne Lundqvist <danne@dotvoid.com>
2024-12-11 09:01:06 +0100

    Improved column layout in different screensizes, basic card layout

--- src/views/Approvals/TimeSlot.tsx
+++ src/views/Approvals/TimeSlot.tsx
@@ -1,32 +1,79 @@
-export const TimeSlot = ({ name, label, slots }: {
+import type { PropsWithChildren } from 'react'
+
+export const TimeSlot = ({ name, label, slots, children }: PropsWithChildren & {
   name: string
   label: string
   slots: number[]
 }): JSX.Element => {
+  const { startTime, endTime } = getTimeRange(slots)
+
   return (
     <div className={`
-      w-[95%]
+      w-[96%]
+      col-span-1
       flex-grow
       flex-shrink-0
       @lg/view:w-1/2
       @3xl/view:w-1/3
       @5xl/view:w-[25%]
-      @7xl/view:w-auto
-      snap-start
-      border-e
-      last:border-s-0
-      -mx-2
-      px-4
-      first:ps-0
-      first:ms-0
-      last:pe-0
-      last:me-0
+      @7xl/view:w-[auto]
+      snap-center
+      @lg/view:snap-start
+      last:-border-s-0
+      -mx-1
+      px-2
+      @lg/view:last:pe-0
+      @lg/view:last:me-0
+      flex
+      flex-col
       `}
     >
-      <div className='w-full h-full bg-pinks-300'>
-        <strong>{label}</strong>
-        <p>{slots.join(',')}</p>
+      <div className='w-full flex flex-col bg-white p-2'>
+        <span className='text-sm'>{label}</span>
+
+        <div className='flex flex-row gap-1 text-xs text-gray-400'>
+          {(startTime) && <time dateTime={startTime}>{startTime}</time>}
+          {(startTime || endTime) ? <span>-</span> : <span>&nbsp;</span>}
+          {(endTime) && <time dateTime={endTime}>{endTime}</time>}
+        </div>
       </div>
+
+      <div className='w-full flex-grow bg-muted'>
+        <div className='p-1 rounded-sm'>
+          {children}
+        </div>
+      </div>
+
     </div>
   )
+}
+
+
+/**
+ * Format time range into startTime and endTime
+ *
+ * FIXME: Time format should be locale specific
+*/
+function getTimeRange(hours: number[]): {
+  startTime?: string
+  endTime?: string
+} {
+  if (hours.length === 0) {
+    return {}
+  }
+
+  // Sort the array to ensure values are in order
+  const sortedHours = [...hours].sort((a, b) => a - b)
+
+  const startHour = sortedHours[0]
+  const endHour = sortedHours[sortedHours.length - 1]
+
+  const formatHour = (hour: number, minutes: string) => {
+    return hour.toString().padStart(2, '0') + ':' + minutes
+  }
+
+  return {
+    startTime: formatHour(startHour, '00'),
+    endTime: formatHour(endHour, '59')
+  }
 }
--- src/views/Approvals/index.tsx
+++ src/views/Approvals/index.tsx
@@ -5,6 +5,7 @@ import { useRegistry } from '@/hooks/useRegistry'
 import { useSession } from 'next-auth/react'
 import { EarthIcon } from '@ttab/elephant-ui/icons'
 import { TimeSlot } from './TimeSlot'
+import { ClockIcon } from '@/components/ClockIcon'
 
 const meta: ViewMetadata = {
   name: 'Approvals',
@@ -34,7 +35,7 @@ export const Approvals = (): JSX.Element => {
       </ViewHeader.Root>
 
       <View.Content>
-        <div className='flex flex-row h-screen overflow-x-scroll snap-x snap-mandatory'>
+        <div className='flex flex-row h-full overflow-x-scroll snap-x snap-mandatory @7xl/view:grid @7xl/view:grid-cols-5 '>
           {Object.keys(Slots).map((slot) => {
             return (
               <TimeSlot
@@ -42,7 +43,29 @@ export const Approvals = (): JSX.Element => {
                 name={slot}
                 label={Slots[slot].label}
                 slots={Slots[slot].slots}
-              />
+              >
+
+                <div className='flex flex-col justify-stretch border bg-white rounded p-2 text-sm'>
+                  <div className='flex flex-row justify-between'>
+                    <div></div>
+                    <div className='flex flex-row gap-1 items-center'>
+                      <ClockIcon hour={9} size={14} className='opacity-50' />
+                      <time>21:34</time>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className='text-sm font-bold'>
+                      Utvecklingssamtal i skolan – då kommer advokaten och ställer till det
+                    </div>
+                    <div>lärarombud</div>
+                  </div>
+
+                  <div className='flex flex-row text-xs'>
+                    Inrikes
+                  </div>
+                </div>
+              </TimeSlot>
             )
           })}
         </div>
