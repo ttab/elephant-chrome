2be3a24da580b04e65ec2ab3bbe3d65f73c0aa1d
Danne Lundqvist <danne@dotvoid.com>
2024-12-13 11:44:42 +0100

    Support suppressing automatic scroll handling of view content

--- src/components/View/Content.tsx
+++ src/components/View/Content.tsx
@@ -1,15 +1,26 @@
+import type { CSSProperties, PropsWithChildren } from 'react'
 import { ScrollArea } from '@ttab/elephant-ui'
 import { cn } from '@ttab/elephant-ui/utils'
-import type { PropsWithChildren } from 'react'
 
-export const Content = ({ children, className }: {
+export const Content = ({ children, className, style, autoScroll = true }: {
   className?: string
+  autoScroll?: boolean
+  style?: CSSProperties
 } & PropsWithChildren): JSX.Element => {
   return (
-    <div className={cn('flex-1 w-full mx-auto overflow-hidden', className)}>
-      <ScrollArea className='h-full w-full'>
-        {children}
-      </ScrollArea>
-    </div>
+    <>
+      {autoScroll
+        ? (
+            <ScrollArea className={cn(' flex-1 w-full mx-auto overflow-hidden h-full', className)} style={style}>
+              {children}
+            </ScrollArea>
+          )
+        : (
+            <div className={className} style={style}>
+              {children}
+            </div>
+          )
+      }
+    </>
   )
 }
