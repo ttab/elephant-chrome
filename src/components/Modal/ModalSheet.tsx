2c303f3ac8d7567352910fcd3f4a2973ac79c645
Gustav Larsson <gularsson@users.noreply.github.com>
2024-12-16 13:26:47 +0100

    fix: improve modal handling and preview functionality (#969)
    * fix: improve modal handling and preview functionality
    * Remove full editor view
    * Click shows preview
    * Click outside hides preview
    * Group +/-
    * Show full time
    
    * fix: use type for handleopen

--- src/components/Modal/ModalSheet.tsx
+++ src/components/Modal/ModalSheet.tsx
@@ -1,19 +1,24 @@
 import { Sheet, SheetContent, SheetDescription, SheetTitle } from '@ttab/elephant-ui'
 import type { PropsWithChildren } from 'react'
+import { useModal } from './useModal'
 
 export const ModalSheet = ({ children, isVisible }: {
   isVisible: boolean
-} & PropsWithChildren): JSX.Element => (
-  <Sheet open={isVisible}>
-    <SheetContent
-      side='bottom'
-      className='p-3 h-1/3 focus:outline-none pb-6 max-w-[1200px] mx-auto rounded-t rounded-lg'
-    >
-      <SheetTitle />
-      <SheetDescription />
-      <div className='flex flex-grow gap-4'>
-        {children}
-      </div>
-    </SheetContent>
-  </Sheet>
-)
+} & PropsWithChildren): JSX.Element => {
+  const { hideModal } = useModal()
+  return (
+    <Sheet open={isVisible}>
+      <SheetContent
+        onPointerDownOutside={hideModal}
+        side='bottom'
+        className='p-3 h-1/3 focus:outline-none pb-6 max-w-[1200px] mx-auto rounded-t rounded-lg'
+      >
+        <SheetTitle />
+        <SheetDescription />
+        <div className='flex flex-grow gap-4'>
+          {children}
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
