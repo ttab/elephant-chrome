ec1b4c74f3a7e715b65570de9d5f15390586e976
Danne Lundqvist <danne@dotvoid.com>
2025-01-08 16:16:48 +0100

    Focus first card in current timeslot column (#1007)
    * Focus first card in current timeslot column
    
    * Fixed linting
    
    * Ensure clicks are handled in the next event loop to allow sheets and dialogs etc to close before acting on links
    
    * Improved focus/blur handling when sheet closes
    
    * Added fixme regarding focus ring not visible in some circumstances
    
    * Fixed linting

--- src/components/App/Menu.tsx
+++ src/components/App/Menu.tsx
@@ -14,17 +14,25 @@ import { ThemeSwitcher } from './ThemeSwitcher'
 import { MenuItem } from './MenuItem'
 import { signOut, useSession } from 'next-auth/react'
 import { applicationMenu, type ApplicationMenuItem, type MenuGroups } from '@/defaults/applicationMenuItems'
+import { useRef } from 'react'
 
 export const Menu = (): JSX.Element => {
   const { data } = useSession()
+  const triggerRef = useRef<HTMLButtonElement>(null)
 
   return (
     <Sheet>
-      <SheetTrigger className='rounded-md hover:bg-gray-100 hover:border w-9 h-9 flex items-center justify-center'>
+      <SheetTrigger ref={triggerRef} className='rounded-md hover:bg-gray-100 hover:border w-9 h-9 flex items-center justify-center'>
         <MenuIcon strokeWidth={2.25} size={18} />
       </SheetTrigger>
 
-      <SheetContent side='left' className='p-0 flex flex-col justify-between'>
+      <SheetContent
+        onCloseAutoFocus={(event) => {
+          event.preventDefault()
+        }}
+        side='left'
+        className='p-0 flex flex-col justify-between'
+      >
         <div>
           <SheetHeader>
             <SheetDescription />
