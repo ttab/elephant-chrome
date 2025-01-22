d55833997772c5541ad2f6065dc8f5cfd1ff7ae9
Danne Lundqvist <danne@dotvoid.com>
2025-01-22 16:46:12 +0100

    Added whitespace for readability :)

--- src/hooks/index/lib/assignments/getStatus.ts
+++ src/hooks/index/lib/assignments/getStatus.ts
@@ -10,6 +10,7 @@ export const getStatus = (item: StatusOverviewItem) => {
   const draftStatus = 'draft'
   const currentVersion = item.version
   const headsKeys = Object.keys(item?.heads)
+
   // If there are no statuses set in item.heads, default to 'draft' status
   if (headsKeys.length === 0) {
     return 'draft'
@@ -18,19 +19,23 @@ export const getStatus = (item: StatusOverviewItem) => {
     // If the current version is higher than all of the heads versions, default to 'draft'
     const headsVersions = headsKeys.map((key) => item.heads[key]?.version)
     const currentIsNewest = headsVersions.every((version) => version < currentVersion)
+
     if (currentIsNewest) {
       return 'draft'
     }
+
     if (!currentIsNewest) {
       const latestStatus = headsKeys.filter((key) => item.heads[key].version === currentVersion)
       if (latestStatus.length === 1) {
         return latestStatus[0]
       }
+
       if (latestStatus.length > 1) {
         const sortByCreated = headsKeys.sort((a, b) => item.heads[a].created > item.heads[b].created ? -1 : 1)
         return sortByCreated[0]
       }
     }
   }
+
   return draftStatus
 }
