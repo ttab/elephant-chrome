689b1cb2a5f0afae0302d812d7845026bbf3fe5c
Gustav Larsson <gularsson@users.noreply.github.com>
2025-01-30 08:49:46 +0100

    fix: remove dialog description and title warnings from command (#1085)

--- src/components/Commands/Menu/index.tsx
+++ src/components/Commands/Menu/index.tsx
@@ -16,7 +16,9 @@ import {
   Command,
   CommandGroup,
   CommandItem,
-  CommandSeparator
+  CommandSeparator,
+  DialogTitle,
+  DialogDescription
 } from '@ttab/elephant-ui'
 import { DebouncedCommandInput } from './DebouncedCommandInput'
 import { handleLink } from '@/components/Link/lib/handleLink'
@@ -61,6 +63,8 @@ export const CommandMenu = ({ children, onKeyDown, onChange }: CommandMenuProps)
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogTitle />
+      <DialogDescription />
       <DialogContent className='overflow-hidden p-0 shadow-lg'>
         <Command
           className='[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5'
