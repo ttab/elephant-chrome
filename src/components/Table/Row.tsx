1e62f8b1f64af196b84a7169cfe5ee64c00165f0
gvl <git@gula.dev>
2025-02-05 13:18:12 +0100

    feat: add toggle and toggle-group

--- src/components/Table/Row.tsx
+++ src/components/Table/Row.tsx
@@ -2,16 +2,20 @@ import { type MouseEvent } from 'react'
 import { TableRow, TableCell } from '@ttab/elephant-ui'
 import { type Row as RowType, flexRender } from '@tanstack/react-table'
 import { cn } from '@ttab/elephant-ui/utils'
+import type { Wire } from '@/lib/index/schemas/wire'
 
+type DocumentType = 'Planning' | 'Event' | 'Assignments' | 'Search' | 'Wires'
 export const Row = ({ row, handleOpen, openDocuments, type }: {
-  type: 'Planning' | 'Event' | 'Assignments' | 'Search' | 'Wires'
+  type: DocumentType
   row: RowType<unknown>
   handleOpen: (event: MouseEvent<HTMLTableRowElement>, subRow: RowType<unknown>) => void
   openDocuments: string[]
-}): JSX.Element => (
-  <TableRow
-    tabIndex={0}
-    className={`flex
+}): JSX.Element => {
+  const rowDecorators = getRowDecorators(type, row)
+  return (
+    <TableRow
+      tabIndex={0}
+      className={`flex
         items-center
         cursor-default
         scroll-mt-10
@@ -20,26 +24,56 @@ export const Row = ({ row, handleOpen, openDocuments, type }: {
         focus-visible:ring-2
         focus-visible:ring-table-selected
         data-[state=selected]:bg-table-selected
+        ${rowDecorators}
       `}
-    data-state={((openDocuments.includes((row.original as { _id: string })?._id) && 'selected'))
-    || (type === 'Wires' && row.getIsSelected() && 'focused')}
-    onClick={(event: MouseEvent<HTMLTableRowElement>) => handleOpen(event, row)}
-    ref={(el) => {
-      if (el && row.getIsSelected()) {
-        el.focus()
-      }
-    }}
-  >
-    {row.getVisibleCells().map((cell) => (
-      <TableCell
-        key={cell.id}
-        className={cn(
-          'first:pl-2 last:pr-2 sm:first:pl-6 sm:last:pr-6',
-          cell.column.columnDef.meta?.className
-        )}
-      >
-        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-      </TableCell>
-    ))}
-  </TableRow>
-)
+      data-state={((openDocuments.includes((row.original as { _id: string })?._id) && 'selected'))
+      || (type === 'Wires' && row.getIsSelected() && 'focused')}
+      onClick={(event: MouseEvent<HTMLTableRowElement>) => handleOpen(event, row)}
+      ref={(el) => {
+        if (el && row.getIsSelected()) {
+          el.focus()
+        }
+      }}
+    >
+      {row.getVisibleCells().map((cell) => (
+        <TableCell
+          key={cell.id}
+          className={cn(
+            'first:pl-2 last:pr-2 sm:first:pl-6 sm:last:pr-6',
+            cell.column.columnDef.meta?.className
+          )}
+        >
+          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+        </TableCell>
+      ))}
+    </TableRow>
+  )
+}
+
+function getRowDecorators(type: DocumentType, row: RowType<unknown>): string {
+  if (type !== 'Wires') {
+    return ''
+  }
+
+  const wireRow = row as RowType<Wire>
+
+  const done = wireRow.original?._source?.['heads.done.version']?.[0] > -1
+    && wireRow.original?._source?.['heads.done.created']?.[0]
+  const approved = wireRow.original?._source?.['heads.approved.version']?.[0] > -1
+    && wireRow.original?._source?.['heads.approved.created']?.[0]
+
+
+  if (done || approved) {
+    return done && (!approved || new Date(done) > new Date(approved))
+      ? `bg-done-background
+      data-[state=selected]:bg-done
+      focus-visible:bg-done-background
+      data-[state=focused]:bg-done-border`
+      : `bg-approved-background
+      data-[state=selected]:bg-approved
+      focus-visible:bg-approved-background
+      data-[state=focused]:bg-approved-border`
+  }
+
+  return ''
+}
--- src/views/Wires/components/ModalContent.tsx
+++ src/views/Wires/components/ModalContent.tsx
@@ -1,8 +1,10 @@
-import { Badge, SheetClose } from '@ttab/elephant-ui'
-import { X } from '@ttab/elephant-ui/icons'
+import { Badge, SheetClose, ToggleGroup, ToggleGroupItem } from '@ttab/elephant-ui'
+import { Check, Save, X } from '@ttab/elephant-ui/icons'
 import { Editor } from '../../../components/PlainEditor'
 import { FaroErrorBoundary } from '@grafana/faro-react'
 import { Error } from '@/views'
+import { useDocumentStatus } from '@/hooks/useDocumentStatus'
+import { useNavigationKeys } from '@/hooks/useNavigationKeys'
 
 export const ModalContent = ({ id, source, role, handleClose }: {
   id: string
@@ -10,39 +12,101 @@ export const ModalContent = ({ id, source, role, handleClose }: {
   textOnly?: boolean
   role?: string
   handleClose: () => void
-}): JSX.Element => (
-  <FaroErrorBoundary
-    fallback={(error) => <Error error={error} />}
-  >
-    <div className='w-full flex gap-4 pt-2'>
-      <div className='flex flex-col flex-1 gap-2'>
-        {source && (
-          <Badge
-            className='w-32 h-8 justify-center align-center'
-          >
-            {source.replace('wires://source/', '')}
-          </Badge>
-        )}
-        {role === 'pressrelease' && (
-          <Badge
-            variant='destructive'
-            className='w-32 h-8 justify-center align-center'
-          >
-            Pressmeddelande
-          </Badge>
-        )}
+}): JSX.Element => {
+  const [documentStatus, setDocumentStatus] = useDocumentStatus(id)
+
+  useNavigationKeys({
+    keys: ['s', 'l'],
+    onNavigation: (event) => {
+      if (event.key === 'l') {
+        setDocumentStatus('approved').catch((error) => console.error(error))
+        return
+      }
+
+      if (event.key === 's') {
+        setDocumentStatus('done').catch((error) => console.error(error))
+        return
+      }
+    }
+  })
+
+  return (
+    <FaroErrorBoundary
+      fallback={(error) => <Error error={error} />}
+    >
+      <div className='w-full flex flex-col gap-4'>
+        <div className='flex flex-row gap-6 justify-between items-center'>
+          <div className='flex flex-row gap-2'>
+            {source && (
+              <Badge
+                className='w-fit h-6 justify-center align-center'
+              >
+                {source.replace('wires://source/', '')}
+              </Badge>
+            )}
+            {role === 'pressrelease' && (
+              <Badge
+                variant='destructive'
+                className='w-fit h-6 justify-center align-center'
+              >
+                Pressmeddelande
+              </Badge>
+            )}
+          </div>
+
+          <div className='flex flex-row gap-2'>
+            <ToggleGroup
+              type='single'
+              size='xs'
+              value={documentStatus?.name}
+              onValueChange={(value) => {
+                if (!value && documentStatus) {
+                  setDocumentStatus({
+                    name: 'draft',
+                    version: documentStatus.version,
+                    uuid: documentStatus.uuid
+                  }).catch(console.error)
+                } else {
+                  setDocumentStatus(value).catch(console.error)
+                }
+              }}
+            >
+              <ToggleGroupItem
+                value='done'
+                aria-label='Toggle save'
+                className='border
+              !border-done-border
+              data-[state="on"]:!bg-done
+              data-[state="off"]:!bg-done-background
+              data-[state="off"]:!text-muted-foreground'
+              >
+                <Save className='h-4 w-4' />
+              </ToggleGroupItem>
+              <ToggleGroupItem
+                value='approved'
+                aria-label='Toggle check'
+                className='border
+              !border-approved-border
+              data-[state="on"]:!bg-approved
+              data-[state="off"]:!bg-approved-background
+              data-[state="off"]:!text-muted-foreground'
+              >
+                <Check className='h-4 w-4' />
+              </ToggleGroupItem>
+            </ToggleGroup>
+
+            <SheetClose
+              className='rounded-md hover:bg-gray-100 w-9 h-9 flex items-center justify-center outline-none'
+              onClick={handleClose}
+            >
+              <X strokeWidth={1.75} size={18} />
+            </SheetClose>
+          </div>
+        </div>
+        <div className='flex-2 -mt-2'>
+          <Editor id={id} textOnly />
+        </div>
       </div>
-      <div className='flex-2 -mt-2'>
-        <Editor id={id} textOnly />
-      </div>
-      <div className='flex-1 flex justify-end -mr-2 -mt-3'>
-        <SheetClose
-          className='rounded-md hover:bg-gray-100 w-9 h-9 flex items-center justify-center outline-none'
-          onClick={handleClose}
-        >
-          <X strokeWidth={1.75} size={18} />
-        </SheetClose>
-      </div>
-    </div>
-  </FaroErrorBoundary>
-)
+    </FaroErrorBoundary>
+  )
+}
