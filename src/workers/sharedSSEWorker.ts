43a60b4075fa0acd0b96e4bbfb5ab05a4351b186
Danne Lundqvist <danne@dotvoid.com>
2025-02-10 11:23:38 +0100

    Remove superfluous eslint disable comment

--- src/workers/sharedSSEWorker.ts
+++ src/workers/sharedSSEWorker.ts
@@ -2,7 +2,6 @@ import { SharedWorker } from './SharedWorker'
 
 declare let self: SharedWorkerGlobalScope
 
-// -@ts-expect-error We don't have types for self.onconnect
 self.onconnect = (event: MessageEvent) => {
   const worker = SharedWorker.getInstance()
   worker.addClient(event.ports[0])
