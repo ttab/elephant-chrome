b5df6b002405444e16db61fe6a561ee30686a20d
Danne Lundqvist <danne@dotvoid.com>
2025-02-10 11:22:29 +0100

    Support webworker types

--- src/workers/sharedSSEWorker.ts
+++ src/workers/sharedSSEWorker.ts
@@ -1,6 +1,8 @@
 import { SharedWorker } from './SharedWorker'
 
-// @ts-expect-error We don't have types for self.onconnect
+declare let self: SharedWorkerGlobalScope
+
+// -@ts-expect-error We don't have types for self.onconnect
 self.onconnect = (event: MessageEvent) => {
   const worker = SharedWorker.getInstance()
   worker.addClient(event.ports[0])
