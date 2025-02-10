781e6ffd29c62650fbd8b279b2dffa7c379de4f3
Victor Lindh <37335077+Victor-Lindh@users.noreply.github.com>
2025-02-10 15:11:14 +0100

    feature: show published articles (#1128)
    * add new Menu item
    
    * add status parameter in useAssignments hook
    
    * add new Latest component
    
    * add date-fns
    
    * improvement: article should open as last
    
    * check for uuid
    
    * add cursor
    
    * add component mapping
    
    * add open state to main menu
    
    * move nested sheet to the left
    
    * move Latest menu item above action line
    
    * uniform sheet styles
    
    * improve: tweak padding values
    
    * add SheetHeader, closing X, icon

--- src/components/App/Menu.tsx
+++ src/components/App/Menu.tsx
@@ -16,7 +16,8 @@ import { signOut, useSession } from 'next-auth/react'
 import { applicationMenu, type ApplicationMenuItem, type MenuGroups } from '@/defaults/applicationMenuItems'
 import { useUserTracker } from '@/hooks/useUserTracker'
 import { cn } from '@ttab/elephant-ui/utils'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
+import { Latest as LatestComponent } from '@/views/Latest'
 
 const BASE_URL = import.meta.env.BASE_URL
 const hasUserDoc = (obj: object | undefined) => obj && Object.keys(obj).length > 0
@@ -25,9 +26,19 @@ export const Menu = (): JSX.Element => {
   const { data } = useSession()
   const triggerRef = useRef<HTMLButtonElement>(null)
   const [user] = useUserTracker<object>('')
+  const [mainOpen, setMainMenuOpen] = useState<boolean>(false)
+
+  const sheetComponents: Record<string, React.ComponentType> = {
+    Latest: LatestComponent
+  }
 
   return (
-    <Sheet>
+    <Sheet
+      open={mainOpen}
+      onOpenChange={() => {
+        setMainMenuOpen(!mainOpen)
+      }}
+    >
       <SheetTrigger ref={triggerRef} className='rounded-md hover:bg-gray-100 hover:border w-9 h-9 flex items-center justify-center'>
         <MenuIcon strokeWidth={2.25} size={18} />
       </SheetTrigger>
@@ -61,6 +72,49 @@ export const Menu = (): JSX.Element => {
               return (
                 <div key={group.name}>
                   {group.items.map((item: ApplicationMenuItem) => {
+                    if (item.target === 'sheet') {
+                      const SheetItemComponent = sheetComponents[item.name]
+
+                      return (
+                        <Sheet
+                          key={item.name}
+                          onOpenChange={(isOpen) => {
+                            if (!isOpen) {
+                              setMainMenuOpen(false)
+                            }
+                          }}
+                        >
+                          <SheetTrigger className='rounded-md hover:bg-gray-100 h-9 px-3 w-full'>
+                            <div className='flex items-center gap-3'>
+                              <item.icon strokeWidth={2.25} size={18} color={item.color} />
+                              <div className='pl-2'>{item.label}</div>
+                            </div>
+                          </SheetTrigger>
+                          <SheetContent
+                            side='left'
+                            className='p-0 min-h-screen focus:outline-none pb-6 max-w-[1200px] mx-auto rounded-t rounded-lg'
+                          >
+                            <SheetHeader>
+                              <SheetDescription />
+                              <SheetTitle className='flex flex-row gap-4 justify-between justify-items-center items-center h-14 px-4 border-b'>
+                                <SheetClose className='rounded-md hover:bg-gray-100 w-9 h-9 flex items-center justify-center'>
+                                  <X strokeWidth={2.25} />
+                                </SheetClose>
+                                <SheetClose asChild>
+                                  <div className='flex items-center gap-2'>
+                                    <div>{item.label}</div>
+                                    <item.icon strokeWidth={2.25} size={18} color={item.color} />
+                                  </div>
+                                </SheetClose>
+                                <ThemeSwitcher />
+                              </SheetTitle>
+                            </SheetHeader>
+
+                            {SheetItemComponent && <SheetItemComponent />}
+                          </SheetContent>
+                        </Sheet>
+                      )
+                    }
                     return <MenuItem key={item.name} menuItem={item} />
                   })}
                 </div>
