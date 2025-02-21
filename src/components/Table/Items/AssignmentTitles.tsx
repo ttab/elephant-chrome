acdfad77e1e510dad6d5a9113eda565ff2a5aafb
Victor Lindh <victorlindh@hotmail.com>
2025-02-21 11:06:33 +0100

    remove chevron, Tooltip

--- src/components/Table/Items/AssignmentTitles.tsx
+++ src/components/Table/Items/AssignmentTitles.tsx
@@ -14,9 +14,6 @@ export const AssignmentTitles = ({ planningTitle, assignmentTitle }: TitleProps)
           <div className='w-[200px] min-w-[200px] max-w-[200px] truncate space-x-2 items-center text-muted-foreground'>{planningTitle}</div>
         </Tooltip>
       </div>
-      <Tooltip content={planningTitle}>
-        <div className='text-gray-400 opacity-50 w-4 flex-shrink-0'>{'>'}</div>
-      </Tooltip>
       <div className='space-x-2 items-center'>{assignmentTitle}</div>
     </div>
   ), [planningTitle, assignmentTitle])
