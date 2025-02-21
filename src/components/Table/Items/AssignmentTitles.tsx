b16d3f6a705825daee4a90ea652f7fb47fa8983b
Victor Lindh <victorlindh@hotmail.com>
2025-02-21 12:48:48 +0100

    stack titles

--- src/components/Table/Items/AssignmentTitles.tsx
+++ src/components/Table/Items/AssignmentTitles.tsx
@@ -8,13 +8,13 @@ interface TitleProps {
 
 export const AssignmentTitles = ({ planningTitle, assignmentTitle }: TitleProps): JSX.Element => {
   return useMemo(() => (
-    <div className='gap-1 items-center flex'>
-      <div className='sm:flex'>
+    <>
+      <div className='w-fit'>
         <Tooltip content={planningTitle}>
-          <div className='w-[200px] min-w-[200px] max-w-[200px] truncate space-x-2 items-center text-muted-foreground'>{planningTitle}</div>
+          <div className='w-[200px] truncate space-x-2 items-center text-muted-foreground'>{planningTitle}</div>
         </Tooltip>
       </div>
-      <div className='space-x-2 items-center'>{assignmentTitle}</div>
-    </div>
+      <div>{assignmentTitle}</div>
+    </>
   ), [planningTitle, assignmentTitle])
 }
