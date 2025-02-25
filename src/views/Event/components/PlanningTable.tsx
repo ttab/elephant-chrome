045dc0637d9c175cc46f0819d61fd729c9d65875
Gustav Larsson <gularsson@users.noreply.github.com>
2025-02-25 11:15:50 +0100

    Feature/ele 1166 deprecate createdocumentdialog fix create planning from (#1158)

--- src/views/Event/components/PlanningTable.tsx
+++ src/views/Event/components/PlanningTable.tsx
@@ -1,4 +1,3 @@
-import { CreateDocumentDialog } from '@/components/View/ViewHeader/CreateDocumentDialog'
 import { useIndexUrl } from '@/hooks/useIndexUrl'
 import { useLink } from '@/hooks/useLink'
 import { useRepositoryEvents } from '@/hooks/useRepositoryEvents'
@@ -9,35 +8,39 @@ import { useRef } from 'react'
 import { NewItems } from '@/components/Table/NewItems'
 import useSWR from 'swr'
 import type { FormProps } from '@/components/Form/Root'
+import { Button } from '@ttab/elephant-ui'
+import { createDocument } from '@/lib/createYItem'
+import { Planning } from '@/views/Planning'
+import { useModal } from '@/components/Modal/useModal'
+import * as Templates from '@/defaults/templates'
+import type { HocuspocusProvider } from '@hocuspocus/provider'
+import { createPayload } from '@/defaults/templates/lib/createPayload'
+import { Block } from '@ttab/elephant-api/newsdoc'
 
 interface StatusResult {
   title: string
   uuid: string | undefined
 }
 
-export const PlanningTable = ({ templateValues, asDialog }: {
-  templateValues: {
-    eventId: string
-    eventTitle?: string
-    eventSection?: string
-    newsvalue?: string
-    story?: string
-  }
+export const PlanningTable = ({ provider, documentId, asDialog }: {
+  documentId: string
+  provider?: HocuspocusProvider
 } & FormProps): JSX.Element => {
   const { data: session, status } = useSession()
   const openPlanning = useLink('Planning')
   const createdDocumentIdRef = useRef<string | undefined>()
   const indexUrl = useIndexUrl()
+  const { showModal, hideModal } = useModal()
 
   const { data, mutate, error } = useSWR<StatusResult[] | undefined, Error>([
-    `relatedPlanningItems/${templateValues.eventId}`,
+    `relatedPlanningItems/${documentId}`,
     status,
     indexUrl.href
   ], async (): Promise<StatusResult[] | undefined> => {
     if (status !== 'authenticated') {
       throw new Error('Not authenticated')
     }
-    const statusResults = await Events.relatedPlanningSearch(indexUrl, session.accessToken, [templateValues.eventId], {
+    const statusResults = await Events.relatedPlanningSearch(indexUrl, session.accessToken, [documentId], {
       size: 100
     })
 
@@ -69,27 +72,52 @@ export const PlanningTable = ({ templateValues, asDialog }: {
 
   return (
     <div className='pl-6'>
-      <div className='flex flex-start pb-2'>
-        <CreateDocumentDialog
-          type='Planning'
-          payload={{ ...templateValues, createdDocumentIdRef }}
-          createdDocumentIdRef={createdDocumentIdRef}
-        >
-          <a
-            href='#'
-            className='flex flex-start items-center text-sm gap-2 p-2 -ml-2 rounded-sm hover:bg-gray-100'
-          >
-            <div className='bg-primary rounded-full w-5 h-5 relative'>
-              <PlusIcon
-                size={15}
-                strokeWidth={2.25}
-                color='#FFFFFF'
-                className='absolute inset-0 m-auto'
+      <div className='flex flex-start pt-2 text-primary pb-4'>
+        <Button
+          variant='ghost'
+          className='flex flex-start items-center text-sm gap-2 p-2 -ml-2 rounded-sm hover:bg-gray-100'
+          onClick={() => {
+            const payload = provider?.document
+              ? createPayload(provider.document)
+              : undefined
+
+            const initialDocument = createDocument({
+              template: Templates.planning,
+              inProgress: true,
+              payload: {
+                ...payload || {},
+                links: {
+                  ...payload?.links,
+                  'core/event': [Block.create({
+                    type: 'core/event',
+                    uuid: documentId,
+                    title: 'Untitled',
+                    rel: 'event'
+                  })]
+                }
+              }
+            })
+
+            showModal(
+              <Planning
+                onDialogClose={hideModal}
+                asDialog
+                id={initialDocument[0]}
+                document={initialDocument[1]}
               />
-            </div>
-            Lägg till planering
-          </a>
-        </CreateDocumentDialog>
+            )
+          }}
+        >
+          <div className='bg-primary rounded-full w-5 h-5 relative'>
+            <PlusIcon
+              size={15}
+              strokeWidth={2.25}
+              color='#FFFFFF'
+              className='absolute inset-0 m-auto'
+            />
+          </div>
+          Lägg till planering
+        </Button>
       </div>
       {data?.map((planning) => (
         <div key={planning.uuid}>
