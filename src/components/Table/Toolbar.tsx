c841baa2c1ff8eb07eed1ad3ccb50dd5c6f22c8b
gvl <git@gula.dev>
2025-02-27 08:53:18 +0100

    feat: add static section quick filters

--- src/components/Table/Toolbar.tsx
+++ src/components/Table/Toolbar.tsx
@@ -1,21 +1,23 @@
 import { X } from '@ttab/elephant-ui/icons'
 import { type Table } from '@tanstack/react-table'
 
-import { Button } from '@ttab/elephant-ui'
+import { Button, ToggleGroup, ToggleGroupItem } from '@ttab/elephant-ui'
 import { SelectedFilters } from './SelectedFilters'
-
-interface ToolbarProps<TData> {
-  table: Table<TData>
-}
+import { useSections } from '@/hooks/useSections'
 
 export const Toolbar = <TData,>({
   table
-}: ToolbarProps<TData>): JSX.Element => {
+}: {
+  table: Table<TData>
+}): JSX.Element => {
   const isFiltered = table.getState().columnFilters.length > 0
     || !!table.getState().globalFilter
 
+  const allSections = useSections()
+  const column = table.getColumn('section')
+
   return (
-    <div className='flex items-center justify-between'>
+    <div className='flex items-center justify-between py-1 pr-2.5'>
       <div className='flex flex-1 items-center space-x-2'>
         <SelectedFilters table={table} />
         {isFiltered && (
@@ -32,6 +34,26 @@ export const Toolbar = <TData,>({
           </Button>
         )}
       </div>
+      <ToggleGroup
+        type='multiple'
+        size='xs'
+        value={column?.getFilterValue() as string[] | undefined || []}
+        onValueChange={(value) => {
+          column?.setFilterValue(value.length ? value : undefined)
+        }}
+        className='px-1'
+      >
+        {allSections.map((section) => (
+          <ToggleGroupItem
+            key={section.id}
+            value={section.id}
+            aria-label={`Toggle ${section.title}`}
+            className='border data-[state=off]:text-muted-foreground'
+          >
+            {section.title}
+          </ToggleGroupItem>
+        ))}
+      </ToggleGroup>
     </div>
   )
 }
