a17f081db27f39fd483211c1fcf80303894373ca
Danne Lundqvist <danne@dotvoid.com>
2025-03-10 16:01:47 +0100

    Removed default transition to better show current status

--- src/components/DocumentStatus.tsx
+++ src/components/DocumentStatus.tsx
@@ -1,6 +1,6 @@
 import { Button, DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@ttab/elephant-ui'
-import { ChevronDown, type LucideIcon } from 'lucide-react'
-import { useRef, useState, useEffect, type MouseEventHandler } from 'react'
+import { Check, ChevronDown, type LucideIcon } from 'lucide-react'
+import { useRef, useState, useEffect } from 'react'
 import { useWorkflow } from '@/hooks/index/useWorkflow'
 import { cn } from '@ttab/elephant-ui/utils'
 import type { StatusSpecification, WorkflowTransition } from '@/defaults/workflowSpecification'
@@ -31,33 +31,32 @@ export const DocumentStatus = ({ type, status: currentStatus, setStatus }: {
   const currentStatusDef = statuses[currentStatusName]
   const transitions = workflow[currentStatusName]?.transitions || {}
 
-  const defaultTransitionStatus = Object.keys(transitions).find((status) => transitions[status].default)
-  const defaultTransition = defaultTransitionStatus
-    ? transitions[defaultTransitionStatus]
-    : Object.values(transitions)[0]
-
-  const defaultStatusDef = defaultTransitionStatus ? statuses[defaultTransitionStatus] : undefined
-
-  if (!defaultTransitionStatus || !Object.keys(transitions).length) {
+  if (!Object.keys(transitions).length) {
     return null
   }
+
+  const CurrentIcon = currentStatusDef.icon
 
   return (
     <>
       <div className='flex items-center' ref={containerRef}>
-        <DefaultActionButton
-          onClick={() => showPrompt({
-            status: defaultTransitionStatus,
-            ...defaultTransition
-          })}
-          transition={defaultTransition}
-          statusDef={defaultStatusDef}
-        />
 
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button size='sm' className='h-8 rounded-l-none px-2'>
-              <ChevronDown size={16} />
+            <Button size='sm' variant='outline' className='flex items-center h-8 px-3'>
+              <div className='pe-2'>
+                <CurrentIcon
+                  size={18}
+                  strokeWidth={1.75}
+                  className={currentStatusDef?.className}
+                />
+              </div>
+              <div className='pe-1 border-l'>
+                {workflow[currentStatusName]?.title}
+              </div>
+              <div className='ps-1'>
+                <ChevronDown size={16} />
+              </div>
             </Button>
           </DropdownMenuTrigger>
 
@@ -67,17 +66,17 @@ export const DocumentStatus = ({ type, status: currentStatus, setStatus }: {
             sideOffset={0}
             style={{ minWidth: `${dropdownWidth}px` }}
           >
-            <StatusOptions
-              transitions={transitions}
-              statuses={statuses}
-              onSelect={showPrompt}
-            />
-
-            <StatusFooter
+            <StatusHeader
               icon={currentStatusDef?.icon}
               className={currentStatusDef?.className}
               title={workflow[currentStatusName]?.title}
               description={workflow[currentStatusName]?.description}
+            />
+
+            <StatusOptions
+              transitions={transitions}
+              statuses={statuses}
+              onSelect={showPrompt}
             />
 
           </DropdownMenuContent>
@@ -104,48 +103,32 @@ export const DocumentStatus = ({ type, status: currentStatus, setStatus }: {
 }
 
 
-const DefaultActionButton = ({ onClick, transition, statusDef }: {
-  onClick: MouseEventHandler<HTMLButtonElement>
-  transition: WorkflowTransition
-  statusDef?: StatusSpecification
-}) => (
-  <Button
-    size='sm'
-    className='h-8 rounded-r-none border-r flex flex-row gap-2'
-    onClick={onClick}
-    title={transition?.description}
-  >
-    {!!statusDef?.icon && (
-      <statusDef.icon
-        size={18}
-        strokeWidth={1.75}
-        className={statusDef.className}
-      />
-    )}
-    {transition?.title}
-  </Button>
-)
-
-
-const StatusFooter = ({ icon: Icon, className, title, description }: {
+const StatusHeader = ({ icon: Icon, className, title, description }: {
   icon?: LucideIcon
   className: string
   title: string
   description: string
 }) => (
-  <div className='flex flex-row gap-4 p-4 py-6 bg-secondary text-secondary-foreground border-b'>
+  <div className='flex flex-row gap-5 p-4 py-4 border-t text-secondary-foreground border-b bg-gray-75 dark:bg-popover'>
     <div className='w-4 grow-0 shrink-0 pt-0.5'>
       {!!Icon && (
         <Icon
-          size={24}
+          size={22}
           strokeWidth={1.75}
-          className={cn(className, '-ms-0.5 -mt-0.5')}
+          className={cn(className, '-mt-0.5')}
         />
       )}
     </div>
-    <div className='grow flex flex-col gap-0.5'>
-      <div className='font-bold'>{title}</div>
+    <div className='grow flex flex-col gap-0.5 text-sm'>
+      <div className='font-semibold'>{title}</div>
       <div className='text-sm'>{description}</div>
+    </div>
+    <div>
+      <Check
+        size={24}
+        strokeWidth={2.25}
+        className='mt-1'
+      />
     </div>
   </div>
 )
@@ -168,15 +151,15 @@ const StatusOptions = ({ transitions, statuses, onSelect }: {
         return (
           <DropdownMenuItem
             key={status}
-            className='flex flex-row gap-4 w-full py-2 pe-2 items-start rounded-md'
+            className='flex flex-row gap-5 w-full py-2 pe-2 items-start rounded-md'
             onClick={() => onSelect({ status, ...state })}
           >
             <div className='w-4 grow-0 shrink-0 pt-0.5'>
               {!!statusDef.icon && (
                 <statusDef.icon
-                  size={18}
+                  size={22}
                   strokeWidth={1.75}
-                  className={statusDef.className}
+                  className={cn(statusDef.className, '-mt-1')}
                 />
               )}
             </div>
