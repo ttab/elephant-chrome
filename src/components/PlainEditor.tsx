a113148d6051ad071cc6b92cd78d0f1ea2cbcce0
Victor Lindh <victorlindh@hotmail.com>
2025-03-17 23:50:15 +0100

    look for specific version of document, render PreversionView if found

--- src/components/PlainEditor.tsx
+++ src/components/PlainEditor.tsx
@@ -1,21 +1,25 @@
-import type { EleDocumentResponse } from '@/shared/types'
-import type { TBElement } from '@ttab/textbit'
-import Textbit from '@ttab/textbit'
+import type { EleDocument, EleDocumentResponse } from '@/shared/types'
+import Textbit, { type TBElement } from '@ttab/textbit'
 import useSWR from 'swr'
 import { LoadingText } from './LoadingText'
 import { Bold, Italic, Link, Text, OrderedList, UnorderedList, TTVisual, Factbox, Table } from '@ttab/textbit-plugins'
-
+import { PreversionView } from './Version/PreversionView'
+import type { DocumentVersion } from '@ttab/elephant-api/repository'
+import { PreversionViewInfo } from './Version/PreversionViewInfo'
 const BASE_URL = import.meta.env.BASE_URL || ''
-
 
 const plugins = [Text, UnorderedList, OrderedList, Bold, Italic, Link, TTVisual, Factbox, Table]
 
-const fetcher = async (url: string): Promise<TBElement[] | undefined> => {
+const fetcher = async (url: string): Promise<TBElement[] | EleDocument | undefined> => {
   const response = await fetch(url)
   if (!response.ok) {
     throw new Error('Network response was not ok')
   }
   const result = await response.json() as EleDocumentResponse
+
+  if (result.document?.content.length === 0 && result?.document?.meta && result?.document?.links) {
+    return result?.document
+  }
   return result.document?.content
 }
 
@@ -23,8 +27,8 @@ export const Editor = ({ id, textOnly = false }: {
   id: string
   textOnly?: boolean
 }): JSX.Element => {
-  const { data: content, error } = useSWR<TBElement[] | undefined, Error>(
-    `${BASE_URL}/api/documents/${id}`,
+  const { data: content, error } = useSWR<TBElement[] | EleDocument | undefined, Error>(
+    `${BASE_URL}/api/documents/${id}${previewVersion ? `?version=${previewVersion}` : ''}`,
     fetcher,
     { revalidateOnFocus: false, revalidateOnReconnect: false }
   )
@@ -36,6 +40,16 @@ export const Editor = ({ id, textOnly = false }: {
     </LoadingText>
   )
 
+  if ('title' in content) {
+    // Preversion-preview: render non-article types of documents, such as Planning or Event
+    return (
+      <PreversionView
+        content={content}
+        previewVersion={previewVersion}
+        versionHistory={versionHistory}
+      />
+    )
+  }
 
   return (
     <div className='flex flex-col w-full pb-6 overflow-y-auto max-w-screen-lg mx-auto'>
