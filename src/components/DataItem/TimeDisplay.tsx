fa6e43e81cdd33cb4bab00cc676f004167745461
Victor Lindh <victorlindh@hotmail.com>
2025-03-17 23:58:13 +0100

    TimeDisplay className prop

--- src/components/DataItem/TimeDisplay.tsx
+++ src/components/DataItem/TimeDisplay.tsx
@@ -1,11 +1,11 @@
 import { dateToReadableTime } from '@/lib/datetime'
 import { useRegistry } from '@/hooks'
 
-export const TimeDisplay = ({ date }: { date: Date }): JSX.Element => {
+export const TimeDisplay = ({ date, className }: { date: Date, className?: string }): JSX.Element => {
   const { locale, timeZone } = useRegistry()
 
   return (
-    <span className='font-medium text-sm'>
+    <span className={`font-medium text-sm ${className || ''}`}>
       {dateToReadableTime(date, locale, timeZone) || '-'}
     </span>
   )
