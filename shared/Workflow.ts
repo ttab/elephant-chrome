e6224e032086c2d2bb33a1774d0d116ce29cdfdc
Danne Lundqvist <danne@dotvoid.com>
2025-03-18 08:49:20 +0100

    Removed unused fetched workflow data from backend

--- shared/Workflow.ts
+++ shared/Workflow.ts
@@ -1,17 +1,15 @@
 import { TwirpFetchTransport } from '@protobuf-ts/twirp-transport'
 import { WorkflowsClient } from '@ttab/elephant-api/repository'
-import type { DocumentWorkflow, WorkflowStatus } from '@ttab/elephant-api/repository'
+import type { WorkflowStatus } from '@ttab/elephant-api/repository'
 import { meta } from './meta.js'
 
 export interface WorkflowAndStatuses {
-  workflow: DocumentWorkflow | null
   statuses: WorkflowStatus[]
 }
 
 export class Workflow {
   readonly #client: WorkflowsClient
   #workflows: Record<string, {
-    workflow: DocumentWorkflow | null
     statuses: WorkflowStatus[]
   }>
 
@@ -26,7 +24,7 @@ export class Workflow {
   }
 
   /**
-   * Get a workflow specification, either both workflow and statuses or only statuses if no workflow is defined.
+   * Get a workflow specification, or in reality, the allowed statues for a specific document type.
    */
   async getWorkflow({ type, accessToken }: {
     type: string
@@ -36,39 +34,16 @@ export class Workflow {
       return this.#workflows[type]
     }
 
-    const [workflow, statuses] = await Promise.all([
-      this.#fetchWorkflow({ type, accessToken }),
+    const [statuses] = await Promise.all([
+      // this.#fetchWorkflow({ type, accessToken }),
       this.#fetchStatuses({ type, accessToken })
     ])
 
     this.#workflows[type] = {
-      workflow,
       statuses
     }
 
     return this.#workflows[type]
-  }
-
-  /**
-   * Get a workflow specification from the repository.
-   */
-  async #fetchWorkflow({ type, accessToken }: {
-    type: string
-    accessToken: string
-  }): Promise<DocumentWorkflow | null> {
-    try {
-      const { response } = await this.#client.getWorkflow({
-        type
-      }, meta(accessToken))
-
-      return response.workflow || null
-    } catch (err: unknown) {
-      if (['not_found', 'internal'].includes((err as { code: string })?.code)) {
-        return null
-      }
-
-      throw new Error(`Unable to fetch workflow for ${type}: ${(err as Error)?.message || 'Unknown error'}`)
-    }
   }
 
   /**
