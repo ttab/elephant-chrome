0611ede493265811efec08e5ef863112867b909f
Danne Lundqvist <danne@dotvoid.com>
2025-03-25 13:34:26 +0100

    Removed duplicate file toYMap.ts

--- src/lib/createYItem.ts
+++ src/lib/createYItem.ts
@@ -1,7 +1,7 @@
 import * as Y from 'yjs'
 import { assignmentPlanningTemplate } from '../defaults/templates/assignmentPlanningTemplate'
 import { Block, type Document } from '@ttab/elephant-api/newsdoc'
-import { toYMap } from '@/shared/transformations/lib/toYMap'
+import { toYMap } from '@/shared/transformations/toYMap'
 import { toGroupedNewsDoc, group } from '@/shared/transformations/groupedNewsDoc'
 import { toYjsNewsDoc } from '@/shared/transformations/yjsNewsDoc'
 import type { Wire } from '@/hooks/index/lib/wires'
