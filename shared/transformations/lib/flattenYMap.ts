855f8fac10f110c2ce03ab89845637ccc3cca424
Victor Lindh <victorlindh@hotmail.com>
2025-04-01 11:01:13 +0200

    add function to flatten ymap structure, iterate yxml nodes to persist newlines

---
+++ shared/transformations/lib/flattenYMap.ts
@@ -0,0 +1,28 @@
+import { type EleBlockGroup } from '@/shared/types/index.js'
+import { yTextToSlateElement } from '@slate-yjs/core'
+import { Node } from 'slate'
+import * as Y from 'yjs'
+
+type Result = Record<string, EleBlockGroup | string> | Record<string, unknown>
+
+export function flattenYMap(ymap: Y.Map<unknown>): Result {
+  const result: Result = {}
+
+  for (const [key, value] of ymap.entries()) {
+    if (value instanceof Y.Map) {
+      result[key] = flattenYMap(value)
+    } else if (value instanceof Y.Array) {
+      result[key] = value.toArray()
+        .map((item) => item instanceof Y.Map ? flattenYMap(item) : item)
+    } else if (value instanceof Y.XmlText) {
+      // If the value consist of several Y.XmlText nodes, we need to join each of them
+      // together by newlines, in order to persist the newlines to the repository.
+      const textNodes = value?.length ? yTextToSlateElement(value).children.map(Node.string) : []
+      result[key] = textNodes.join('\n')
+    } else {
+      result[key] = value
+    }
+  }
+
+  return result
+}
