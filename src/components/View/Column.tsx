daa2c8912eb0c58078bc025e413cbd0201bd249f
Victor Lindh <victorlindh@hotmail.com>
2025-04-10 14:40:27 +0200

    adjust approvals columns after removing fullday slot

--- src/components/View/Column.tsx
+++ src/components/View/Column.tsx
@@ -6,28 +6,28 @@ export const Column = ({ className, children }: {
 } & PropsWithChildren) => {
   return (
     <div className={cn(`
-      w-[96vw]
       col-span-1
       flex-grow
       flex-shrink-0
-      @lg/view:w-[49vw]
-      @xl/view:w-[49vw]
-      @2xl/view:w-[20vw]
-      @fhd/view:w-[20vw]
-      qhd:w-[auto]
-      uhd:w-[auto]
       snap-center
-      @lg/view:snap-start
-      last:-border-s-0
-      -mx-1
-      px-2
-      @lg/view:last:pe-1
-      @lg/view:last:me-0
       flex
       flex-col
       bg-muted
       rounded-sm
       gap-1
+      -mx-1
+      @md/view:px-2
+      px-1
+      last:-border-s-0
+      @lg/view:w-[49vw]
+      @xl/view:w-[49vw]
+      @2xl/view:w-[25vw]
+      @fhd/view:w-[25vw]
+      qhd:w-[auto]
+      uhd:w-[auto]
+      @lg/view:snap-start
+      @lg/view:last:pe-1
+      @lg/view:last:me-0
       `, className)}
     >
       {children}
