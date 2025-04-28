1b213da8d6fc4600eb644b91423fa096c1452ac0
gvl <git@gula.dev>
2025-04-28 16:01:45 +0200

    fix(Wire): mark as used without creating article

--- src/components/Form/Footer.tsx
+++ src/components/Form/Footer.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { type FormProps } from './Root'
+import { cn } from '@ttab/elephant-ui/utils'
 
-export const Footer = ({ children, asDialog, setValidateForm, validateStateRef }: FormProps): JSX.Element | null => {
+export const Footer = ({ children, asDialog, setValidateForm, validateStateRef, className }:
+  FormProps & {
+    className?: string
+  }): JSX.Element | null => {
   if (!asDialog) return null
 
   return (
-    <div className='border-t p-5'>
+    <div className={cn('border-t p-5', className)}>
       {React.Children.map(children, (child: React.ReactNode): React.ReactNode =>
         React.isValidElement<FormProps>(child)
           ? React.cloneElement(child, {
