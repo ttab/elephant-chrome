62c39ca3e872f8b16fb16a931e0f9d66f63be95f
Victor Lindh <victorlindh@hotmail.com>
2025-05-07 11:00:23 +0200

    set current cause as default checked value

--- src/components/DocumentStatus/PromptCauseField.tsx
+++ src/components/DocumentStatus/PromptCauseField.tsx
@@ -1,12 +1,13 @@
 import { Label, Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@ttab/elephant-ui'
 
-export const PromptCauseField = ({ onValueChange }: {
+export const PromptCauseField = ({ onValueChange, cause }: {
   onValueChange: (value: string) => void
+  cause?: string | undefined
 }) => {
   return (
     <>
       <Label htmlFor='StatusCause'>Anledning</Label>
-      <Select onValueChange={onValueChange} name='StatusCause'>
+      <Select onValueChange={onValueChange} name='StatusCause' defaultValue={cause && cause}>
         <SelectTrigger>
           <SelectValue placeholder='Välj anledning...' />
         </SelectTrigger>
