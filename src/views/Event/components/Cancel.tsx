d52556f92c804bae998bf447688dac28139e1571
Victor Lindh <victorlindh@hotmail.com>
2025-05-12 12:40:47 +0200

    remove unused class

--- src/views/Event/components/Cancel.tsx
+++ src/views/Event/components/Cancel.tsx
@@ -4,7 +4,7 @@ import { useMemo } from 'react'
 export const Cancel = ({ cancelled, setCancelled }: { cancelled: boolean | undefined, setCancelled: (arg: boolean) => void }) => {
   const isCancelled = typeof cancelled === 'string' ? cancelled === 'true' ? true : false : cancelled
   return useMemo(() => (
-    <div className='flex items-center gap-2 relative'>
+    <div className='flex items-center gap-2'>
       <span className='my-0 mr-4 invisible'>x</span>
       <Checkbox
         id='cancelled'
