cb0eaa1479d54af5cbd08c8eb7788e233a9b6427
Victor Lindh <victorlindh@hotmail.com>
2025-05-12 12:40:34 +0200

    also check type of parameter

--- src/views/Event/components/Cancel.tsx
+++ src/views/Event/components/Cancel.tsx
@@ -2,6 +2,7 @@ import { Checkbox, Label } from '@ttab/elephant-ui'
 import { useMemo } from 'react'
 
 export const Cancel = ({ cancelled, setCancelled }: { cancelled: boolean | undefined, setCancelled: (arg: boolean) => void }) => {
+  const isCancelled = typeof cancelled === 'string' ? cancelled === 'true' ? true : false : cancelled
   return useMemo(() => (
     <div className='flex items-center gap-2 relative'>
       <span className='my-0 mr-4 invisible'>x</span>
