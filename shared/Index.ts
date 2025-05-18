69d415a16daace4fdb2b416e74867711074f6418
Gustav Larsson <gularsson@users.noreply.github.com>
2025-05-18 21:15:52 +0200

    Feature/ele 1354 pollsubscribe impl should ignore events not in the result (#1411)
    * fix(subscriptions): always share queries
    
    * fix(useDocuments): improve polling error handling and updates
    
    Enhance the polling logic in useDocuments to better handle subscription
    updates and error scenarios. Now, when polling fails, a more descriptive
    error message is shown and a 30-second retry delay is introduced. The
    pollSubscriptions function now checks for missing matched items and
    triggers a data refetch if needed, otherwise performs an optimistic
    update for matched items. This ensures the document list stays up to
    date with minimal unnecessary reloads.
    
    * fix: remove log
    
    * feat: add getInterval utility and refactor polling logic
    
    - Introduced a shared getInterval utility for generating randomized jitter
      intervals in milliseconds, reducing code duplication and thundering herd
      issues.
    - Refactored OpenDocuments extension and useDocuments hook to use the new
      getInterval function for periodic tasks and polling retries.
    - Improved polling logic in useDocuments to use refs for subscriptions,
      mutate, and data, ensuring up-to-date values during polling.

--- shared/Index.ts
+++ shared/Index.ts
@@ -93,6 +93,7 @@ export class Index {
               }
             }),
             source,
+            shared: true,
             searchAfter: [],
             loadDocument: loadDocument || options?.asAssignments,
             subscribe: currentPage === 1 ? options?.subscribe : false
