11540e79010c1fe6151ef9426f8c63d84967d695
gvl <git@gula.dev>
2025-05-19 16:04:41 +0200

    fix: this is better

--- src/components/Modal/ModalDialog.tsx
+++ src/components/Modal/ModalDialog.tsx
@@ -1,5 +1,5 @@
 import { Dialog, DialogDescription, DialogTitle, DialogContent } from '@ttab/elephant-ui'
-import { type PropsWithChildren } from 'react'
+import  type { PropsWithChildren } from 'react'
 
 export const ModalDialog = ({ children, isVisible }: {
   isVisible: boolean
