a8cb23c93e65f4785289d69345ce8b71b2ab74fe
gvl <git@gula.dev>
2025-05-20 16:08:32 +0200

    fix: bigint error

--- src-srv/api/documents/[id]/index.ts
+++ src-srv/api/documents/[id]/index.ts
@@ -49,7 +49,7 @@ export const GET: RouteHandler = async (req: Request, { cache, repository, res }
           payload: {
             from: 'cache',
             version: documentResponse.version.toString(),
-            document: fromGroupedNewsDoc(documentResponse)
+            document: fromGroupedNewsDoc(documentResponse).document
           }
         }
       }
@@ -59,7 +59,7 @@ export const GET: RouteHandler = async (req: Request, { cache, repository, res }
         payload: {
           from: 'cache',
           version: documentResponse.version.toString(),
-          document: documentResponse
+          document: documentResponse.document
         }
       }
     }
