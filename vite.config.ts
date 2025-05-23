e54f13026661fb26e05ac486216029c7481fd036
Victor Lindh <victorlindh@hotmail.com>
2025-05-23 18:00:04 +0200

    node.env

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 export default defineConfig(() => {
   return {
     port: 5173,
-    base: '/elephant',
+    base: process.env.NODE_ENV !== 'test' ? process.env.BASE_URL : '/elephant',
     plugins: [
       react(),
       viteStaticCopy({
