7e2b1f4b276d233b77a40a56bd5e8b08f8c033b0
Victor Lindh <37335077+Victor-Lindh@users.noreply.github.com>
2025-05-24 12:39:51 +0200

    remove prod banner (#1479)
    * remove prod banner
    
    * change logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { AppContent } from './AppContent'
 import { ModalProvider } from './components/Modal/ModalProvider'
 import { FaroErrorBoundary } from '@grafana/faro-react'
 import { Error as ErrorPage } from './views'
-const isStage = import.meta.env.VITE_ENV === 'stage'
+const isProd = import.meta.env.VITE_ENV !== 'stage'
 
 export const App = (): JSX.Element => (
   <>
@@ -12,19 +12,21 @@ export const App = (): JSX.Element => (
     >
       <ModalProvider>
         <div className='relative flex h-screen flex-col'>
-          <div style={{
-            background: isStage ? 'red' : 'darkblue',
-            color: '#f5f5f5',
-            width: '100vw',
-            fontSize: '8px',
-            display: 'flex',
-            justifyContent: 'center',
-            position: 'sticky',
-            top: 0
-          }}
-          >
-            {isStage ? 'STAGE' : 'PRODUKTION'}
-          </div>
+          {!isProd && (
+            <div style={{
+              background: 'red',
+              color: '#f5f5f5',
+              width: '100vw',
+              fontSize: '8px',
+              display: 'flex',
+              justifyContent: 'center',
+              position: 'sticky',
+              top: 0
+            }}
+            >
+              STAGE
+            </div>
+          )}
 
           <div className='grid grid-cols-12 h-screen'>
             <AppContent />
