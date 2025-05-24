92184d5f4065930da4e385933c88de5f9a99c200
Victor Lindh <victorlindh@hotmail.com>
2025-05-24 10:58:43 +0200

    add environment marker

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AppContent } from './AppContent'
 import { ModalProvider } from './components/Modal/ModalProvider'
 import { FaroErrorBoundary } from '@grafana/faro-react'
 import { Error as ErrorPage } from './views'
+const isDevelopment = import.meta.env.MODE === 'development'
 
 export const App = (): JSX.Element => (
   <>
@@ -11,6 +12,20 @@ export const App = (): JSX.Element => (
     >
       <ModalProvider>
         <div className='relative flex h-screen flex-col'>
+          <div style={{
+            background: isDevelopment ? 'red' : 'darkblue',
+            color: '#f5f5f5',
+            width: '100vw',
+            fontSize: '8px',
+            display: 'flex',
+            justifyContent: 'center',
+            position: 'sticky',
+            top: 0
+          }}
+          >
+            {isDevelopment ? 'STAGE' : 'PRODUKTION'}
+          </div>
+
           <div className='grid grid-cols-12 h-screen'>
             <AppContent />
           </div>
