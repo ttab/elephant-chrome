014d31fd0af4207daeb394a4333ab32f0fa67dfc
Danne Lundqvist <danne@dotvoid.com>
2025-06-02 13:35:42 +0200

    Feature/elelog 28 endpoint for reverting document to repository version (#1499)
    * Correctly handle specificity in routes to allow nested routes after params
    
    * Endpoint to restore collaborative state to repository state
    
    * Refactor addassignment endpoint to correct place.
    
    * Refactored snapshot into correct structure
    
    * Cleaned up console.log and unecessary casts
    
    * Cleaned up unecessary casts
    
    * Merged assertContext and getContextFromValidSession into context for clarification of types and typeguards
    
    * Fixed indentation
    
    * Fixed broken import

--- src-srv/api/documents/[id]/addassignment.ts
+++ src-srv/api/documents/[id]/addassignment.ts
@@ -1,36 +1,33 @@
 import type { Request } from 'express'
-import type { RouteContentResponse, RouteHandler, RouteStatusResponse } from '../../routes.js'
-import logger from '../../lib/logger.js'
+import type { RouteContentResponse, RouteHandler, RouteStatusResponse } from '../../../routes.js'
+import logger from '../../../lib/logger.js'
 import { Block } from '@ttab/elephant-api/newsdoc'
-import { appendAssignment, appendDocumentToAssignment } from '../../lib/createYItem.js'
-import type { Context } from '../../lib/assertContext.js'
-import { assertContext } from '../../lib/assertContext.js'
-import { createSnapshot } from '../../utils/createSnapshot.js'
-import { planningDocumentTemplate } from '../../../shared/templates/planningDocumentTemplate.js'
-import { getDeliverableType } from '../../../src/defaults/templates/lib/getDeliverableType.js'
+import { appendAssignment, appendDocumentToAssignment } from '../../../lib/createYItem.js'
+import { createSnapshot } from '../../../utils/createSnapshot.js'
+import { planningDocumentTemplate } from '../../../../shared/templates/planningDocumentTemplate.js'
+import { getDeliverableType } from '../../../../src/defaults/templates/lib/getDeliverableType.js'
 import { toYjsNewsDoc } from '@/shared/transformations/yjsNewsDoc.js'
 import { toGroupedNewsDoc } from '@/shared/transformations/groupedNewsDoc.js'
-import type { Wire } from '../../../src/hooks/index/useDocuments/schemas/wire.js'
+import type { Wire } from '../../../../src/hooks/index/useDocuments/schemas/wire.js'
+import { getContextFromValidSession, isContext, type Context } from '../../../lib/context.js'
 
 type Response = RouteContentResponse | RouteStatusResponse
 
 /**
- * Add assignment to an existing planning or a newly craeted one.
+ * Add assignment to an existing planning or a newly created one.
  */
 export const POST: RouteHandler = async (req: Request, { collaborationServer, res }) => {
+  const planningId = req.params.id
   const locals = res.locals as Record<string, unknown> | undefined
   const session = locals?.session as { accessToken?: string, user?: Context['user'] } | undefined
 
-  if (!session?.accessToken || !session?.user) {
-    return {
-      statusCode: 401,
-      statusMessage: 'Unauthorized: Session not found, can not snapshot document'
-    }
+  const context = getContextFromValidSession(session)
+  if (!isContext(context)) {
+    return context
   }
 
   // Validate incoming data
   const {
-    planningId,
     planningTitle,
     type,
     deliverableId,
@@ -73,19 +70,6 @@ export const POST: RouteHandler = async (req: Request, { collaborationServer, re
     return {
       statusCode: 400,
       statusMessage: 'Invalid input to document addassignment endpoint'
-    }
-  }
-
-  const context: Context = {
-    accessToken: session.accessToken,
-    user: session.user,
-    agent: 'server'
-  }
-
-  if (!assertContext(context)) {
-    return {
-      statusCode: 500,
-      statusMessage: 'Invalid context provided'
     }
   }
 
