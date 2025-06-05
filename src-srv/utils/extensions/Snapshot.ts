60227c787b889829b250fdc50c3c8ebab29ab8d9
Victor Lindh <victorlindh@hotmail.com>
2025-06-05 12:07:20 +0200

    restore api endpoint due to revert

--- src-srv/utils/extensions/Snapshot.ts
+++ src-srv/utils/extensions/Snapshot.ts
@@ -107,7 +107,7 @@ export async function snapshot(uuid: string, context: Context): Promise<Snapshot
     }
 
     const base = `${process.env.PROTOCOL || 'http'}://${process.env.HOST || 'localhost'}${process.env.PORT ? `:${process.env.PORT}` : ''}`
-    const url = new URL(`${BASE_URL}/api/documents/${uuid}/snapshot/`, base)
+    const url = new URL(`${BASE_URL}/api/snapshot/${uuid}`, base)
 
     const response = await fetch(url, {
       headers: {
