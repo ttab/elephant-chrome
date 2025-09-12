502a3c4edc32cf6beca02f645a9adffcba1de6e9
Victor Lindh <victorlindh@hotmail.com>
2025-09-12 10:10:46 +0200

    don't get utc minutes, as it will pick out incorrect timezoned values in approvals sort

--- src/hooks/index/lib/assignments/getTimeValue.ts
+++ src/hooks/index/lib/assignments/getTimeValue.ts
@@ -1,7 +1,8 @@
 import { DEFAULT_TIMEZONE } from '@/defaults/defaultTimezone'
 import { parseISO } from 'date-fns'
+import { toZonedTime } from 'date-fns-tz'
 
 export function getTimeValue(dateStr: string) {
-  const d = parseISO(dateStr)
-  return d.getUTCHours() * 3600 + d.getUTCMinutes() * 60 + d.getUTCSeconds()
+  const d = toZonedTime(parseISO(dateStr), DEFAULT_TIMEZONE)
+  return d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds()
 }
