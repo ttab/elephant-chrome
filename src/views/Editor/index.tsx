9a288c5cc86f7f0f03c3e50f5b760cb44b873e0c
Danne Lundqvist <danne@dotvoid.com>
2025-09-15 13:15:18 +0200

    Awareness indicator should not depend on the editor having focus (#1771)

--- src/views/Editor/index.tsx
+++ src/views/Editor/index.tsx
@@ -101,6 +101,13 @@ function EditorWrapper(props: ViewProps & {
   const [notes] = useYValue<Block[] | undefined>('meta.core/note')
   const [, setIsFocused] = useAwareness(props.documentId)
 
+  useEffect(() => {
+    setIsFocused(true)
+    return () => setIsFocused(false)
+    // We only want to rerun when provider change
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [provider])
+
   // Plugin configuration
   const getConfiguredPlugins = () => {
     const basePlugins = [
@@ -135,12 +142,6 @@ function EditorWrapper(props: ViewProps & {
     <View.Root>
       <Textbit.Root
         autoFocus={props.autoFocus ?? true}
-        onBlur={() => {
-          setIsFocused(false)
-        }}
-        onFocus={() => {
-          setIsFocused(true)
-        }}
         plugins={getConfiguredPlugins()}
         placeholders='multiple'
         className='h-screen max-h-screen flex flex-col'
