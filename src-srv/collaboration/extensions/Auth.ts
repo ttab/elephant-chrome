6b8b9a04eb033ef37ab6d4113e87c0918584369d
Fredrik Appelberg <fredrik@appelberg.me>
2025-09-16 15:55:58 +0200

    Verify access tokens algorithmically (#1774)

--- src-srv/collaboration/extensions/Auth.ts
+++ src-srv/collaboration/extensions/Auth.ts
@@ -5,15 +5,30 @@ import {
   type Extension,
   type onAuthenticatePayload
 } from '@hocuspocus/server'
-import { decodeJwt } from 'jose'
+import type { FlattenedJWSInput, GetKeyFunction, JWTHeaderParameters, JWTPayload, JWTVerifyOptions } from 'jose'
+import { jwtVerify } from 'jose'
 import { type JWT } from '@auth/core/jwt'
-import type { OidcConfig } from '../../utils/authConfig.js'
+import { JWTClaimValidationFailed, JWTExpired } from 'jose/errors'
+
+interface KeycloakJWTPayload extends JWTPayload {
+  sub: string
+  name?: string
+  email?: string
+  roles?: string[]
+}
+
+export interface AuthConfiguration {
+  jwks: GetKeyFunction<JWTHeaderParameters, FlattenedJWSInput>
+  verifyOptions: JWTVerifyOptions
+}
 
 export class Auth implements Extension {
-  oidc: OidcConfig
+  jwks: GetKeyFunction<JWTHeaderParameters, FlattenedJWSInput>
+  verifyOptions: JWTVerifyOptions
 
-  constructor(oidcConf: OidcConfig) {
-    this.oidc = oidcConf
+  constructor(configuration: AuthConfiguration) {
+    this.jwks = configuration.jwks
+    this.verifyOptions = configuration.verifyOptions
   }
 
   async onAuthenticate({ token: accessToken }: onAuthenticatePayload): Promise<{
@@ -21,48 +36,40 @@ export class Auth implements Extension {
     accessToken: string
     user: JWT
   }> {
-    const isValidAccessToken = await this.validateAccessToken(accessToken)
+    const jwt = await this.validateAccessToken(accessToken)
 
-    if (isValidAccessToken) {
-      return {
-        agent: 'user',
-        accessToken,
-        user: { ...decodeJwt(accessToken) }
-      }
+    return {
+      agent: 'user',
+      accessToken,
+      user: jwt
     }
-
-    throw new Error('Could not authenticate: Invalid accessToken')
   }
 
   async onStateless({ payload, connection }: onStatelessPayload): Promise<void> {
     const statelessMessage = parseStateless<StatelessAuth>(payload)
 
     if (statelessMessage.type === StatelessType.AUTH) {
-      const valid = await this.validateAccessToken(statelessMessage.message.accessToken)
+      const jwt = await this.validateAccessToken(statelessMessage.message.accessToken)
 
-      if (valid) {
-        const context = connection.context as {
-          accessToken: string
-          user: User
-        }
-
-        context.accessToken = statelessMessage.message.accessToken
-        context.user = { ...decodeJwt(statelessMessage.message.accessToken) }
-      } else {
-        throw new Error('Could not authenticate: Invalid new accessToken')
+      const context = connection.context as {
+        accessToken: string
+        user: User
       }
+
+      context.accessToken = statelessMessage.message.accessToken
+      context.user = jwt
     }
   }
 
-  async validateAccessToken(accessToken: string): Promise<boolean> {
-    const response = await fetch(this.oidc.userinfo_endpoint, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
+  async validateAccessToken(accessToken: string) {
+    try {
+      const jwt = await jwtVerify<KeycloakJWTPayload>(accessToken, this.jwks, this.verifyOptions)
+      return jwt.payload
+    } catch (e) {
+      if (e instanceof JWTExpired || e instanceof JWTClaimValidationFailed) {
+        throw new Error('Could not authenticate: Invalid new accessToken')
       }
-    }).catch((e) => {
       throw new Error('validate access token', { cause: e })
-    })
-
-    return response.ok
+    }
   }
 }
