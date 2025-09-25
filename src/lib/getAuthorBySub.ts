e3f007869f7b280b28da9bc75f91ca59c92190b3
Danne Lundqvist <danne@dotvoid.com>
2025-09-25 16:19:19 +0200

    Add action to take over assignment creation (#1800)

--- src/lib/getAuthorBySub.ts
+++ src/lib/getAuthorBySub.ts
@@ -4,6 +4,10 @@ import type { IDBAuthor } from 'src/datastore/types'
  * Retrieve an author from IndexedDb by sub
  */
 export function getAuthorBySub(authors: IDBAuthor[], sub?: string) {
+  if (typeof sub !== 'string') {
+    return undefined
+  }
+
   const id = sub?.slice(sub?.lastIndexOf('/') + 1)
 
   return (id)
