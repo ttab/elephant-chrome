549bd04ab17dae7a414a177ddba5114db3a0cc3b
gvl <git@gula.dev>
2025-09-26 08:44:38 +0200

    fix: listen for changed planning events for Approval

--- src/hooks/index/useAssignments.tsx
+++ src/hooks/index/useAssignments.tsx
@@ -85,6 +85,7 @@ export const useAssignments = ({
     'core/editorial-info', 'core/editorial-info+meta',
     'core/flash', 'core/flash+meta'
   ], (event) => {
+    console.log('event', event)
     if ((event.event !== 'document'
       && event.event !== 'status'
       && event.event !== 'delete_document')) {
@@ -100,7 +101,10 @@ export const useAssignments = ({
         .find((assignment) =>
           (assignment._id === event.uuid
             || event.mainDocument === assignment._id
-            || assignment._deliverableId === event.uuid))
+            || assignment._deliverableId === event.uuid
+            || assignment._planningId === event.uuid
+          ))
+
       if (assignment) {
         void mutate()
         return
