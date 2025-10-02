aebe1d75f708690a2ef0520885ddda6747c57336
Danne Lundqvist <danne@dotvoid.com>
2025-10-02 14:35:33 +0200

    Removed left over console.log

--- src/components/Init/index.tsx
+++ src/components/Init/index.tsx
@@ -88,7 +88,6 @@ export const Init = ({ children }: PropsWithChildren): JSX.Element => {
     )
   }
 
-  console.log(webSocketUrl.toString())
   return (
     <WebSocketProvider url={webSocketUrl.toString()}>
       <UserTrackerProvider>
