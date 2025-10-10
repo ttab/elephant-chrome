07780df7ff8807adf098d11b63896fe7366b2c51
AnnikaK <annika.karlsson@tt.se>
2025-10-10 11:15:31 +0200

    Add initial Cocncept view

--- src/views/Concept/index.tsx
+++ src/views/Concept/index.tsx
@@ -1,6 +1,14 @@
 // Todo specify Conceptfiles for each Concept
 
-import type { ViewMetadata } from '@/types/index'
+import { AwarenessDocument } from '@/components/AwarenessDocument'
+import { useQuery } from '@/hooks/useQuery'
+import type { ViewMetadata, ViewProps } from '@/types/index'
+import type * as Y from 'yjs'
+import { Error } from '../Error'
+import { useCollaboration } from '@/hooks/useCollaboration'
+import { useAwareness } from '@/hooks/useAwareness'
+import { useEffect } from 'react'
+import { View } from '@/components/View'
 
 const meta: ViewMetadata = {
   name: 'Concept',
@@ -18,8 +26,53 @@ const meta: ViewMetadata = {
   }
 }
 
-export const Concept = () => {
-  return <></>
+export const Concept = (props: ViewProps & { document?: Y.Doc }) => {
+  const [query] = useQuery()
+  const documentId = props.id || query.id
+
+  if (!documentId) {
+    return <></>
+  }
+
+  return (
+    <>
+      {typeof documentId === 'string'
+        ? (
+            <AwarenessDocument documentId={documentId} document={props.document}>
+              <ConceptWrapper {...props} documentId={documentId} />
+            </AwarenessDocument>
+          )
+        : (
+            <Error
+              title='Dokument saknas'
+              message='Inget dokument att redigera är angivet. Navigera tillbaka till översikten och försök igen.'
+            />
+          )}
+    </>
+  )
+}
+
+const ConceptWrapper = (props: ViewProps & { documentId: string }): JSX.Element => {
+  const { provider, synced, user } = useCollaboration()
+  const [, setIsFocused] = useAwareness(props.documentId)
+
+  useEffect(() => {
+    provider?.setAwarenessField('data', user)
+    setIsFocused(true)
+
+    return () => {
+      setIsFocused(false)
+    }
+  }, [provider])
+
+  return (
+    <>
+      <View.Root asDialog={props?.asDialog} className={props?.className}>
+        <h1>Concept Wrapper</h1>
+
+      </View.Root>
+    </>
+  )
 }
 
 Concept.meta = meta
