1656b352663a75f9d12e0389af0434f04a5cdc4a
LillaBlond <annika.karlsson@tt.se>
2025-10-24 12:05:59 +0200

    Validation now working for both fields

--- src/views/Section/index.tsx
+++ src/views/Section/index.tsx
@@ -15,10 +15,10 @@ import { View } from '@/components/View'
 import { ConceptHeader } from '../Concepts/components/ConceptHeader'
 import { InfoIcon } from '@ttab/elephant-ui/icons'
 import { Button } from '@ttab/elephant-ui'
-import type { HocuspocusProvider } from '@hocuspocus/provider'
-import type { AwarenessUserData } from '@/contexts/CollaborationProvider'
 import { Form } from '@/components/Form'
 import { TextBox } from '@/components/ui'
+import { Validation } from '@/components/Validation'
+import { Title } from '@/components/Title'
 
 
 const meta: ViewMetadata = {
@@ -47,7 +47,7 @@ export const Section = (props: ViewProps & { document?: Y.Doc }): JSX.Element =>
       {typeof documentId === 'string'
         ? (
             <AwarenessDocument documentId={documentId} document={props.document}>
-              <SectionWrapper {...props} documentId={documentId} />
+              <SectionContent2 {...props} documentId={documentId} />
             </AwarenessDocument>
           )
         : (
@@ -60,13 +60,20 @@ export const Section = (props: ViewProps & { document?: Y.Doc }): JSX.Element =>
   )
 }
 
-const SectionWrapper = (props: ViewProps & { documentId: string }): JSX.Element => {
+const SectionContent2 = ({
+  documentId,
+  onDialogClose,
+  asDialog,
+  className
+}: {
+  documentId: string
+} & ViewProps): JSX.Element => {
   const { provider, synced, user } = useCollaboration()
-  const [, setIsFocused] = useAwareness(props.documentId)
+  const [, setIsFocused] = useAwareness(documentId)
   const [isChanged] = useYValue<boolean>('root.changed')
-  const [title] = useYValue<boolean>('root.title')
-  const { status } = useSession()
-
+  const { status, data: session } = useSession()
+  const [, setChanged] = useYValue<boolean>('root.changed')
+  const environmentIsSane = provider && status === 'authenticated'
   useEffect(() => {
     provider?.setAwarenessField('data', user)
     setIsFocused(true)
@@ -76,11 +83,18 @@ const SectionWrapper = (props: ViewProps & { documentId: string }): JSX.Element 
     }
   }, [provider, user])
 
-  const environmentIsSane = provider && status === 'authenticated'
+  const handleChange = useCallback((value: boolean): void => {
+    const root = provider?.document.getMap('ele').get('root') as Y.Map<unknown>
+    const changed = root.get('changed') as boolean
+    if (changed !== value) {
+      root.set('changed', value)
+    }
+  }, [provider])
 
   const handleSubmit = (): void => {
+    console.log('submitting')
     if (environmentIsSane) {
-      void snapshotDocument(props.documentId, { status: 'usable' }).then((response) => {
+      void snapshotDocument(documentId, { status: 'usable', addToHistory: true }).then((response) => {
         if (response?.statusMessage) {
           toast.error('Kunde inte skapa ny sektion!', {
             duration: 5000,
@@ -89,86 +103,18 @@ const SectionWrapper = (props: ViewProps & { documentId: string }): JSX.Element 
           return
         }
 
-        if (props.onDialogClose) {
-          props.onDialogClose(props.documentId, 'title')
+        // TODO should add mutate to add it to show in the list directly
+        if (onDialogClose) {
+          onDialogClose(/* props.documentId, 'title' */)
         }
       })
     }
   }
 
-  return (
-    <>
-      <View.Root asDialog={props?.asDialog} className={props?.className}>
-        <ConceptHeader
-          documentId={props.documentId}
-          asDialog={!!props.asDialog}
-          isChanged={isChanged}
-          onDialogClose={props.onDialogClose}
-          type='Sektion'
-        />
-        {!!provider && synced
-          ? (
-              <SectionContent
-                {...props}
-                documentId={props.documentId}
-                provider={provider}
-                synced={synced}
-                user={user}
-              />
-            )
-          : <></>}
-        <View.Footer className='justify-center'>
-          {props.asDialog
-            && (
-              <>
-                {!environmentIsSane && (
-                  <div className='text-sm leading-tight pb-2 text-left flex gap-2'>
-                    <span className='w-4'>
-                      <InfoIcon size={18} strokeWidth={1.75} className='text-muted-foreground' />
-                    </span>
-                    <p>
-                      Du är utloggad eller har tappat kontakt med systemet.
-                      Vänligen försök logga in igen.
-                    </p>
-                  </div>
-                )}
+  const handleReset = (): void => {
+    console.log('reset')
+  }
 
-                <Button
-                  onClick={handleSubmit}
-                  disabled={!title || !environmentIsSane}
-                  className='whitespace-nowrap'
-                >
-                  Skapa Sektion
-                </Button>
-              </>
-            )}
-        </View.Footer>
-      </View.Root>
-    </>
-  )
-}
-
-const SectionContent = ({
-  provider,
-  documentId,
-  asDialog
-}: {
-  provider: HocuspocusProvider | undefined
-  synced: boolean
-  user: AwarenessUserData
-  documentId: string
-} & ViewProps): JSX.Element => {
-  const { status, data: session } = useSession()
-  const [title] = useYValue<boolean>('root.title')
-  const [isChanged, setChanged] = useYValue<boolean>('root.changed')
-  const environmentIsSane = provider && status === 'authenticated'
-  const handleChange = useCallback((value: boolean): void => {
-    const root = provider?.document.getMap('ele').get('root') as Y.Map<unknown>
-    const changed = root.get('changed') as boolean
-    if (changed !== value) {
-      root.set('changed', value)
-    }
-  }, [provider])
   const onSave = async (): Promise<void> => {
     if (!session) {
       toast.error('Ett fel har uppstått, ändringen kunde inte spara! Ladda om webbläsaren och försök igen')
@@ -189,47 +135,108 @@ const SectionContent = ({
 
   return (
     <>
-      <View.Content className='flex flex-col max-w-[1000px] p-5'>
-        <Form.Root
-          asDialog={asDialog}
-          onChange={handleChange}
-        >
-          <Form.Content>
-            <TextBox
-              singleLine={true}
-              path='root.title'
-              className='border-[1px]'
-              onChange={handleChange}
-              placeholder='Titel'
-            >
-            </TextBox>
-            <TextBox
-              singleLine={true}
-              path='meta.core/section[0].data.code'
-              className='border-[1px]'
-              onChange={handleChange}
-              placeholder='Kod'
-            >
-            </TextBox>
-            {!asDialog
-              && (
-                <div className='flex gap-2.5 align-end'>
-                  <Button
-                    onClick={(e) => {
-                      e.preventDefault()
-                      void onSave()
-                    }}
-                    disabled={!title || !environmentIsSane || !isChanged}
-                    className=''
-                  >
-                    Spara
-                  </Button>
-                </div>
-              )}
-          </Form.Content>
-        </Form.Root>
-      </View.Content>
+      <View.Root asDialog={asDialog} className={className}>
+        <ConceptHeader
+          documentId={documentId}
+          asDialog={!!asDialog}
+          isChanged={isChanged}
+          onDialogClose={onDialogClose}
+          type='Sektion'
+        />
+        {!!provider && synced
+          ? (
+              <View.Content className='flex flex-col max-w-[1000px] p-5'>
+                <Form.Root
+                  asDialog={asDialog}
+                  onChange={handleChange}
+                >
+                  <Form.Content>
+                    <Form.Title>
+                      <Title
+                        placeholder='Sektionsnamn'
+                        autoFocus={true}
+                      />
+                    </Form.Title>
+                    <Validation
+                      path='meta.core/section[0].data.code'
+                      label='code'
+                      block='meta.core/section[0].data.code'
+                    >
+                      <TextBox
+                        singleLine={true}
+                        path='meta.core/section[0].data.code'
+                        className='border-[1px]'
+                        placeholder='Kod'
+                      >
+                      </TextBox>
+                    </Validation>
+
+
+                    {!asDialog
+                      && (
+                        <div className='flex gap-2.5 align-end'>
+                          <Button
+                            onClick={(e) => {
+                              e.preventDefault()
+                              void onSave()
+                            }}
+                            disabled={!environmentIsSane}
+                            className=''
+                          >
+                            Spara
+                          </Button>
+                        </div>
+                      )}
+
+                  </Form.Content>
+                  <Form.Footer>
+                    <Form.Submit
+                      onSubmit={() => handleSubmit()}
+                      onReset={() => handleReset()}
+                      className='w-full flex gap-2 justify-end'
+                    >
+                      <Button
+                        type='reset'
+                        className='whitespace-nowrap'
+                        variant='secondary'
+                        disabled={!environmentIsSane}
+                      >
+                        Avbryt
+                      </Button>
+
+                      <Button
+                        type='submit'
+                        disabled={!environmentIsSane}
+                        className='whitespace-nowrap'
+                      >
+                        Skapa Sektion
+                      </Button>
+                    </Form.Submit>
+                    {asDialog
+                      && (
+                        <>
+                          {!environmentIsSane && (
+                            <div className='text-sm leading-tight pb-2 text-left flex gap-2'>
+                              <span className='w-4'>
+                                <InfoIcon size={18} strokeWidth={1.75} className='text-muted-foreground' />
+                              </span>
+                              <p>
+                                Du är utloggad eller har tappat kontakt med systemet.
+                                Vänligen försök logga in igen.
+                              </p>
+                            </div>
+                          )}
+
+                        </>
+                      )}
+                  </Form.Footer>
+                </Form.Root>
+              </View.Content>
+            )
+          : <></>}
+      </View.Root>
     </>
   )
 }
+
 Section.meta = meta
