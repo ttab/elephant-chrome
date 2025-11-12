59656f6e6be68053153cfea071ab3b9415c360fa
LillaBlond <annika.karlsson@tt.se>
2025-11-12 09:48:35 +0100

    removing conceptHooks not used yet

--- src/views/Concepts/lib/useConcepts.ts
+++ src/views/Concepts/lib/useConcepts.ts
@@ -3,12 +3,10 @@ import { useStories } from '@/hooks/useStories'
 import type { TableDataKey } from './conceptDataTable'
 import { tableDataMap } from './conceptDataTable'
 
-
 export const useConcepts = (title: TableDataKey | undefined) => {
   const sections = useSections({ activeOnly: false })
   const storyTags = useStories({ activeOnly: false })
-  /* const categories = useCategories()
-  const organisers = useOrganisers() */
+
   const conceptMap = {
     Sektioner: { ...tableDataMap.Sektioner, data: sections },
     'Story tags': { ...tableDataMap['Story tags'], data: storyTags }
