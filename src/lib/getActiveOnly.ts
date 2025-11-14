6aca5e08075f59e7d54a904f7602f4a71b5d8a7c
LillaBlond <annika.karlsson@tt.se>
2025-11-14 11:59:48 +0100

    removing conditional check since usableVersion is a required field everywhere

--- src/lib/getActiveOnly.ts
+++ src/lib/getActiveOnly.ts
@@ -3,7 +3,7 @@ import type { IDBConcept } from 'src/datastore/types'
 
 export const getActiveOnly = <T extends IDBConcept>(objects: T[]) => {
   const filteredObjects = objects.filter((item) => {
-    return item.usableVersion && item.usableVersion > 0
+    return item.usableVersion > 0
   })
   return filteredObjects
 }
