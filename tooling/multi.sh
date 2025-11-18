9b09dc2f6a77d431794bf21aad9e09aa9d94ac13
gvl <git@gula.dev>
2025-11-18 09:47:53 +0100

    fix: change ports due to macos conflicts - correct ports

--- tooling/multi.sh
+++ tooling/multi.sh
@@ -15,6 +15,6 @@ if ! command -v caddy >/dev/null 2>&1; then
 fi
 
 concurrently --kill-others --names "bff-a,bff-b,caddy" \
-  "WS_URL=ws://localhost:5183/elephant/ws PORT=5183 VITE_HMR_PORT=6000 VITE_DEV_SERVER_PORT=4901 HOST=localhost PROTOCOL=http BASE_URL=/elephant AUTH_POST_LOGOUT_URI=http://localhost:5183/elephant npm run dev" \
-  "WS_URL=ws://localhost:5283/elephant/ws PORT=5283 VITE_HMR_PORT=5000 VITE_DEV_SERVER_PORT=5901 HOST=localhost PROTOCOL=http BASE_URL=/elephant AUTH_POST_LOGOUT_URI=http://localhost:5283/elephant npm run dev" \
+  "WS_URL=ws://localhost:5183/elephant/ws PORT=5183 VITE_HMR_PORT=4901 VITE_DEV_SERVER_PORT=5173 HOST=localhost PROTOCOL=http BASE_URL=/elephant AUTH_POST_LOGOUT_URI=http://localhost:5183/elephant npm run dev" \
+  "WS_URL=ws://localhost:5283/elephant/ws PORT=5283 VITE_HMR_PORT=5901 VITE_DEV_SERVER_PORT=5273 HOST=localhost PROTOCOL=http BASE_URL=/elephant AUTH_POST_LOGOUT_URI=http://localhost:5283/elephant npm run dev" \
   "caddy run --config Caddyfile --adapter caddyfile"
