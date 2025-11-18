7755e150489fb243223410472ddebe258c4c4665
LillaBlond <annika.karlsson@tt.se>
2025-11-18 08:52:22 +0100

    Removing checking inner keys

--- src/views/Concepts/lib/isConceptType.ts
+++ src/views/Concepts/lib/isConceptType.ts
@@ -4,6 +4,6 @@ export const isConceptType = (type: string | undefined): boolean => {
   if (type === undefined) {
     return false
   } else {
-    return type in tableDataMap || Object.values(tableDataMap).some((inneKey) => Object.values(inneKey).includes(type))
+    return type in tableDataMap
   }
 }
