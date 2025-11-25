f560d33915b55557b1921429dd850d53d41d3f0b
LillaBlond <annika.karlsson@tt.se>
2025-11-25 10:38:33 +0100

    moving component types to separate tsx file

--- src/views/Concepts/lib/conceptDataTable.ts
+++ src/views/Concepts/lib/conceptDataTable.ts
@@ -1,8 +1,5 @@
 import { TagIcon } from '@ttab/elephant-ui/icons'
 import type { IDBOrganiser, IDBSection, IDBStory } from 'src/datastore/types'
-import { SectionContent } from '../../Concept/components/SectionContent'
-import { StoryTagContent } from '../../Concept/components/StoryTagContent'
-import { OrganiserContent } from '@/views/Concept/components/OrganiserContent'
 
 export const tableDataMap = {
   'core/section': {
@@ -11,8 +8,7 @@ export const tableDataMap = {
     description: 'En sektion för innehåll',
     data: null as unknown as IDBSection[],
     documentType: 'core/section',
-    icon: TagIcon,
-    content: SectionContent
+    icon: TagIcon
   },
   'core/story': {
     label: 'Story tags',
@@ -20,8 +16,7 @@ export const tableDataMap = {
     description: 'En pågående historia som det rapporteras om',
     data: null as unknown as IDBStory[],
     documentType: 'core/story',
-    icon: TagIcon,
-    content: StoryTagContent
+    icon: TagIcon
   },
   'core/organiser': {
     label: 'Organisatörer',
@@ -29,10 +24,10 @@ export const tableDataMap = {
     description: 'Ett dokument som beskriver en organisation',
     data: null as unknown as IDBOrganiser[],
     documentType: 'core/organiser',
-    icon: TagIcon,
-    content: OrganiserContent
+    icon: TagIcon
   }
 } as const
+
 
 export type ConceptTableDataMap = typeof tableDataMap
 export type ConceptTableDataKey = keyof ConceptTableDataMap
