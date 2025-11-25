c9d0f0740759ce143e34d1d7ac29d140f659ee89
LillaBlond <annika.karlsson@tt.se>
2025-11-25 16:03:28 +0100

    small fix to make label showing for inaktiv concepts

--- src/components/Table/Items/DocumentStatus.tsx
+++ src/components/Table/Items/DocumentStatus.tsx
@@ -1,11 +1,13 @@
 import { StatusSpecifications, WorkflowSpecifications } from '@/defaults/workflowSpecification'
+import { isConceptType } from '@/views/Concepts/lib/isConceptType'
 import { Tooltip } from '@ttab/elephant-ui'
 
 export const DocumentStatus = ({ type, status }: {
   type: string
   status: string
 }): JSX.Element => {
-  const docStatus = StatusSpecifications[status]
+  const newStatus = isConceptType(type) && status === 'unpublished' ? 'inactive' : status
+  const docStatus = StatusSpecifications[newStatus]
   const label = WorkflowSpecifications[type]?.[status]?.title || null
   return (
     <Tooltip content={label}>
