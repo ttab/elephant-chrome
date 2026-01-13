13fe38a713bbf5b568250f667d468aa286abc5c0
LillaBlond <annika.karlsson@tt.se>
2026-01-13 10:27:39 +0100

    changed concept type back to object

--- src/views/Concept/lib/ConceptContentRender.tsx
+++ src/views/Concept/lib/ConceptContentRender.tsx
@@ -1,21 +1,19 @@
 import { OrganiserContent } from '@/views/Concept/components/OrganiserContent'
 import { SectionContent } from '@/views/Concept/components/SectionContent'
 import { StoryTagContent } from '@/views/Concept/components/StoryTagContent'
-import type { ConceptTableDataMap } from '@/views/Concepts/lib/conceptDataTable'
 import { Error } from '@/views/Error'
 import type { HocuspocusProvider } from '@hocuspocus/provider'
 
 export const ConceptContentRender = ({ documentType, concept, provider, isActive, asDialog, handleChange }:
 {
   documentType: string | undefined
-  concept: ConceptTableDataMap
+  concept: object
   provider: HocuspocusProvider
   isActive: boolean
   asDialog: boolean | undefined
   handleChange: (value: boolean) => void
 }) => {
   if (!concept || !provider) return <></>
-  console.log(concept)
   switch (documentType) {
     case 'core/section':
       return SectionContent({ isActive, handleChange, asDialog })
