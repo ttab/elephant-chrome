45c789664a9b40d1a86c22958afda94058ba12bd
LillaBlond <annika.karlsson@tt.se>
2026-01-13 09:47:56 +0100

    adding typing to Concept parameter

--- src/views/Concept/lib/ConceptContentRender.tsx
+++ src/views/Concept/lib/ConceptContentRender.tsx
@@ -1,20 +1,21 @@
 import { OrganiserContent } from '@/views/Concept/components/OrganiserContent'
 import { SectionContent } from '@/views/Concept/components/SectionContent'
 import { StoryTagContent } from '@/views/Concept/components/StoryTagContent'
+import type { ConceptTableDataMap } from '@/views/Concepts/lib/conceptDataTable'
 import { Error } from '@/views/Error'
 import type { HocuspocusProvider } from '@hocuspocus/provider'
 
 export const ConceptContentRender = ({ documentType, concept, provider, isActive, asDialog, handleChange }:
 {
   documentType: string | undefined
-  concept: object
+  concept: ConceptTableDataMap
   provider: HocuspocusProvider
   isActive: boolean
   asDialog: boolean | undefined
   handleChange: (value: boolean) => void
 }) => {
   if (!concept || !provider) return <></>
-
+  console.log(concept)
   switch (documentType) {
     case 'core/section':
       return SectionContent({ isActive, handleChange, asDialog })
