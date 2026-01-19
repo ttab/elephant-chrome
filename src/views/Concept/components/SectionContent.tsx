35d84a587d7e6d05035209d978bbc0ac03af9f4f
LillaBlond <annika.karlsson@tt.se>
2026-01-19 09:42:58 +0100

    adding props to sectionContent

--- src/views/Concept/components/SectionContent.tsx
+++ src/views/Concept/components/SectionContent.tsx
@@ -1,30 +1,32 @@
-import { Form } from '@/components/Form'
+import { Form } from '@/components/index'
 import { TextInput } from '@/components/ui/TextInput'
 import { useYValue } from '@/modules/yjs/hooks'
 import type { YDocument } from '@/modules/yjs/hooks/useYDocument'
+import type { ViewProps } from '@/types/index'
 import type * as Y from 'yjs'
 
-export const SectionContent = ({ ydoc, isActive, asDialog }: {
-  ydoc: YDocument<Y.Map<unknown>>
-  isActive: boolean | null
-  asDialog: boolean | undefined
-}) => {
+export const SectionContent = ({
+  ydoc,
+  isActive,
+  ...props
+}: ViewProps & { ydoc: YDocument<Y.Map<unknown>>, isActive: boolean | null }) => {
   const [title] = useYValue<Y.XmlText>(ydoc.ele, 'root.title', true)
   const [code] = useYValue<Y.XmlText>(ydoc.ele, 'meta.core/section[0].data.code', true)
   return (
-    <Form.Content>
+    <Form.Content {...props}>
       <TextInput
         ydoc={ydoc}
         value={title}
         className={isActive ? 'border' : 'bg-slate-100 text-slate-500'}
         placeholder='Titel'
         label='Titel'
+        autoFocus={!!props.asDialog}
         disabled={!isActive}
       />
       <TextInput
         ydoc={ydoc}
         value={code}
-        asDialog={asDialog}
+        asDialog={props.asDialog}
         className={isActive ? 'border' : 'bg-slate-100 text-slate-500'}
         placeholder='Kod'
         label='Kod'
