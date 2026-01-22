3c370075f63983c0ea59ad40ec521d13fbb603c6
Victor Lindh <victorlindh@hotmail.com>
2026-01-22 14:13:26 +0100

    add key separator

--- i18next.config.ts
+++ i18next.config.ts
@@ -9,10 +9,8 @@ export default defineConfig({
     input: 'src/**/*.{js,jsx,ts,tsx}',
     output: 'src/locales/{{language}}/{{namespace}}.json',
     defaultValue: '', // What to put in the value field
-    keySeparator: '.', // This is the magic part: it tells the parser to nest based on dots
-    // Ensure your output is formatted nicely
+    keySeparator: '.',
     indentation: 2,
-    // If you want to keep the keys sorted alphabetically
     sort: true
   }
 })
