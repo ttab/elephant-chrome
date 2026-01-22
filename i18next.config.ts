ae5f525da25c890aa6a8eed910102ef149a7f51b
Victor Lindh <victorlindh@hotmail.com>
2026-01-22 14:12:25 +0100

    change translation parsing config

--- i18next.config.ts
+++ i18next.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
   ],
   extract: {
     input: 'src/**/*.{js,jsx,ts,tsx}',
-    output: 'src/locales/{{language}}/{{namespace}}.json'
+    output: 'src/locales/{{language}}/{{namespace}}.json',
+    defaultValue: '', // What to put in the value field
+    keySeparator: '.', // This is the magic part: it tells the parser to nest based on dots
+    // Ensure your output is formatted nicely
+    indentation: 2,
+    // If you want to keep the keys sorted alphabetically
+    sort: true
   }
 })
