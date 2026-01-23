8e3fa2bd000ba7f672165c90f752c5e44a1af93d
Gustav Larsson <gularsson@users.noreply.github.com>
2026-01-23 12:13:45 +0100

    fix(planning): update ignoreChangeKeys and unpublish desc (#2139)
    * fix(planning): update ignoreChangeKeys and unpublish desc
    
    - Expanded `ignoreChangeKeys` in PlanningViewContent to include assignment
      deliverables and status fields, preventing unnecessary change detection
      for these properties.
    - Updated the "unpublish" workflow description to remove "externally" for
      clarity.
    
    * fix: update unpublished transition description

--- src/defaults/workflowSpecification.tsx
+++ src/defaults/workflowSpecification.tsx
@@ -272,7 +272,7 @@ export const WorkflowSpecifications: Record<string, WorkflowSpecification> = {
         unpublished: {
           verify: true,
           title: 'Avpublicera',
-          description: 'Avpublicera händelsen externt'
+          description: 'Avpublicera händelsen'
         }
       }
     },
@@ -339,7 +339,7 @@ export const WorkflowSpecifications: Record<string, WorkflowSpecification> = {
         unpublished: {
           verify: true,
           title: 'Avpublicera',
-          description: 'Avpublicera planeringen externt'
+          description: 'Avpublicera planeringen'
         }
       }
     },
