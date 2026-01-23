cafc2468b33788b3a7cb5d03a953139ef6e851ea
LillaBlond <annika.karlsson@tt.se>
2026-01-23 12:00:57 +0100

    timeslots in columns translated

--- src/defaults/assignmentTimeslots.ts
+++ src/defaults/assignmentTimeslots.ts
@@ -4,25 +4,25 @@ type Slots = Record<string, { slots: number[], label: string }>
 export const timesSlots: Slots = {
   morning: {
     slots: [0, 1, 2, 3, 4, 5, 6],
-    label: 'Morgon'
+    label: 'morning'
   },
   forenoon: {
     slots: [7, 8, 9, 10, 11, 12],
-    label: 'Förmiddag'
+    label: 'forenoon'
   },
   afternoon: {
     slots: [13, 14, 15, 16, 17, 18],
-    label: 'Eftermiddag'
+    label: 'afternoon'
   },
   evening: {
     slots: [19, 20, 21, 22, 23],
-    label: 'Kväll'
+    label: 'evening'
   }
 }
 
 export const slotLabels = [
-  { value: 'morning', label: 'Morgon' },
-  { value: 'forenoon', label: 'Förmiddag' },
-  { value: 'afternoon', label: 'Eftermiddag' },
-  { value: 'evening', label: 'Kväll' }
+  { value: 'morning', label: 'morning' },
+  { value: 'forenoon', label: 'forenoon' },
+  { value: 'afternoon', label: 'afternoon' },
+  { value: 'evening', label: 'evening' }
 ]
