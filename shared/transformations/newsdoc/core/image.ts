955a4ae4e0323785a4bf828d063557f321aaa9db
Victor-Lindh <37335077+Victor-Lindh@users.noreply.github.com>
2026-01-26 12:57:13 +0100

    ttvisual: capture image caption format (#2119)
    * ttvisual: capture image caption format
    
    * handle format for core/image as well
    
    * set html_caption alongside text attribute
    
    * update tests
    
    * only pass html_caption if text has been formatted
    
    * fix tests, again

--- shared/transformations/newsdoc/core/image.ts
+++ shared/transformations/newsdoc/core/image.ts
@@ -12,6 +12,7 @@ export const transformImage = (element: Block): TBElement => {
     uri: links[0].uri,
     type: links[0].type,
     text: data.text,
+    html_caption: data.html_caption,
     credit: links[0].data.credit,
     width: data.width,
     height: data.height,
@@ -53,14 +54,39 @@ export function revertImage(element: TBElement): Block {
 
   function getText(node: Descendant | undefined) {
     let text = ''
-    if (node && 'children' in node && Array.isArray(node?.children)) {
-      const [child] = node.children
+    let html_caption = ''
+    let hasFormattedText = false
 
-      if (child && 'text' in child && child?.text) {
-        text = child.text
+    if (node && 'children' in node && Array.isArray(node?.children)) {
+      for (const child of node.children) {
+        const formatted = Object.keys(child).find((key) => key.startsWith('core/'))
+
+        if (child && 'text' in child && child?.text) {
+          text += child.text
+
+          if (!formatted) {
+            html_caption += child.text
+          }
+
+          if (formatted) {
+            hasFormattedText = true
+
+            if (formatted === 'core/bold') {
+              html_caption += `<strong>${child.text}</strong>`
+            }
+
+            if (formatted === 'core/italic') {
+              html_caption += `<em>${child.text}</em>`
+            }
+          }
+        }
       }
     }
-    return text
+
+    return {
+      text,
+      ...(hasFormattedText && { html_caption })
+    }
   }
 
   const captionText = getText(textNode)
@@ -75,18 +101,22 @@ export function revertImage(element: TBElement): Block {
     }
   ]
 
-  if (bylineText.length > 0) {
+  if (bylineText.text.length > 0) {
     links.push(Block.create({
       rel: 'author',
       type: 'core/author',
-      title: toString(bylineText),
+      title: toString(bylineText.text),
       uuid: crypto.randomUUID()
     }))
   }
 
+  const html_caption = toString(captionText.html_caption)
+  const text = toString(captionText.text)
+
   const data: Record<string, string> = {
-    text: toString(captionText),
-    credit: toString(bylineText),
+    ...(html_caption && { html_caption }),
+    text,
+    credit: toString(bylineText.text),
     height: toString(properties?.height),
     width: toString(properties?.width)
   }
--- shared/transformations/newsdoc/tt/visual.ts
+++ shared/transformations/newsdoc/tt/visual.ts
@@ -33,6 +33,7 @@ export const transformVisual = (element: Block): TBElement => {
     type: links[0].type,
     credit: links[0].data.credit,
     text: data.caption,
+    html_caption: data.html_caption,
     width: links[0].data.width,
     height: links[0].data.height,
     ...transformSoftcrop(meta) || {}
@@ -73,30 +74,59 @@ export function revertVisual(element: TBElement): Block {
 
   function getText(node: Descendant | undefined) {
     let text = ''
-    if (node && 'children' in node && Array.isArray(node?.children)) {
-      const [child] = node.children
+    let html_caption = ''
+    let hasFormattedText = false
 
-      if (child && 'text' in child && child?.text) {
-        text = child.text
+    if (node && 'children' in node && Array.isArray(node?.children)) {
+      for (const child of node.children) {
+        const formatted = Object.keys(child).find((key) => key.startsWith('core/'))
+
+        if (child && 'text' in child && child?.text) {
+          if (!formatted) {
+            html_caption += child.text
+          }
+
+          if (child && 'text' in child && child?.text) {
+            text += child.text
+
+            if (formatted) {
+              hasFormattedText = true
+              if (formatted === 'core/bold') {
+                html_caption += `<strong>${child.text}</strong>`
+              }
+              if (formatted === 'core/italic') {
+                html_caption += `<em>${child.text}</em>`
+              }
+            }
+          }
+        }
       }
     }
-    return text
+
+    return {
+      text,
+      ...(hasFormattedText && { html_caption })
+    }
   }
 
   const captionText = getText(textNode)
   const bylineText = getText(bylineNode)
 
   const data: Record<string, string> = {
-    credit: toString(bylineText),
+    credit: toString(bylineText.text),
     height: toString(properties?.height),
     width: toString(properties?.width)
   }
+
+  const caption = toString(captionText.text)
+  const html_caption = toString(captionText.html_caption)
 
   return Block.create({
     id,
     type: 'tt/visual',
     data: {
-      caption: toString(captionText)
+      caption,
+      ...(html_caption && { html_caption })
     },
     meta: revertSoftcrop(element),
     links: [
