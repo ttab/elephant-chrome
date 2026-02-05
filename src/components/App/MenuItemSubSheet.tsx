50b11a54a0d09bc64cfcb50f4da9da830bd740b3
LillaBlond <annika.karlsson@tt.se>
2026-02-05 10:57:50 +0100

    adding translation for MenuItemSubSheet title in sv

--- src/components/App/MenuItemSubSheet.tsx
+++ src/components/App/MenuItemSubSheet.tsx
@@ -10,6 +10,7 @@ import {
 import { XIcon } from '@ttab/elephant-ui/icons'
 import { Latest as LatestComponent } from '@/views/Latest'
 import type { JSX } from 'react'
+import { useTranslation } from 'react-i18next'
 
 interface SheetComponentProps {
   setOpen: (open: boolean) => void
@@ -24,8 +25,25 @@ export const MenuItemSubSheet = ({
   isOpen: boolean
   onClose: () => void
 }): JSX.Element => {
+  const { t } = useTranslation('app')
   const sheetComponents: Record<string, React.ComponentType<SheetComponentProps>> = {
     Latest: LatestComponent
+  }
+
+  const showTranslatedText = (menuItem: string): string => {
+    switch (menuItem) {
+      case 'plannings': return t('mainMenu.plans')
+      case 'approvals': return t('mainMenu.approvals')
+      case 'events': return t('mainMenu.events')
+      case 'assignments': return t('mainMenu.assignments')
+      case 'wires': return t('mainMenu.wires')
+      case 'factboxes': return t('mainMenu.factboxes')
+      case 'search': return t('mainMenu.search')
+      case 'print': return t('mainMenu.print')
+      case 'last published': return t('mainMenu.lastPublished')
+      default:
+        return 'Translation missing'
+    }
   }
 
   const SheetItemComponent = sheetComponents[menuItem.name]
@@ -46,7 +64,7 @@ export const MenuItemSubSheet = ({
             </SheetClose>
 
             <div className='flex items-center gap-2'>
-              <div>{menuItem.label}</div>
+              <div>{showTranslatedText(menuItem.label)}</div>
               <menuItem.icon strokeWidth={2.25} size={18} color={menuItem.color} />
             </div>
 
