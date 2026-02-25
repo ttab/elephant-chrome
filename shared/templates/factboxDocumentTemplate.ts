2bbdb058be0977852426e6cb10bb44c2820dfb3e
LillaBlond <annika.karlsson@tt.se>
2026-02-25 09:58:40 +0000

    adding payload to factboxTemplate parameters

--- shared/templates/factboxDocumentTemplate.ts
+++ shared/templates/factboxDocumentTemplate.ts
@@ -1,17 +1,19 @@
 import { Block, Document } from '@ttab/elephant-api/newsdoc'
+import type { TemplatePayload } from '.'
+
 
 /**
 * Create a template for a factbox document
 * @returns Document
 */
 
-export function factboxDocumentTemplate(id: string): Document {
+export function factboxDocumentTemplate(id: string, payload?: TemplatePayload): Document {
   return Document.create({
     uuid: id,
     type: 'core/factbox',
     uri: `core://factbox/${id}`,
     language: 'sv-se',
-    title: '',
+    title: payload?.title ?? '',
     content: [
       Block.create({
         type: 'core/text',
