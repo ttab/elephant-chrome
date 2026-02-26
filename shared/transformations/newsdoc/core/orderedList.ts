5c1302fbd0a98f8f1e080d9c138f4bb4d3f96072
LillaBlond <annika.karlsson@tt.se>
2026-02-26 14:27:29 +0000

    making it possible to use unordered and ordered lists in factboxes

---
+++ shared/transformations/newsdoc/core/orderedList.ts
@@ -0,0 +1,39 @@
+import { Block } from '@ttab/elephant-api/newsdoc'
+import type { TBElement } from '@ttab/textbit'
+import { toString } from '../../lib/toString.js'
+
+export function transformOrderedList(element: Block): TBElement {
+  return {
+    id: element.id || crypto.randomUUID(),
+    class: 'text',
+    type: 'core/ordered-list',
+    children: element.content.map((child: Block) => {
+      return {
+        id: child.id || crypto.randomUUID(),
+        class: 'text',
+        type: 'core/ordered-list/list-item',
+        children: [
+          { text: child.data.text }
+        ]
+      }
+    })
+  }
+}
+
+export function revertOrderedList(transformedList: TBElement): Block {
+  return Block.create({
+    id: transformedList.id || crypto.randomUUID(),
+    type: transformedList.type,
+    content: (transformedList.children as TBElement[]).map((item) => {
+      return Block.create({
+        id: toString(item.id),
+        type: 'core/text',
+        data: {
+          text: Array.isArray(item.children) && item.children.length > 0 && typeof item.children[0] === 'object' && 'text' in item.children[0]
+            ? item.children[0].text
+            : ''
+        }
+      })
+    })
+  })
+}
