074a82e7516f97a910d0954b25da3bc81141dee9
Danne Lundqvist <danne@dotvoid.com>
2026-02-27 15:06:00 +0100

    Update shared/meta.ts
    Co-authored-by: Gustav Larsson <gularsson@users.noreply.github.com>

--- shared/meta.ts
+++ shared/meta.ts
@@ -5,7 +5,14 @@ export function meta(accessToken: string, abort?: AbortSignal) {
   return {
     meta: {
       authorization: `bearer ${accessToken}`
+export function meta(accessToken: string, abort?: AbortSignal): RpcOptions {
+  return {
+    meta: {
+      authorization: `bearer ${accessToken}`
     },
-    signal: abort
+    abort
+  }
+}
+
   }
 }
