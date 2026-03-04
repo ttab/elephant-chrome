a5dcbfc3ae599e7854e9fa404a9a7c0887b2aa97
LillaBlond <annika.karlsson@tt.se>
2026-03-04 15:57:04 +0100

    adding quickfilter to toolbar and changing background of toolbar

--- src/views/Factboxes/Toolbar.tsx
+++ src/views/Factboxes/Toolbar.tsx
@@ -2,12 +2,13 @@ import { Command } from '@ttab/elephant-ui'
 import { DebouncedCommandInput } from '@/components/Commands/Menu/DebouncedCommandInput'
 import { useQuery } from '@/hooks/useQuery'
 import type { JSX } from 'react'
+import { QuickFilter } from '@/components/Table/QuickFilter'
 
 export const Toolbar = (): JSX.Element => {
   const [filter, setFilter] = useQuery(['query'])
 
   return (
-    <div className='bg-table-bg flex items-center justify-between py-1 px-4 border-b sticky top-0 z-10'>
+    <div className='bg-background flex items-center justify-between py-1 px-4 border-b sticky top-0 z-10'>
       <div className='flex flex-1 items-center space-x-2'>
         <Command
           className='[&_[cmdk-input-wrapper]]:border-none'
@@ -26,6 +27,7 @@ export const Toolbar = (): JSX.Element => {
           />
         </Command>
       </div>
+      <QuickFilter />
     </div>
   )
 }
