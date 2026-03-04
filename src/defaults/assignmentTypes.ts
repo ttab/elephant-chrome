b81dac4b9d4e5487f31eff135d55edc9ed135419
vilindh <victorlindh@hotmail.com>
2026-03-04 15:52:53 +0100

    add labels to assignment types

--- src/defaults/assignmentTypes.ts
+++ src/defaults/assignmentTypes.ts
@@ -8,6 +8,7 @@ import {
   FileWarningIcon,
   ChartPieIcon
 } from '@ttab/elephant-ui/icons'
+import i18next from 'i18next'
 
 const iconProps = {
   size: 18,
@@ -18,36 +19,43 @@ const iconProps = {
 export const AssignmentTypes: DefaultValueOption[] = [
   {
     value: 'text',
+    label: i18next.t('shared:assignmentTypes.text'),
     icon: FileTextIcon,
     iconProps
   },
   {
     value: 'flash',
+    label: i18next.t('shared:assignmentTypes.flash'),
     icon: ZapIcon,
     iconProps
   },
   {
     value: 'editorial-info',
+    label: i18next.t('shared:assignmentTypes.editorial-info'),
     icon: FileWarningIcon,
     iconProps
   },
   {
     value: 'graphic',
+    label: i18next.t('shared:assignmentTypes.graphic'),
     icon: ChartPieIcon,
     iconProps
   },
   {
     value: 'picture',
+    label: i18next.t('shared:assignmentTypes.picture'),
     icon: CameraIcon,
     iconProps
   },
   {
     value: 'video',
+    label: i18next.t('shared:assignmentTypes.video'),
     icon: VideoIcon,
     iconProps
   },
   {
     value: 'picture/video',
+    label: i18next.t('shared:assignmentTypes.picture/video'),
     icon: ApertureIcon,
     iconProps
   }
