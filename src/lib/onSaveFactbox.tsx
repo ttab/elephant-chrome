c7306b4189266a77299175bc40befab75d6299e7
vilindh <victorlindh@hotmail.com>
2026-03-05 08:21:45 +0100

    add function for saving an inline created factbox

---
+++ src/lib/onSaveFactbox.tsx
@@ -0,0 +1,56 @@
+import { yTextToSlateElement } from '@slate-yjs/core'
+import { revertFactbox } from '@/shared/transformations/newsdoc/core/factbox'
+import { Document } from '@ttab/elephant-api/newsdoc'
+import { TextbitElement, type TBElement } from '@ttab/textbit'
+import type * as Y from 'yjs'
+import type { Repository } from '@/shared/Repository'
+import { toast } from 'sonner'
+
+export const onSaveFactbox = async ({
+  id,
+  content,
+  repository,
+  documentLanguage,
+  accessToken,
+  onClose
+}: {
+  id: string
+  content: Y.XmlText
+  repository: Repository
+  documentLanguage: string
+  accessToken: string
+  onClose: () => void
+}) => {
+  if (!id || !content || !repository || !accessToken) {
+    return
+  }
+
+  const { children } = yTextToSlateElement(content)
+  const factboxElement = (children as TBElement[]).find((el) =>
+    TextbitElement.isElement(el)
+    && el.type === 'core/factbox'
+    && el.properties?.original_id === id
+  )
+
+  if (!factboxElement) {
+    return
+  }
+
+  const factboxBlock = revertFactbox(factboxElement)
+
+  const document = Document.create({
+    uuid: id,
+    type: 'core/factbox',
+    uri: `core://factbox/${id}`,
+    language: documentLanguage ?? 'sv-se',
+    title: factboxBlock.title,
+    content: factboxBlock.content
+  })
+
+  await repository.saveDocument(document, accessToken, 'usable')
+    .then(() => toast.success('Faktarutan har sparats'))
+    .catch(() => {
+      throw new Error('Could not save factbox')
+    })
+    .finally(() => onClose?.())
+}
