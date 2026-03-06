ce7611f8f58966b775f8a28505e6af195b4347ef
vilindh <37335077+vilindh@users.noreply.github.com>
2026-03-06 15:05:44 +0100

    Editorial info translations (#2277)
    * use translation key instead of hard-coded value
    
    * add translations
    
    * corrected import

--- shared/templates/editorialInfoDocumentTemplate.ts
+++ shared/templates/editorialInfoDocumentTemplate.ts
@@ -1,6 +1,7 @@
 import { Document } from '@ttab/elephant-api/newsdoc'
 import type { TemplatePayload } from './index.js'
 import { getSystemLanguage } from '@/shared/getSystemLanguage.js'
+import i18n from 'i18next'
 
 /**
  * Generates a document template for type editorial-info
@@ -35,7 +36,7 @@ export function editorialInfoDocumentTemplate(id: string, payload?: TemplatePayl
       {
         type: 'core/note',
         data: {
-          text: 'Obs! Detta meddelande är inte avsett för publicering.'
+          text: i18n.t('shared:editorialInfo.noteText')
         },
         role: 'public'
       },
