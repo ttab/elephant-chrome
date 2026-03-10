bf51088962c8e42b03a88ed708609571552dab8f
vilindh <victorlindh@hotmail.com>
2026-03-10 15:30:52 +0100

    pass featureFlags as argument to func

--- src/components/View/ViewHeader/Title.tsx
+++ src/components/View/ViewHeader/Title.tsx
@@ -6,6 +6,7 @@ import type { YDocument } from '@/modules/yjs/hooks'
 import type * as Y from 'yjs'
 import { useQuery } from '@/hooks/useQuery'
 import { useTranslation } from 'react-i18next'
+import { useRegistry } from '@/hooks/index'
 
 export const Title = ({
   name,
@@ -32,6 +33,7 @@ export const Title = ({
   const timeoutRef = useRef<NodeJS.Timeout | null>(null)
   const timeout2Ref = useRef<NodeJS.Timeout | null>(null)
   const { t } = useTranslation()
+  const { featureFlags } = useRegistry()
 
   useEffect(() => {
     if (timeoutRef.current) {
@@ -79,7 +81,7 @@ export const Title = ({
     setQuery({ preview: undefined })
   }
 
-  const { icon: ViewIcon, color } = getApplicationMenu().groups
+  const { icon: ViewIcon, color } = getApplicationMenu(featureFlags).groups
     .flatMap((g) => g.items)
     .find((i) => i.name === name) || {}
 
