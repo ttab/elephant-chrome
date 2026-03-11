eac33a70d9892aee5397d48bd06c9bbffbdf2de3
vilindh <victorlindh@hotmail.com>
2026-03-11 20:56:24 +0100

    stabilize memo deps

--- src/hooks/useFeatureFlags.tsx
+++ src/hooks/useFeatureFlags.tsx
@@ -4,16 +4,14 @@ import type { AllowedFeatureFlag } from 'src/datastore/types'
 
 export const useFeatureFlags = (flags: (keyof AllowedFeatureFlag)[]): AllowedFeatureFlag => {
   const { featureFlags } = useRegistry()
+  const flagsKey = flags.join(',')
 
-  const returnedFlags = useMemo(() => {
+  return useMemo(() => {
     const resolvedFlags: AllowedFeatureFlag = {}
-
     flags.forEach((flag) => {
       resolvedFlags[flag] = featureFlags[flag] ?? false
     })
-
     return resolvedFlags
-  }, [flags, featureFlags])
-
-  return returnedFlags
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [featureFlags, flagsKey])
 }
