ac4fe8a87e7998dfef0eece8bc532077941d2ac3
Gustav Larsson <gularsson@users.noreply.github.com>
2026-03-13 12:47:38 +0100

    Fix/add correct icon to ed info (#2304)
    * fix: use correct icon for editorial info readonly
    
    * fix: key should be Article because of cast to View
    
    * fix: quickie restore add updated elephant-ui

--- src/defaults/documentTypeFormats.ts
+++ src/defaults/documentTypeFormats.ts
@@ -4,45 +4,114 @@ import {
   CalendarPlus2Icon,
   NewspaperIcon,
   BoxesIcon,
+  FileWarningIcon,
+  PenBoxIcon,
+  PenOffIcon,
+  ZapIcon,
   type LucideIcon
 } from '@ttab/elephant-ui/icons'
-import { ZapIcon } from 'lucide-react'
 
-export const documentTypeValueFormat: Record<string, { icon: LucideIcon, key: string, label: string, color: string }> = {
+type DocumentTypeFormat = Record<string, {
+  icon: LucideIcon
+  key: string
+  label: string
+  color: string
+  readonly?: {
+    icon: LucideIcon
+  }
+}>
+
+const event: DocumentTypeFormat = {
   'core/event': {
     icon: CalendarPlus2Icon,
     label: 'Händelse',
     key: 'Event',
-    color: '#5E9F5D'
-  },
+    color: '#D802FD'
+  }
+}
+
+const planning: DocumentTypeFormat = {
   'core/planning-item': {
     icon: CalendarDaysIcon,
     label: 'Planering',
     key: 'Planning',
     color: '#FF971E'
-  },
+  }
+}
+
+const assignment: DocumentTypeFormat = {
   'core/assignment': {
     icon: BriefcaseBusinessIcon,
     label: 'Uppdrag',
     key: 'Assignment',
     color: '#006bb3'
-  },
+  }
+}
+
+const article: DocumentTypeFormat = {
   'core/article': {
-    icon: NewspaperIcon,
-    label: 'Två på två',
+    icon: PenBoxIcon,
+    label: 'Artikel',
     key: 'Article',
+    color: '#50BEBF',
+    readonly: {
+      icon: PenOffIcon
+    }
+  }
+}
+
+const editorialInfo: DocumentTypeFormat = {
+  'core/editorial-info': {
+    icon: FileWarningIcon,
+    label: 'Till red',
+    key: 'EditorialInfo',
     color: '#50BEBF'
-  },
+  }
+}
+
+const factbox: DocumentTypeFormat = {
   'core/factbox': {
     icon: BoxesIcon,
     label: 'Faktaruta',
     key: 'Factbox',
     color: '#99c5c4'
-  },
+  }
+}
+
+const flash: DocumentTypeFormat = {
   'core/flash': {
     icon: ZapIcon,
     label: 'Flash',
     key: 'Flash',
     color: '#FF5150'
   }
+}
+
+const quickArticle: DocumentTypeFormat = {
+  'core/article': {
+    icon: NewspaperIcon,
+    label: 'Två på två',
+    key: 'QuickArticle',
+    color: '#aabbcc'
+  }
+}
+
+
+export const documentTypeValueFormat = {
+  ...event,
+  ...planning,
+  ...article,
+  ...editorialInfo,
+  ...factbox,
+  ...flash
+}
+
+export const addButtonGroupValueFormat = {
+  ...event,
+  ...planning,
+  ...assignment,
+  ...quickArticle,
+  ...editorialInfo,
+  ...factbox,
+  ...flash
 }
