a3ba9689bd2fefd7dd06da80f1ad83c4f0b1b35e
LillaBlond <annika.karlsson@tt.se>
2026-03-16 09:46:16 +0100

    adding onChange as a prop attribute to freetextfilter

--- src/components/Commands/index.tsx
+++ src/components/Commands/index.tsx
@@ -2,24 +2,54 @@ import { CommandList } from '@ttab/elephant-ui'
 import { ClearFilter } from '@/components/Filter/ClearFilter'
 import { ColumnFilter } from './Items'
 import { useTable } from '@/hooks/useTable'
-import type { JSX } from 'react'
+import { useQuery } from '@/hooks/index'
+import { useMemo, type JSX } from 'react'
 import { FreeTextFilter } from '../Filter/common/FreeTextFilter'
 
-export const Commands = ({ filterType }: { filterType: string }): JSX.Element => {
-  const { table } = useTable()
+export const Commands = ({ filterType }: { filterType?: 'freetext' | 'filterOptions' }): JSX.Element => {
+  const { table, command } = useTable()
+  const [, setFilter] = useQuery(['query'])
+  const { setSearch } = command
+
   const state = table.getState()
   const hasFilter = !!state.columnFilters.length || typeof state.globalFilter === 'string'
+
+  const isFreetext = useMemo(() => filterType === 'freetext', [filterType])
 
   const handleClear = () => {
     table.resetColumnFilters()
     table.resetGlobalFilter()
   }
 
+  const handleInputChange = (value: string) => {
+    if (isFreetext) {
+      if (value) {
+        if (table.setGlobalFilter) {
+          table.setGlobalFilter(value)
+        } else {
+          setFilter({ query: [value] })
+        }
+      } else {
+        if (table.resetGlobalFilter) {
+          table.resetGlobalFilter()
+        } else {
+          setFilter({ query: [] })
+        }
+      }
+    } else {
+      setSearch(value)
+    }
+  }
+
   return (
     <>
-      {filterType !== 'freetext' && <FreeTextFilter filterType={filterType} />}
+      {filterType !== 'freetext' && (
+        <FreeTextFilter filterType={filterType ?? 'filterOptions'} value='' onChange={handleInputChange} />
+      )}
       <CommandList>
-        {filterType === 'freetext' && <FreeTextFilter filterType={filterType} />}
+        {filterType === 'freetext' && (
+          <FreeTextFilter filterType={filterType ?? ''} value='' onChange={handleInputChange} />
+        )}
         <ColumnFilter />
         <ClearFilter hasFilter={hasFilter} onClear={handleClear} />
       </CommandList>
--- src/components/Filter/common/FreeTextFilter.tsx
+++ src/components/Filter/common/FreeTextFilter.tsx
@@ -1,69 +1,41 @@
 import { DebouncedCommandInput } from '@/components/Commands/Menu/DebouncedCommandInput'
-import { useQuery, useTable } from '@/hooks/index'
 import { CommandItem } from '@ttab/elephant-ui'
 import { cn } from '@ttab/elephant-ui/utils'
-import { useMemo, useState, type JSX } from 'react'
+import { useState, type JSX } from 'react'
 
-export const FreeTextFilter = ({ filterType }: { filterType: string }): JSX.Element => {
-  const { table, command } = useTable()
-  const [filter, setFilter] = useQuery(['query'])
+interface FreeTextFilterProps {
+  value: string
+  onChange: (value: string) => void
+  filterType: 'freetext' | 'filterOptions'
+  autoFocus?: boolean
+}
+
+export const FreeTextFilter = ({ value, onChange, filterType, autoFocus = true }: FreeTextFilterProps): JSX.Element => {
   const [selected, setSelected] = useState(true)
-  const { setSearch, search } = command
-
-  const filterTypeValue = useMemo(() => {
-    if (filterType === 'freetext') {
-      return filterType
-    }
-  }, [filterType])
-
-  const handleInputChange = (value: string | undefined) => {
-    if (filterTypeValue === 'freetext') {
-      if (value) {
-        if (table.setGlobalFilter) {
-          table.setGlobalFilter(value)
-        } else {
-          setFilter({ query: [value] })
-        }
-      } else {
-        if (table.resetGlobalFilter) {
-          table.resetGlobalFilter()
-        } else {
-          setFilter({ query: [] })
-        }
-      }
-    } else {
-      console.log('filter type is not freetext, not setting search:', value)
-      if (value) {
-        setSearch(value)
-      } else {
-        setSearch('')
-      }
-      console.log('setting search:', search)
-    }
-  }
-
   return (
     <CommandItem
       forceMount={true}
       key='freetext'
       value='freetext'
       onKeyUp={(e) => {
-        if (e.currentTarget.getAttribute('data-selected') === 'true') {
-          setSelected(true)
-        } else {
-          setSelected(false)
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+          if (e.currentTarget.getAttribute('data-selected') === 'true') {
+            setSelected(true)
+          } else {
+            setSelected(false)
+          }
         }
       }}
       className={cn('pb-0 mb-1.5 bg-white!',
-        (filterType === 'freetext') && 'data-[selected=true]:shadow-[inset_0_0px_3px_rgba(0,0,0,0.5)]')}
+        filterType === 'freetext' && 'data-[selected=true]:shadow-[inset_0_0px_3px_rgba(0,0,0,0.5)]')}
     >
       <DebouncedCommandInput
-        value={filter?.query?.[0] ?? search ?? ''}
-        onChange={(value) => handleInputChange(value)}
+        value={value}
+        onChange={(v) => onChange(v ?? '')}
         placeholder={filterType === 'freetext' ? 'Fritext' : 'Sök alternativ'}
         className='h-9'
-        autoFocus={true}
-        readOnly={!selected && filterType === 'freetext'}
+        autoFocus={autoFocus}
+        readOnly={filterType === 'freetext' && !selected ? true : false}
       />
     </CommandItem>
   )
