c071878670aef6c6b80161980e8ca90936c595b9
LillaBlond <annika.karlsson@tt.se>
2026-03-20 12:22:48 +0100

    adding grouping by user assignees to assignments

--- src/views/Assignments/AssignmentColumns.tsx
+++ src/views/Assignments/AssignmentColumns.tsx
@@ -221,9 +221,20 @@ export function assignmentColumns({ authors = [], locale, timeZone, sections = [
         ),
         name: 'Uppdragstagare',
         columnIcon: UsersIcon,
-        className: 'flex-none w-[112px] hidden @5xl/view:[display:revert]'
+        className: 'flex-none w-[112px] hidden @5xl/view:[display:revert]',
+        display: (value: string) => {
+          const names = value.split(',').filter(Boolean)
+          return <Assignees assignees={names} />
+        }
       },
       accessorFn: (data) => data.fields['document.meta.core_assignment.rel.assignee.uuid']?.values,
+      getGroupingValue: (data) => {
+        const assignees = data.fields['document.meta.core_assignment.rel.assignee.uuid']?.values ?? []
+        return assignees
+          .map((uuid) => authors.find((a) => a.id === uuid)?.name ?? '??')
+          .sort()
+          .join(',')
+      },
       cell: ({ row }) => {
         const assignees = (row.getValue<string[]>('assignees') || []).map((assigneeId) => {
           return authors.find((author) => author.id === assigneeId)?.name || ''
@@ -235,7 +246,7 @@ export function assignmentColumns({ authors = [], locale, timeZone, sections = [
         const assignees = row.getValue<string[]>(id) || []
         return value.some((v) => assignees.includes(v))
       },
-      enableGrouping: false
+      enableGrouping: true
     },
     {
       id: 'assignment_time',
