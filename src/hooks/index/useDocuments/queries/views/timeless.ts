87b375ecfb6b9a6a170666749185caddb5f756d4
gvl <git@gula.dev>
2026-04-21 11:03:09 +0200

    fix(timeless): remove document.language filter from overview query

--- src/hooks/index/useDocuments/queries/views/timeless.ts
+++ src/hooks/index/useDocuments/queries/views/timeless.ts
@@ -1,40 +1,10 @@
 import type { QueryParams } from '@/hooks/useQuery'
-import { getSystemLanguage } from '@/shared/getSystemLanguage'
-import {
-  QueryV1,
-  BoolQueryV1,
-  TermsQueryV1,
-  SortingV1
-} from '@ttab/elephant-api/index'
+import { SortingV1 } from '@ttab/elephant-api/index'
 import { fields } from '@/shared/schemas/timelessArticle'
-
-function constructQuery(): QueryV1 {
-  const systemLanguage = getSystemLanguage()
-
-  return QueryV1.create({
-    conditions: {
-      oneofKind: 'bool',
-      bool: BoolQueryV1.create({
-        must: [
-          {
-            conditions: {
-              oneofKind: 'terms',
-              terms: TermsQueryV1.create({
-                field: 'document.language',
-                values: [systemLanguage, systemLanguage.split('-')[0]]
-              })
-            }
-          }
-        ]
-      })
-    }
-  })
-}
 
 export const timelessParams = (_filter: QueryParams) => ({
   documentType: 'core/article#timeless',
   fields,
-  query: constructQuery(),
   sort: [
     SortingV1.create({ field: 'modified', desc: true }),
     SortingV1.create({ field: 'document.title.sort', desc: false })
