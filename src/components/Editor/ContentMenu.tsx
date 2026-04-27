3726e55c7e565407da18b0a7949963b0d9d87d7e
vilindh <37335077+vilindh@users.noreply.github.com>
2026-04-27 08:18:30 +0200

    Editor: prevent block class actions from being displayed and added fr… (#2486)
    * Editor: prevent block class actions from being displayed and added from gutter content menu
    
    * add reflective comment
    
    * use isBlock method from textbit to check context level instead
    
    * don't render empty menu container if no actions are available

--- src/components/Editor/ContentMenu.tsx
+++ src/components/Editor/ContentMenu.tsx
@@ -1,31 +1,46 @@
 import type { JSX } from 'react'
-import { Menu, usePluginRegistry } from '@ttab/textbit'
+import { Menu, useEditor, usePluginRegistry } from '@ttab/textbit'
 import { ContentMenuGroup } from './ContentMenuGroup'
 import { ContentMenuItem } from './ContentMenuItem'
+import { TextbitElement } from '@ttab/textbit'
 
 export const ContentMenu = (): JSX.Element => {
+  const editor = useEditor()
   const { actions } = usePluginRegistry()
 
-  const textActions = actions.filter((action) => action.plugin.class === 'text')
-  const blockActions = actions.filter((action) => action.plugin.class === 'block')
+  // Check in what plugin context the user is upon selecting from the menu.
+  // If they are inside a block class element, other block class elements should not be selectable to insert,
+  // as we currently have no need for adding one block class element inside another.
+  // However, this might change in future iterations.
+  const topNode = editor.selection
+    ? editor.children[editor.selection.anchor.path[0]]
+    : undefined
+  const insideBlock = TextbitElement.isBlock(topNode)
 
-  return (
-    <Menu.Root className='group mt-2'>
-      <Menu.Trigger className="flex justify-center place-items-center center font-bold border w-8 h-8 ml-3 rounded-full cursor-default group-data-[state='open']:border-gray-200 hover:border-gray-400 dark:text-slate-200 dark:bg-slate-950 dark:border-slate-600 dark:group-data-[state='open']:border-slate-700 dark:hover:border-slate-500">⋮</Menu.Trigger>
-      <Menu.Content className='flex flex-col mt-[1.3rem] -ml-[1.3rem] border rounded-lg divide-y shadow-xl bg-white border-gray-100 dark:text-white dark:bg-slate-900 dark:border-slate-800 dark:divide-slate-800 dark:shadow-none z-50'>
-        {blockActions.length > 0
-          && (
-            <ContentMenuGroup>
-              {blockActions.map((action) => <ContentMenuItem action={action} key={action.name} />)}
-            </ContentMenuGroup>
-          )}
-        {textActions.length > 0
-          && (
-            <ContentMenuGroup>
-              {textActions.map((action) => <ContentMenuItem action={action} key={action.name} />)}
-            </ContentMenuGroup>
-          )}
-      </Menu.Content>
-    </Menu.Root>
-  )
+  const textActions = actions.filter((action) => action.plugin.class === 'text')
+  const blockActions = insideBlock
+    ? []
+    : actions.filter((action) => action.plugin.class === 'block')
+
+  return (blockActions.length > 0 || textActions.length > 0)
+    ? (
+        <Menu.Root className='group mt-2'>
+          <Menu.Trigger className="flex justify-center place-items-center center font-bold border w-8 h-8 ml-3 rounded-full cursor-default group-data-[state='open']:border-gray-200 hover:border-gray-400 dark:text-slate-200 dark:bg-slate-950 dark:border-slate-600 dark:group-data-[state='open']:border-slate-700 dark:hover:border-slate-500">⋮</Menu.Trigger>
+          <Menu.Content className='flex flex-col mt-[1.3rem] -ml-[1.3rem] border rounded-lg divide-y shadow-xl bg-white border-gray-100 dark:text-white dark:bg-slate-900 dark:border-slate-800 dark:divide-slate-800 dark:shadow-none z-50'>
+            {blockActions.length > 0
+              && (
+                <ContentMenuGroup>
+                  {blockActions.map((action) => <ContentMenuItem action={action} key={action.name} />)}
+                </ContentMenuGroup>
+              )}
+            {textActions.length > 0
+              && (
+                <ContentMenuGroup>
+                  {textActions.map((action) => <ContentMenuItem action={action} key={action.name} />)}
+                </ContentMenuGroup>
+              )}
+          </Menu.Content>
+        </Menu.Root>
+      )
+    : <></>
 }
