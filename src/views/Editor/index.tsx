6c7d04d02df35f3383babe0a135b4d0c1c8fefcb
vilindh <victorlindh@hotmail.com>
2026-05-13 12:54:15 +0200

    pass role instead of name

--- src/views/Editor/index.tsx
+++ src/views/Editor/index.tsx
@@ -164,7 +164,7 @@ function EditorWrapper(props: ViewProps & {
       }),
       Text({
         countCharacters: hast ? ['heading-1', 'preamble'] : ['heading-1'],
-        ...(hasVignette ? {} : { hiddenStyles: ['set-dateline'] }),
+        ...(hasVignette ? {} : { hiddenStyles: ['vignette'] }),
         ...getContentMenuLabels()
       }),
       (() => {
